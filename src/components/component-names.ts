3521135869d9de2d10c79b4c7ccf4a9b19f3463a
Martin Simard <martin.simard@dti.ulaval.ca>
2017-05-31 15:25:21 -0400

    Added a directive sample

--- src/components/component-names.ts
+++ src/components/component-names.ts
@@ -1,3 +1,4 @@
+// components
 export const BUTTON_NAME: string = 'm-button';
 export const LIST_NAME: string = 'm-list';
 export const DYNAMIC_TEMPLATE_NAME: string = 'm-dynamic-template';
@@ -6,4 +7,11 @@ export const components: string[] = [
     BUTTON_NAME,
     LIST_NAME,
     DYNAMIC_TEMPLATE_NAME
+];
+
+// directives
+export const BACKGROUND_COLOR_NAME: string = 'm-background-color';
+
+export const directives: string[] = [
+    BACKGROUND_COLOR_NAME
 ];
