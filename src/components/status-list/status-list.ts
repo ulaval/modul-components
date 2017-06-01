be48eee063c05b8d40172f92495897f964a4102d
Philippe Rail <philipperail@gmail.com>
2017-06-01 16:28:46 -0400

    Status list components 0.1

--- src/components/status-list/status-list.ts
+++ src/components/status-list/status-list.ts
@@ -1,11 +1,42 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import {Prop} from 'vue-property-decorator';
 import WithRender from './status-list.html?style=./status-list.scss';
-
+import { STATUS_LIST_NAME } from '../component-names';
 @WithRender
 @Component
+
 export class MStatusList extends Vue {
-    public highlight(): void {
-        console.log('status list init');
-    }
+    private componentName = STATUS_LIST_NAME;
+
+    @Prop({ default:
+        function() {
+            return [
+                    {
+                        'status': 'completed',
+                        'text': 'Informations default'
+                    },
+                    {
+                        'status': 'pending',
+                        'text': 'Questions default'
+                    },
+                    {
+                        'status': 'error',
+                        'text': 'Pièces default'
+                    },
+                    {
+                        'status': 'completed',
+                        'text': 'Formulaire default'
+                    }
+            ];
+        }
+    })
+    private model: Object[];
+
+    public values: Object[] = this.model;
+
+    // public mounted() {
+    //     console.log(this.model);
+    // }
+
 }
