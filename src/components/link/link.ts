21ba9b0fc495cfc47f6c4b4f17814cafb3bc96c2
Martin Simard <martin.simard@dti.ulaval.ca>
2017-06-08 09:34:55 -0400

    Fix tslint on switch-case

--- src/components/link/link.ts
+++ src/components/link/link.ts
@@ -27,18 +27,18 @@ export class MLink extends Vue {
     private mounted() {
         this.hrefAttribute = this.url;
         switch (this.$props.type) {
-        case 'link':
-            this.isLink = true;
-            break;
-        case 'externalLink':
-            this.isExternalLink = true;
-            break;
-        case 'button':
-            this.isButton = true;
-            break;
-        default:
-            this.url = this.url == '#' ? '/' : this.url;
-            this.isRouterLink = true;
+            case 'link':
+                this.isLink = true;
+                break;
+            case 'externalLink':
+                this.isExternalLink = true;
+                break;
+            case 'button':
+                this.isButton = true;
+                break;
+            default:
+                this.url = this.url == '#' ? '/' : this.url;
+                this.isRouterLink = true;
         }
     }
 
