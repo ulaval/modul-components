49e1a4ad1b0867bda4708ed3ac9339a23d10f15f
Raphaël Paré <raph.pare@outlook.com>
2017-06-13 14:53:56 -0400

    P408-33: Fixe CSS accordion and add accordion.lang.fr

--- src/lang/fr.ts
+++ src/lang/fr.ts
@@ -4,6 +4,7 @@ import { FRENCH } from '../utils/i18n';
 const FrenchPlugin: PluginObject<any> = {
     install(v, options) {
         if ((v as any).$i18n) {
+            (v as any).$i18n.addMessages(FRENCH, require('../components/accordion/accordion.lang.fr.json'));
             (v as any).$i18n.addMessages(FRENCH, require('../components/accordion-group/accordion-group.lang.fr.json'));
             (v as any).$i18n.addMessages(FRENCH, require('../components/dialog/dialog.lang.fr.json'));
         } else {
