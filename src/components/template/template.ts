9a938e88bd09f9f7b66c16ad60bafc9cca3b0dad
Raphaël Paré <raph.pare@outlook.com>
2017-06-26 13:00:45 -0400

    Fixe template component

--- src/components/template/template.ts
+++ src/components/template/template.ts
@@ -3,14 +3,9 @@ import Component from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
 import WithRender from './template.html?style=./template.scss';
 import { TEMPLATE_NAME } from '../component-names';
-import { MediaQueries, MediaQueriesMixin } from '../../mixins/media-queries';
 
 @WithRender
-@Component({
-    mixins: [
-        MediaQueries
-    ]
-})
+@Component
 export class Mtemplate extends Vue {
     @Prop()
     public title: string;
