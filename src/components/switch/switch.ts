28b2fe5c0d565a6ef3c152881f4fe4a76ff921b3
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2017-06-28 11:58:29 -0400

    Switch component v1

--- src/components/switch/switch.ts
+++ src/components/switch/switch.ts
@@ -4,34 +4,49 @@ import Component from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
 import WithRender from './switch.html?style=./switch.scss';
 import { SWITCH_NAME } from '../component-names';
+import uuid from '../../utils/uuid/uuid';
 
 const POSITION_LEFT: string = 'left';
+const ON_TEXT: string = 'yes';
+const OFF_TEXT: string = 'no';
 
 @WithRender
 @Component
 export class MSwitch extends Vue {
 
-    @Prop({ default: 'switch' })
-    public name: string;
-    @Prop({ default: true })
-    public hasLabel: boolean;
+    @Prop({ default: false })
+    public isChecked: boolean;
     @Prop({ default: POSITION_LEFT })
     public position: string;
+    @Prop({ default: false })
+    public hasLabel: boolean;
+    @Prop({ default: ON_TEXT })
+    public onText: string;
+    @Prop({ default: OFF_TEXT })
+    public offText: string;
 
     public componentName: string = SWITCH_NAME;
+    private propsIsChecked: boolean = true;
     private propsHasLabel: boolean = true;
-    private propsPosition: string = POSITION_LEFT;
-    private isFocus: boolean = false;
-    private checkedValue: string = '';
-    private test: boolean = false;
+    private propsOnText: string = ON_TEXT;
+    private propsOffText: string = OFF_TEXT;
+    private isFocus = false;
+    private id: string = `switch${uuid.generate()}`;
 
-    private mounted(): void {
+    public mounted(): void {
+        this.propsIsChecked = this.isChecked;
         this.propsHasLabel = this.hasLabel;
-        this.propsPosition = this.position;
+        this.propsOnText = this.onText;
+        this.propsOffText = this.offText;
     }
 
     private onClick(event): void {
-        this.$emit('click', this.checkedValue);
+        this.$emit('click');
+        this.$refs['switch']['blur']();
+    }
+
+    public get hasSwitchLeft(): boolean {
+        return this.position == POSITION_LEFT;
     }
 }
 
