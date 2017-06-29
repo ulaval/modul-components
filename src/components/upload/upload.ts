9d7fe881e3f1328ee9ba8583759b63082994215c
Jean-Philippe Guilmette <jpguilmette@gmail.com>
2017-06-29 12:53:08 -0400

    Upload V2

--- src/components/upload/upload.ts
+++ src/components/upload/upload.ts
@@ -1,27 +1,56 @@
 import Vue from 'vue';
 import { PluginObject } from 'vue';
 import Component from 'vue-class-component';
+import { Prop, Watch } from 'vue-property-decorator';
 import WithRender from './upload.html?style=./upload.scss';
 import { UPLOAD_NAME } from '../component-names';
-import Upload from 'element-ui';
 
 @WithRender
 @Component
 export class MUpload extends Vue {
-    public fileList: Object[] = [{ name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }, { name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }];
-    private componentName: string = UPLOAD_NAME;
 
-    public handleRemove(file, fileList) {
-        console.log(file, fileList);
+    @Prop({ default: true })
+    public multiple: boolean;
+
+    // Initialize data for v-model to work
+    public files: any[] = [];
+
+    private fileAPISupport: boolean = false;
+
+    public beforeCreate() {
+        // Check for the various File API support.
+        if ((window as any).File && (window as any).FileReader && (window as any).FileList && window.Blob) {
+            this.fileAPISupport = true;
+        }
     }
-    public handlePreview(file) {
-        console.log(file);
+
+    public get filesSelected() {
+        return this.files;
     }
+
+    public filesChange($event: Event) {
+        let fileList: FileList = ($event.target as any).files as FileList;
+    }
+
+    // public handleFileSelect(evt) {
+    //     let files = evt.target.files; // FileList object
+
+    //     // files is a FileList of File objects. List some properties.
+    //     let output = [];
+    //     for (let file in files) {
+    //         output.push('<li><strong>', escape(file.name), '</strong> (', file.type || 'n/a', ') - ',
+    //                 f.size, ' bytes, last modified: ',
+    //                 f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+    //                 '</li>');
+    //     }
+    //     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+    // }
+
+    // document.getElementById('files').addEventListener('change', handleFileSelect, false);
 }
 
 const UploadPlugin: PluginObject<any> = {
     install(v, options) {
-        v.use(Upload);
         v.component(UPLOAD_NAME, MUpload);
     }
 };
