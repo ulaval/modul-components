831462363d9ae540253bc3cf6862aeab130b0631
Raphaël Paré <raph.pare@outlook.com>
2017-06-29 16:22:47 -0400

    P408-3: Fixe button component

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import { PluginObject } from 'vue';
 import Component from 'vue-class-component';
-import { Prop, Watch } from 'vue-property-decorator';
+import { Prop } from 'vue-property-decorator';
 import WithRender from './button.html?style=./button.scss';
 import { BUTTON_NAME } from '../component-names';
 import { ICON_NAME } from '../component-names';
@@ -44,45 +44,25 @@ export class MButton extends Vue {
 
     public componentName: string = BUTTON_NAME;
 
-    private propsType: string = TYPE_BUTTON;
-    private propsMode: string = MODE_PRIMARY;
-    private propsState: string = STATE_DEFAULT;
-    private propsIconSize: string = ICON_SIZE;
-
     private errorMessageIcon: string = 'ERROR in <' + BUTTON_NAME + ' mode="icon"> : props "iconName" is undefined';
 
-    @Watch('mode')
-    private changeMode(newMode): void {
-        this.propsState = this.getMode(newMode);
+    private get propsType(): string {
+        return this.type != TYPE_SUBMIT && this.type != TYPE_RESET ? TYPE_BUTTON : this.type;
     }
 
-    @Watch('state')
-    private changeState(newState): void {
-        this.propsState = this.getState(newState);
+    private get propsMode(): string {
+        return this.mode != MODE_SECONDARY && this.mode != MODE_ICON ? MODE_PRIMARY : this.mode;
     }
 
-    private beforeMount(): void {
-        this.propsType = this.type != TYPE_SUBMIT && this.type != TYPE_RESET ? TYPE_BUTTON : this.type;
-        this.propsMode = this.getMode(this.mode);
-        this.propsState = this.getState(this.state);
-        if (this.propsMode == MODE_ICON) {
-            this.propsIconSize = this.iconSize == undefined ? ICON_SIZE : this.iconSize;
-        } else {
-            this.propsIconSize = this.iconSize == undefined ? ICON_SIZE_SMALL : this.iconSize;
+    private get propsState(): string {
+        return this.state != STATE_DISABLED && this.state != STATE_WAITING && this.state != STATE_SELECTED ? STATE_DEFAULT : this.state;
+    }
+
+    private get propsIconSize(): string {
+        if (this.iconSize == MODE_ICON) {
+            return this.iconSize == undefined ? ICON_SIZE : this.iconSize;
         }
-    }
-
-    private getMode(mode: string): string {
-        return mode != MODE_SECONDARY && mode != MODE_ICON ? MODE_PRIMARY : mode;
-    }
-
-    private getState(state: string): string {
-        return state != STATE_DISABLED && state != STATE_WAITING && state != STATE_SELECTED ? STATE_DEFAULT : state;
-    }
-
-    private onClick(event): void {
-        this.$emit('click');
-        this.$el.blur();
+        return this.iconSize == undefined ? ICON_SIZE_SMALL : this.iconSize;
     }
 
     private get hasIcone(): boolean {
@@ -93,6 +73,13 @@ export class MButton extends Vue {
         return this.iconPosition == ICON_POSITION_LEFT ? true : false;
     }
 
+    private get isDisabled(): boolean {
+        if (this.propsState == STATE_WAITING || this.propsState == STATE_DISABLED || this.propsState == STATE_SELECTED) {
+            return true;
+        }
+        return false;
+    }
+
     private get hasMoreInfo(): boolean {
         return !!this.$slots['more-info'];
     }
@@ -101,11 +88,9 @@ export class MButton extends Vue {
         return !!this.$slots.default;
     }
 
-    private get isDisabled(): boolean {
-        if (this.propsState == STATE_WAITING || this.propsState == STATE_DISABLED || this.propsState == STATE_SELECTED) {
-            return true;
-        }
-        return false;
+    private onClick(event): void {
+        this.$emit('click');
+        this.$el.blur();
     }
 }
 
