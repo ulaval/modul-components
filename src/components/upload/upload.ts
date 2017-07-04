81ada491727c0e3c1518d98412f8b3e1beacc400
Jean-Philippe Guilmette <jpguilmette@gmail.com>
2017-07-04 08:44:08 -0400

    Upload: Drag-Drop + new props + Error msg

--- src/components/upload/upload.ts
+++ src/components/upload/upload.ts
@@ -5,48 +5,187 @@ import { Prop, Watch } from 'vue-property-decorator';
 import WithRender from './upload.html?style=./upload.scss';
 import { UPLOAD_NAME } from '../component-names';
 
+const UNDEFINED: string = 'undefined';
+
+export interface GlobalFileList {
+    file: File;
+    isRead: boolean;
+    reader?: FileReader;
+}
+
 @WithRender
 @Component
 export class MUpload extends Vue {
 
     @Prop({ default: true })
     public multiple: boolean;
+    @Prop({ default: 10 })
+    public maxFilesAllow: number;
+    @Prop({ default: () => [] })
+    public extensions: string[];
+    @Prop({ default: 26214400 }) // 25 Mo
+    public maxSizeBytes: number;
 
-    // Initialize data for v-model to work
-    public files: any[] = [];
+    public globalFileList: GlobalFileList[] = [];
+    public errorMsgs: string[] = [];
 
     private fileAPISupport: boolean = false;
+    private extensionsProp: string;
 
-    public beforeCreate() {
+    @Watch('extensions')
+    public extensionsChange() {
+        this.extensionsProp = ((typeof this.extensions[0] == UNDEFINED) || (this.extensions[0].trim() == '') || (this.extensions[0].trim() == '*')) ? '*' : this.extensions.join();
+    }
+
+    public created() {
+        this.extensionsProp = ((typeof this.extensions[0] == UNDEFINED) || (this.extensions[0].trim() == '') || (this.extensions[0].trim() == '*')) ? '*' : this.extensions.join();
+    }
+
+    public mounted() {
         // Check for the various File API support.
         if ((window as any).File && (window as any).FileReader && (window as any).FileList && window.Blob) {
             this.fileAPISupport = true;
         }
     }
 
-    public get filesSelected() {
-        return this.files;
+    // Computed data
+    public get hasErrorMsgs(): boolean {
+        if (this.errorMsgs.length > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public filesChange($event: Event) {
-        let fileList: FileList = ($event.target as any).files as FileList;
+    public filesInput($event: Event) {
+        let newFilesAdded: boolean = false;
+
+        this.errorMsgs = [];
+
+        // FileList object.
+        for (let file of ($event.target as any).files) {
+            newFilesAdded = this.addToGlobalFilelist(file) || newFilesAdded;
+        }
+
+        if (newFilesAdded) {
+            this.readFiles();
+        }
     }
 
-    // public handleFileSelect(evt) {
-    //     let files = evt.target.files; // FileList object
+    public filesDrop($event: DragEvent) {
+        let newFilesAdded: boolean = false;
 
-    //     // files is a FileList of File objects. List some properties.
-    //     let output = [];
-    //     for (let file in files) {
-    //         output.push('<li><strong>', escape(file.name), '</strong> (', file.type || 'n/a', ') - ',
-    //                 f.size, ' bytes, last modified: ',
-    //                 f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-    //                 '</li>');
-    //     }
-    //     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-    // }
+        $event.stopPropagation();
+        $event.preventDefault();
+        this.errorMsgs = [];
 
-    // document.getElementById('files').addEventListener('change', handleFileSelect, false);
+        let files: FileList = $event.dataTransfer.files;
+
+        // FileList object.
+        for (let i = 0; i < files.length; i++) {
+            let file: File = files.item(i);
+            newFilesAdded = this.addToGlobalFilelist(file) || newFilesAdded;
+        }
+
+        if (newFilesAdded) {
+            this.readFiles();
+        }
+    }
+
+    private addToGlobalFilelist(file: File): boolean {
+        if (this.hasTooManyFiles()) {
+            this.errorMsgs.push(`File ${file.name} wasn't add - Max number of files (${this.maxFilesAllow}) allow is reach`);
+            return false;
+        }
+
+        if (this.isTooBig(file)) {
+            this.errorMsgs.push(`File ${file.name} is bigger than max size ${this.maxSizeBytes} bytes`);
+            return false;
+        }
+
+        if (this.isFileAlreadyAdded(file)) {
+            this.errorMsgs.push(`File ${file.name} is already in list`);
+            return false;
+        }
+
+        if ((this.extensionsProp != '*') && (!this.validExtension(file))) {
+            this.errorMsgs.push(`File ${file.name} doesn't have a valid extension`);
+            return false;
+        }
+
+        this.globalFileList.push({ file: file, isRead: false });
+        return true;
+    }
+
+    private hasTooManyFiles(): boolean {
+        if ((typeof this.maxFilesAllow != UNDEFINED) && (this.globalFileList.length >= this.maxFilesAllow)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private isTooBig(file: File): boolean {
+        if (file.size > this.maxSizeBytes) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private isFileAlreadyAdded(file: File): boolean {
+        let isAlreadyAdded: boolean = false;
+
+        for (let globalFile of this.globalFileList) {
+            if (encodeURI(file.name) == encodeURI(globalFile.file.name)) {
+                isAlreadyAdded = true;
+                break;
+            }
+        }
+
+        return isAlreadyAdded;
+    }
+
+    private validExtension(file: File): boolean {
+        let validExt: boolean = false;
+
+        for (let extension of this.extensions) {
+            if (file.name.substring(file.name.lastIndexOf('.')) == extension) {
+                validExt = true;
+                break;
+            }
+        }
+
+        return validExt;
+    }
+
+    private readFiles(): boolean {
+        for (let file of this.globalFileList) {
+            if (!file.isRead) {
+                file.reader = new FileReader();
+
+                // Closure to capture the file information.
+                file.reader.onload = ((file) => {
+                    return (e) => {
+                        file.isRead = true;
+                        console.log(file.file.name + ' is done');
+                    // Render thumbnail.
+                    // var span = document.createElement('span');
+                    // span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                    //                     '" title="', escape(theFile.name), '"/>'].join('');
+                    // document.getElementById('list').insertBefore(span, null);
+                    };
+                })(file);
+
+                // Read in the image file as a data URL.
+                file.reader.readAsDataURL(file.file); // Return data: URL
+                // file.reader.readAsArrayBuffer(file.file); // Return ArrayBuffer
+                // file.reader.readAsText(file.file); // Return text string
+            }
+        }
+
+        return true;
+    }
 }
 
 const UploadPlugin: PluginObject<any> = {
