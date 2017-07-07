2ed86dd4e99ecfcabb192eae2941874b818e1d2a
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2017-07-07 10:45:50 -0400

    Spinner component v1

--- src/components/spinner/spinner.ts
+++ src/components/spinner/spinner.ts
@@ -5,59 +5,16 @@ import { Prop } from 'vue-property-decorator';
 import WithRender from './spinner.html?style=./spinner.scss';
 import { SPINNER_NAME } from '../component-names';
 
-export const STATE_SUCCESS: string = 'success';
-export const STATE_INFORMATION: string = 'information';
-export const STATE_WARNING: string = 'warning';
-export const STATE_ERROR: string = 'error';
-
-export const MODE_REGULAR: string = 'regular';
-export const MODE_LIGHT: string = 'light';
+export const MODE_LOADING: string = 'loading';
+export const MODE_PROCESSING: string = 'processing';
 
 @WithRender
 @Component
 export class MSpinner extends Vue {
-    @Prop({ default: STATE_SUCCESS })
-    public state: string;
-    @Prop({ default: MODE_REGULAR })
+    @Prop({ default: MODE_PROCESSING })
     public mode: string;
-    @Prop({ default: true })
-    public icon: boolean;
-    @Prop({ default: false })
-    public closeButton: boolean;
-    @Prop({ default: true })
-    public visible: boolean;
 
     public componentName = SPINNER_NAME;
-
-    private get showCloseButton(): boolean {
-        return this.mode == MODE_REGULAR && this.closeButton;
-    }
-
-    private onClose(event): void {
-        this.$emit('close', event);
-        this.visible = false;
-    }
-
-    private getIcon(): string {
-        let icon: string = '';
-        switch (this.state) {
-            case STATE_SUCCESS:
-                icon = 'chip-check';
-                break;
-            case STATE_INFORMATION:
-                icon = 'default';
-                break;
-            case STATE_WARNING:
-                icon = 'chip-warning';
-                break;
-            case STATE_ERROR:
-                icon = 'chip-error';
-                break;
-            default:
-                break;
-        }
-        return icon;
-    }
 
 }
 
