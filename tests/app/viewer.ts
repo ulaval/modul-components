4acac5cb615573c81aa035cc42c8a4885e474bf0
Jean-Philippe Guilmette <jpguilmette@gmail.com>
2017-07-14 11:31:06 -0400

    Fix: Remove modif to viewer files

--- tests/app/viewer.ts
+++ tests/app/viewer.ts
@@ -8,27 +8,8 @@ import WithRender from './viewer.html';
 export class Viewer extends Vue {
     public tag: string = '';
 
-    public elements = [{codeSession: '201701', dateDebutSession: '2017-09-04T00:00:00-0400', dateFinSession: '2018-01-07T00:00:00-0500', annee: '2017', saison: 'HIVER'},
-                       {codeSession: '201709', dateDebutSession: '2017-09-04T00:00:00-0400', dateFinSession: '2018-01-07T00:00:00-0500', annee: '2017', saison: 'AUTOMNE'},
-                       {codeSession: '201705', dateDebutSession: '2017-09-04T00:00:00-0400', dateFinSession: '2018-01-07T00:00:00-0500', annee: '2017', saison: 'ETE'}];
-    // public elements = ['201701', '201709', '201705', 'ETE'];
-    public open: boolean = false;
-    public ext: string[] = [];
-
     public mounted() {
         this.buildTag();
-    }
-
-    public getText(element: any): string {
-        return element.saison + ' - ' + element.annee;
-    }
-
-    public alertMe($event): void {
-        console.log($event);
-    }
-
-    public change() {
-        this.ext.push('.abc');
     }
 
     @Watch('$route')
