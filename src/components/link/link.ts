22a1170a5f581e468ff56aca6e6b869113ee8b33
Martin Simard <martin.simard@dti.ulaval.ca>
2017-08-02 10:48:32 -0400

    Link - consts to enum; convert initialization code to computed values

--- src/components/link/link.ts
+++ src/components/link/link.ts
@@ -5,18 +5,20 @@ import { Prop } from 'vue-property-decorator';
 import WithRender from './link.html?style=./link.scss';
 import { LINK_NAME } from '../component-names';
 
-const MODE_ROUTER_LINK: string = 'router-link';
-const MODE_EXTERNAL_LINK: string = 'external-link';
-const MODE_LINK: string = 'link';
-const MODE_BUTTON: string = 'button';
+export enum MLinkMode {
+    RouterLink = 'router-link',
+    ExternalLink = 'external-link',
+    Link = 'link',
+    Button = 'button'
+}
 
 @WithRender
 @Component
 export class MLink extends ModulVue {
     @Prop({ default: '/' })
     public url: string;
-    @Prop({ default: MODE_ROUTER_LINK })
-    public mode: string;
+    @Prop({ default: MLinkMode.RouterLink })
+    public mode: MLinkMode;
     @Prop({ default: false })
     public unvisited: boolean;
     @Prop({ default: true })
@@ -26,29 +28,6 @@ export class MLink extends ModulVue {
 
     public componentName: string = LINK_NAME;
 
-    private propsUrl: string;
-    private isRouterLink: boolean = false;
-    private isLink: boolean = false;
-    private isExternalLink: boolean = false;
-    private isButton: boolean = false;
-    private hrefAttribute: string;
-
-    protected beforeMount(): void {
-        this.hrefAttribute = this.url;
-        switch (this.mode) {
-            case MODE_EXTERNAL_LINK:
-                this.isExternalLink = true;
-                break;
-            case MODE_LINK:
-                this.isLink = true;
-                break;
-            case MODE_BUTTON:
-                this.isButton = true;
-                break;
-            default:
-                this.isRouterLink = true;
-        }
-    }
     protected mounted(): void {
         if (this.isExternalLink) {
             this.$refs['link']['setAttribute']('title', this.$i18n.translate('m-link:open-new-tab'));
@@ -64,8 +43,24 @@ export class MLink extends ModulVue {
         this.$emit('click');
     }
 
+    private get isRouterLink(): boolean {
+        return this.mode == MLinkMode.RouterLink || (this.mode != MLinkMode.Button && this.mode != MLinkMode.ExternalLink && this.mode != MLinkMode.Link);
+    }
+
+    private get isLink(): boolean {
+        return this.mode == MLinkMode.Link;
+    }
+
+    private get isExternalLink(): boolean {
+        return this.mode == MLinkMode.ExternalLink;
+    }
+
+    private get isButton(): boolean {
+        return this.mode == MLinkMode.Button;
+    }
+
     private get propUrl(): string {
-        return this.mode == MODE_BUTTON ? '#' : this.url ;
+        return this.mode == MLinkMode.Button ? '#' : this.url;
     }
 
     private get hasIconeLeft(): boolean {
