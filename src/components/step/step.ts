0e035cfb6ee6a76c326adeeb3d05aec5b483a048
Raphaël Paré <raph.pare@outlook.com>
2017-08-24 17:08:51 -0400

    Update step

--- src/components/step/step.ts
+++ src/components/step/step.ts
@@ -5,6 +5,7 @@ import { Prop, Watch } from 'vue-property-decorator';
 import WithRender from './step.html?style=./step.scss';
 import { STEP_NAME } from '../component-names';
 import { TransitionAccordion, TransitionAccordionMixin } from '../../mixins/transition-accordion/transition-accordion';
+import ElementQueries from 'css-element-queries/src/ElementQueries';
 
 export enum MStepState {
     Locked = 'locked',
@@ -12,6 +13,11 @@ export enum MStepState {
     Success = 'success',
     Warning = 'warning',
     Error = 'error'
+}
+
+export enum MStepMode {
+    Default = 'default',
+    Accordion = 'accordion'
 }
 
 @WithRender
@@ -23,6 +29,8 @@ export enum MStepState {
 export class MStep extends ModulVue {
     @Prop({ default: MStepState.Locked })
     public state: MStepState;
+    @Prop({ default: MStepMode.Default })
+    public mode: MStepMode;
     @Prop()
     public open: boolean;
     @Prop({ default: false })
@@ -33,7 +41,8 @@ export class MStep extends ModulVue {
     public last: boolean;
 
     public componentName = STEP_NAME;
-    private propOpen: boolean = false;
+    private internalOpen: boolean = false;
+    private internalMode: MStepMode | string = MStepMode.Default;
 
     @Watch('open')
     protected openChanged(open: boolean): void {
@@ -42,21 +51,37 @@ export class MStep extends ModulVue {
     }
 
     protected mounted(): void {
-        this.propOpen = this.open != undefined ? this.open : false;
+        this.propOpen = this.open;
+        this.propMode = this.mode;
+        ElementQueries.init();
     }
 
     private openStep(event): void {
-        this.as<TransitionAccordionMixin>().isAnimActive = true;
-        this.propOpen = true;
-        this.$emit('openStep', event);
-        event.preventDefault();
+        if (this.propMode == MStepMode.Accordion) {
+            this.as<TransitionAccordionMixin>().isAnimActive = true;
+            this.propOpen = !this.propOpen;
+            event.currentTarget.blur();
+            if (this.propOpen) {
+                this.$emit('openStep', event);
+            } else {
+                this.$emit('closeStep', event);
+            }
+        } else {
+            if (!this.propOpen) {
+                this.as<TransitionAccordionMixin>().isAnimActive = true;
+                this.propOpen = true;
+                this.$emit('openStep', event);
+                event.currentTarget.blur();
+            }
+        }
     }
 
-    private closeStep(event): void {
-        this.as<TransitionAccordionMixin>().isAnimActive = true;
-        this.propOpen = false;
-        this.$emit('closeStep', event);
-        event.preventDefault();
+    private closeStep(): void {
+        if (this.propOpen) {
+            this.as<TransitionAccordionMixin>().isAnimActive = true;
+            this.propOpen = false;
+            this.$emit('closeStep');
+        }
     }
 
     private getIcon(): string {
@@ -78,6 +103,36 @@ export class MStep extends ModulVue {
                 break;
         }
         return icon;
+    }
+
+    private get propOpen(): boolean {
+        return this.internalOpen;
+    }
+
+    private set propOpen(open: boolean) {
+        this.internalOpen = open != undefined ? open : false;
+        let refHeader: HTMLElement = this.$refs.header as HTMLElement;
+        if (this.propMode == MStepMode.Accordion) {
+            if (!refHeader.hasAttribute('tabindex')) {
+                refHeader.setAttribute('tabindex', '0');
+            }
+        } else {
+            if (this.internalOpen) {
+                if (refHeader.hasAttribute('tabindex')) {
+                    refHeader.removeAttribute('tabindex');
+                }
+            } else {
+                refHeader.setAttribute('tabindex', '0');
+            }
+        }
+    }
+
+    private get propMode(): MStepMode | string {
+        return this.internalMode;
+    }
+
+    private set propMode(mode: MStepMode | string) {
+        this.internalMode = mode == undefined || mode == '' ? MStepMode.Default : mode;
     }
 }
 
