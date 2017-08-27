5ffcafceb1255a146046d5bc731864d58a138ec1
Raphaël Paré <raph.pare@outlook.com>
2017-08-27 15:47:51 -0400

    Update media queries

--- src/utils/media-queries/media-queries.ts
+++ src/utils/media-queries/media-queries.ts
@@ -2,71 +2,26 @@ import Vue, { PluginObject } from 'vue';
 import * as enquire from 'enquire.js/dist/enquire';
 
 export enum MediaQueriesBpMin {
-    XL = 'screen and (min-width:1600px)',
-    L = 'screen and (min-width:1200px)',
-    M = 'screen and (min-width:1024px)',
-    S = 'screen and (min-width:768px)',
-    XS = 'screen and (min-width:480px)'
+    XL = '(min-width: 1600px)',
+    L = '(min-width: 1200px)',
+    M = '(min-width: 1024px)',
+    S = '(min-width: 768px)',
+    XS = '(min-width: 480px)'
 }
 
 export enum MediaQueriesBpMax {
-    XL = 'screen and (max-width:1599px)',
-    L = 'screen and (max-width:1199px)',
-    M = 'screen and (max-width:1023px)',
-    S = 'screen and (max-width:767px)',
-    XS = 'screen and (max-width:479px)'
+    XL = '(max-width: 1599px)',
+    L = '(max-width: 1199px)',
+    M = '(max-width: 1023px)',
+    S = '(max-width: 767px)',
+    XS = '(max-width: 479px)'
 }
 
 export enum MediaQueriesBp {
-    L = 'screen and (min-width:1024px) and (max-width:1599px)',
-    M = 'screen and (min-width:768px) and (max-width:1023px)',
-    S = 'screen and (min-width:480px) and (max-width:767px)'
+    L = '(min-width: 1024px) and (max-width:1 599px)',
+    M = '(min-width: 768px) and (max-width: 1023px)',
+    S = '(min-width: 480px) and (max-width: 767px)'
 }
-
-// MIN-WIDTH
-
-// 480px >= ...
-export const SCREEN_MIN_W_XS: string = 'screen and (min-width:480px)';
-
-// 768 >= ...
-export const SCREEN_MIN_W_S: string = 'screen and (min-width:768px)';
-
-// 1024 >= ...
-export const SCREEN_MIN_W_M: string = 'screen and (min-width:1024px)';
-
-// 1200 >= ...
-export const SCREEN_MIN_W_L: string = 'screen and (min-width:1200px)';
-
-// MAX-WIDTH
-
-// ... < 768
-export const SCREEN_MAX_W_S: string = 'screen and (max-width:767px)';
-
-// ... < 1024
-export const SCREEN_MAX_W_M: string = 'screen and (max-width:1023px)';
-
-// ... < 1200px
-export const SCREEN_MAX_W_L: string = 'screen and (max-width:1199px)';
-
-// ... < 1600
-export const SCREEN_MAX_W_XL: string = 'screen and (max-width:1599px)';
-
-// MIN-WIDTH and MAX-WIDTH
-
-// ... < 480
-export const SCREEN_W_XS: string = 'screen and (max-width:479px)';
-
-// 480 =­> ... < 768
-export const SCREEN_W_S: string = 'screen and (min-width:480px) and (max-width:767px)';
-
-// 768 => ... < 1024
-export const SCREEN_W_M: string = 'screen and (min-width:768px) and (max-width:1023px)';
-
-// 1024 => ... < 1600
-export const SCREEN_W_L: string = 'screen and (min-width:1024px) and (max-width:1599px)';
-
-// 1600 >= ...
-export const SCREEN_W_XL: string = 'screen and (min-width:1600px)';
 
 const MATCH: string = 'match-';
 const UNMATCH: string = 'unmatch-';
