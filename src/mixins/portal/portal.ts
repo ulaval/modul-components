6c9ac1c5f6ca1bdd54901ed9311bde17a1f39853
Raphaël Paré <raph.pare@outlook.com>
2017-09-08 12:07:25 -0400

    Create portal mixin

---
+++ src/mixins/portal/portal.ts
@@ -0,0 +1,68 @@
+import { ModulVue } from '../../utils/vue/vue';
+import Component from 'vue-class-component';
+import uuid from '../../utils/uuid/uuid';
+
+export interface PortalMixin {
+    portalEl: HTMLElement;
+    portalId: string;
+    ceratePortalEl: Function;
+    createBackdropEl: Function;
+    appendPortalToBody: Function;
+    appendBackdropAndPortalToBody: Function;
+    removePortal: Function;
+    removeBackdropAndPortal: Function;
+}
+
+@Component
+export class Portal extends ModulVue implements PortalMixin {
+    public portalEl: HTMLElement;
+    public portalId: string;
+
+    public ceratePortalEl(id: string, className: string): void {
+        this.portalEl = document.createElement('div');
+        this.portalId = id + '-' + uuid.generate();
+        this.portalEl.setAttribute('id', this.portalId);
+        this.portalEl.setAttribute('class', className);
+        this.portalEl.style.position = 'absolute';
+    }
+
+    public createBackdropEl(transitionDuration: string = '0.3s'): void {
+        if (!this.$mWindow.hasBackdrop) {
+            this.$mWindow.createBackdrop(this.$mWindow.bodyEl);
+        }
+        this.$mWindow.setBackdropTransitionDuration(transitionDuration);
+    }
+
+    public appendBackdropAndPortalToBody(portalId: string, portalClassName: string, backdropTransitionDuration: string): void {
+        this.ceratePortalEl(portalId, portalClassName);
+        this.$mWindow.addWindow(this.portalId);
+        this.portalEl.style.zIndex = String(this.$mWindow.windowZIndex);
+        this.createBackdropEl(backdropTransitionDuration);
+        this.$mWindow.bodyEl.appendChild(this.portalEl);
+    }
+
+    public appendPortalToBody(id: string, className: string): void {
+        this.ceratePortalEl(id, className);
+        this.portalEl.style.zIndex = String(this.$mWindow.windowZIndex);
+        this.$mWindow.bodyEl.appendChild(this.portalEl);
+    }
+
+    public removePortal(): void {
+        let portalEl: HTMLElement = this.getPotalEl();
+        if (portalEl) {
+            this.$mWindow.bodyEl.removeChild(portalEl);
+        }
+    }
+
+    public removeBackdropAndPortal(): void {
+        let portalEl: HTMLElement = this.getPotalEl();
+        if (portalEl) {
+            this.$mWindow.bodyEl.removeChild(portalEl);
+            this.$mWindow.deleteWindow(this.portalId);
+        }
+    }
+
+    private getPotalEl(): HTMLElement {
+        return this.$mWindow.bodyEl.querySelector('#' + this.portalId) as HTMLElement;
+    }
+}
