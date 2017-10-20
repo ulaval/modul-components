3b8bea1d89c5b5110722fd46e2863db7ab508088
Raphaël Paré <raph.pare@outlook.com>
2017-10-20 16:36:34 -0400

    Update focus on button component

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -55,24 +55,27 @@ export class MButton extends Vue {
     public fullSize: boolean;
 
     public componentName: string = BUTTON_NAME;
+    private focus: boolean = false;
 
     private errorMessageIcon: string = 'ERROR in <' + BUTTON_NAME + ' mode="icon"> : props "icon-name" is undefined';
 
     private onClick(event): void {
+        this.focus = false;
         this.$emit('click', event);
-        this.onBlur(event, false);
     }
 
     private onFocus(event) {
+        this.focus = true;
         this.$emit('focus', event);
     }
 
-    private onBlur(event, emit: boolean = true): void {
-        if (emit) {
-            this.$emit('blur', event);
-        } else {
-            this.$el.blur();
-        }
+    private onBlur(event): void {
+        this.focus = false;
+        this.$emit('blur', event);
+    }
+
+    private onKeydown(event): void {
+        this.$emit('keydown', event);
     }
 
     private get propType(): string {
