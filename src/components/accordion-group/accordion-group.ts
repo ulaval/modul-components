e06e131751fdeae66fa3fb4471b8d5a6a856a274
Unknown <carl.boulianne-boivin@dti.ulaval.ca>
2017-10-20 14:47:40 -0400

    accordion & accordion-group : refactor, added close all button

--- src/components/accordion-group/accordion-group.ts
+++ src/components/accordion-group/accordion-group.ts
@@ -10,54 +10,44 @@ import { MAccordion, MAccordionSkin } from '../accordion/accordion';
 @WithRender
 @Component
 export class MAccordionGroup extends ModulVue {
-    @Prop()
+
+    @Prop({ default: MAccordionSkin.Regular })
     public skin: MAccordionSkin;
+
     @Prop({ default: false })
     public concurrent: boolean;
+
     @Prop({ default: false })
     public allOpen: boolean;
+
     @Prop()
     public value: string;
 
     public componentName: string = ACCORDION_GROUP_NAME;
 
-    private nbAccordion: number = 0;
-    private arrAccordion = new Array();
+    private arrAccordion: MAccordion[] = new Array();
     private nbAccordionOpen: number = 0;
     private indexAccordionOpen: number | undefined = undefined;
 
     private hasError: boolean = false;
     private errorDefaultMesage: string = 'ERROR in <' + ACCORDION_GROUP_NAME + '> : ';
     private errorMessage: string = '';
-    private internalPropAllOpen: boolean = false;
 
     protected mounted(): void {
-        this.concurrent = this.concurrent;
         for (let i = 0; i < this.$children.length; i++) {
             if (this.checkAccordion(i)) {
                 let accordion: MAccordion = this.$children[i] as MAccordion;
-                accordion.id = this.nbAccordion;
-                accordion.$on('click', (id: number, open: boolean) => this.toggleAccordionGroup(id, open));
-                this.arrAccordion.push({
-                    id: this.nbAccordion,
-                    childrenNumber: i,
-                    open: false
-                });
+                accordion.id = i;
+                accordion.$on('click', (open: boolean) => this.toggleAccordionGroup(open));
+                this.arrAccordion.push(accordion);
                 if (accordion.isOpen) {
                     if (this.concurrent) {
-                        this.indexAccordionOpen = this.nbAccordion;
+                        this.indexAccordionOpen = i;
                         accordion.isOpen = false;
-                        this.arrAccordion[this.nbAccordion].open = false;
                     } else {
                         this.nbAccordionOpen++;
-                        this.arrAccordion[this.nbAccordion].open = true;
                     }
                 }
-                if (this.propSkin != accordion.propSkin) {
-                    accordion.propSkin = this.propSkin;
-                    accordion.setSkin();
-                }
-                this.nbAccordion++;
             }
         }
         if (this.concurrent) {
@@ -66,14 +56,14 @@ export class MAccordionGroup extends ModulVue {
         if (this.propAllOpen && !this.concurrent) {
             this.openAllAccordions(false);
         }
-        if (this.nbAccordion == 0) {
+        if (this.arrAccordion.length == 0) {
             this.hasError = true;
             this.errorMessage = this.errorDefaultMesage + 'No <' + ACCORDION_NAME + '> found in <' + ACCORDION_GROUP_NAME + '>';
             console.error(this.errorMessage);
         }
     }
 
-    private toggleAccordionGroup(accordionID: number, open: boolean): void {
+    private toggleAccordionGroup(open: boolean): void {
         if (this.concurrent) {
             this.closeAllAccordions(true);
             if (open) {
@@ -85,13 +75,12 @@ export class MAccordionGroup extends ModulVue {
             } else {
                 this.nbAccordionOpen--;
             }
-            this.propAllOpen = this.nbAccordionOpen == this.nbAccordion ? true : false;
         }
     }
 
     private openAccordionConcurrent(): void {
         if (this.indexAccordionOpen != undefined) {
-            (this.$children[this.arrAccordion[this.indexAccordionOpen].childrenNumber] as MAccordion).isOpen = true;
+            this.arrAccordion[this.indexAccordionOpen].isOpen = true;
             this.nbAccordionOpen = 1;
         } else {
             this.nbAccordionOpen = 0;
@@ -103,45 +92,25 @@ export class MAccordionGroup extends ModulVue {
     }
 
     private openAllAccordions(isAnimActive: boolean = true): void {
-        this.propAllOpen = true;
-        this.nbAccordionOpen = this.nbAccordion;
-        for (let i = 0; i < this.$children.length; i++) {
-            let accordion: MAccordion = this.$children[i] as MAccordion;
-            if (i < this.arrAccordion.length) {
-                this.arrAccordion[i].open = true;
-            }
-            if (this.checkAccordion(i)) {
-                accordion.setIsAnimActive(isAnimActive);
-                accordion.isOpen = true;
-            }
-        }
+        this.nbAccordionOpen = this.arrAccordion.length;
+        this.arrAccordion.forEach(el => {
+            el.isOpen = true;
+        });
     }
 
     private closeAllAccordions(isAnimActive: boolean = true): void {
-        this.propAllOpen = false;
         this.nbAccordionOpen = 0;
-        for (let i = 0; i < this.$children.length; i++) {
-            let accordion: MAccordion = this.$children[i] as MAccordion;
-            if (i < this.arrAccordion.length) {
-                this.arrAccordion[i].open = false;
-            }
-            if (this.checkAccordion(i)) {
-                accordion.setIsAnimActive(isAnimActive);
-                accordion.isOpen = false;
-            }
-        }
+        this.arrAccordion.forEach(el => {
+            el.isOpen = false;
+        });
     }
 
     private get propAllOpen(): boolean {
-        return this.internalPropAllOpen == undefined ? this.allOpen : this.internalPropAllOpen;
+        return this.nbAccordionOpen == this.arrAccordion.length;
     }
 
-    private set propAllOpen(value: boolean) {
-        this.internalPropAllOpen = value;
-    }
-
-    private get propSkin(): MAccordionSkin {
-        return this.skin == MAccordionSkin.Light || this.skin == MAccordionSkin.Vanilla ? this.skin : MAccordionSkin.Regular;
+    private get propAllClosed(): boolean {
+        return this.nbAccordionOpen == 0;
     }
 
     private get hasTitleSlot(): boolean {
