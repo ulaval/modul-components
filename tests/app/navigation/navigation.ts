b426b4a8f9964e696c9e9053f767106c379a8270
Martin Simard <martin.simard@dti.ulaval.ca>
2017-10-30 10:32:16 -0400

    removed test data from pull request

--- tests/app/navigation/navigation.ts
+++ tests/app/navigation/navigation.ts
@@ -3,56 +3,10 @@ import Component from 'vue-class-component';
 import WithRender from './navigation.html';
 import Meta from '../../../src/meta/meta';
 
-class Choice {
-    public selection: any = '';
-
-    constructor() {
-        setInterval(() => {
-            // console.log(this.selection);
-            // console.log(document.activeElement);
-        }, 3000);
-    }
-}
-
 @WithRender
 @Component
 export class Navigation extends ModulVue {
     public routes: string[] = [];
-
-    private items0: string[] = [];
-    private items: string[] = 'item 1,item 2,item 3,item 4,opt 1,opt 2, opt 3'.split(',');
-    private itemsO: any[] = [
-        { v: 'i1', text: 'item o 1' },
-        { v: 'i2', text: 'item o 2' },
-        { v: 'i3', text: 'item o 3' },
-        { v: '01', text: 'opt o 1' },
-        { v: '02', text: 'opt o 2' }
-    ];
-    private items2: string[] = 'item A,item B,item C,item D,item E,item À'.split(',');
-    private items3: string[] = 'item Alpha,item Bravo'.split(',');
-    private myObj: Choice = new Choice();
-    private t: string = '';
-    private o: boolean = false;
-
-    private template: object = {
-        template: `<div>
-    <m-radio-group ref="g" :position="position" >
-        <m-radio ref="a" position="left" value="radio1">Item 1</m-radio>
-        <m-radio ref="b" position="right" value="radio2">Item 2</m-radio>
-        <m-radio ref="c" value="radio3">Item 3</m-radio>
-    </m-radio-group>
-</div>`,
-        data: function() {
-            return {
-                position: 'right'
-            };
-        },
-        methods: {
-            changePosition: function() {
-                this.position = 'right';
-            }
-        }
-    };
 
     public mounted(): void {
         let meta: string[] = [];
@@ -60,15 +14,5 @@ export class Navigation extends ModulVue {
             meta.push(tag);
         });
         this.routes = meta;
-
-        this.myObj.selection = 'item 3';
-    }
-
-    private getLabel(item: string): string {
-        return `--(${item})--`;
-    }
-
-    private onClick(): void {
-        this.$confirm('question?').then(() => console.log('ok!!!')).catch(() => console.log('cancel!!!'));
     }
 }
