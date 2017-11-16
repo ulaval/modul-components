1d195b59a1a13b5f3ebf98fa4b4be169eaf967f8
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2017-11-16 14:12:27 -0500

    M-link: add dynamic route support

--- src/components/link/link.ts
+++ src/components/link/link.ts
@@ -25,7 +25,14 @@ const ICON_NAME_DEFAULT: string = 'right-arrow';
 export class MLink extends ModulVue {
     @Prop({ default: '/' })
     public url: string;
-    @Prop({ default: MLinkMode.RouterLink })
+    @Prop({
+        default: MLinkMode.RouterLink,
+        validator: value =>
+            value == MLinkMode.RouterLink ||
+            value == MLinkMode.ExternalLink ||
+            value == MLinkMode.Link ||
+            value == MLinkMode.Button
+    })
     public mode: MLinkMode;
     @Prop()
     public unvisited: boolean;
@@ -39,7 +46,12 @@ export class MLink extends ModulVue {
     public icon: boolean;
     @Prop()
     public iconName: string;
-    @Prop({ default: MLinkIconPosition.Left })
+    @Prop({
+        default: MLinkIconPosition.Left,
+        validator: value =>
+            value == MLinkIconPosition.Left ||
+            value == MLinkIconPosition.Right
+    })
     public iconPosition: string;
     @Prop({ default: '12px' })
     public iconSize: string;
@@ -64,7 +76,7 @@ export class MLink extends ModulVue {
     }
 
     private get isRouterLink(): boolean {
-        return this.mode == MLinkMode.RouterLink || (this.mode != MLinkMode.Button && this.mode != MLinkMode.ExternalLink && this.mode != MLinkMode.Link);
+        return this.mode == MLinkMode.RouterLink;
     }
 
     private get isLink(): boolean {
@@ -101,6 +113,14 @@ export class MLink extends ModulVue {
 
     private get hasHiddenText(): boolean {
         return this.hiddenText == undefined || this.hiddenText == '' ? false : true;
+    }
+
+    private get rooterLinkUrl(): string | Object {
+        return !this.isObject(this.url) ? { path: this.url } : this.url ;
+    }
+
+    private isObject(a) {
+        return (!!a) && (a.constructor === Object);
     }
 }
 
