fe9adf0ad345173c73d932478f0091bc94d4a056
Martin Simard <martin.simard@dti.ulaval.ca>
2017-11-22 14:05:29 -0500

    apply tests to all .spec files

--- tests/unit.js
+++ tests/unit.js
@@ -10,7 +10,7 @@ Function.prototype.bind = require("function-bind");
 
 // require all test files (files that ends with .spec.js)
 // var testsContext = crequire.context("../src", true, /\.spec$/);
-var testsContext = crequire.context("../src", true, /validation-message\.spec$/);
+var testsContext = crequire.context("../src", true, /\.spec$/);
 testsContext.keys().forEach(testsContext);
 
 // require all src files except main.js and *.d.ts for coverage.
