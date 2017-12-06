7e1f6de2b59718a7fc67627545a15683d465d8b3
Martin Simard <martin.simard@dti.ulaval.ca>
2017-12-06 08:29:28 -0500

    Stylelint emits error

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -111,7 +111,7 @@ module.exports = function (env) {
         plugins: [
             new StyleLintPlugin({
                 configFile: '.stylelintrc',
-                emitErrors: false
+                emitErrors: true
             }),
             new ContextReplacementPlugin(
                 /moment[\/\\]locale$/,
