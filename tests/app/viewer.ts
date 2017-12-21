1c7043725577fd222dfcc51c9f6c45cfe01b4032
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2017-12-21 12:43:24 -0500

    Remove crapy code

--- tests/app/viewer.ts
+++ tests/app/viewer.ts
@@ -8,15 +8,9 @@ import WithRender from './viewer.html';
 @Component
 export class Viewer extends Vue {
     public tag: string = '';
-    public menuOpen: boolean = true;
-    private a: string = 'test 6';
 
     public mounted() {
         this.buildTag();
-    }
-
-    public toggleMenu(): void {
-        this.menuOpen = !this.menuOpen;
     }
 
     @Watch('$route')
