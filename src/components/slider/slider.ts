a036fd01eaf710e683c5a02cb38e8f4820bcedc0
Raphaël Paré <raph.pare@outlook.com>
2018-01-19 16:22:44 -0500

    Update m-slider

--- src/components/slider/slider.ts
+++ src/components/slider/slider.ts
@@ -1,14 +1,14 @@
 import { ModulVue } from '../../utils/vue/vue';
 import { PluginObject } from 'vue';
 import Component from 'vue-class-component';
-import { Prop } from 'vue-property-decorator';
+import { Prop, Watch, Model } from 'vue-property-decorator';
 import WithRender from './slider.html?style=./slider.scss';
 import { SLIDER_NAME } from '../component-names';
 
 @WithRender
 @Component
 export class MSlider extends ModulVue {
-
+    @Model('change')
     @Prop({ default: 50 })
     public value: number;
     @Prop({ default: 0 })
@@ -18,18 +18,32 @@ export class MSlider extends ModulVue {
     @Prop({ default: 1 })
     public step: number;
 
-    private selectedValue: number = this.value;
+    private internalValue: number = 50;
     private range: number = this.max - this.min;
     private offsetLeft: number;
     private offsetRatio: number;
 
     protected mounted(): void {
-        this.offsetLeft = this.$refs['m-slider']['offsetLeft'];
-        this.offsetRatio = this.range / this.$refs['m-slider']['clientWidth'];
+        this.offsetLeft = this.$el.offsetLeft;
+        this.offsetRatio = this.range / this.$el.clientWidth;
+        this.model = this.value;
+    }
+
+    @Watch('value')
+    private onValueChange(value: any) {
+        this.model = value;
+    }
+
+    private get model(): number {
+        return this.internalValue;
+    }
+
+    private set model(value: number) {
+        this.internalValue = value;
     }
 
     private get percentage(): number {
-        return ((this.selectedValue - this.min) / this.range) * 100;
+        return ((this.model - this.min) / this.range) * 100;
     }
 
     private computeNearestStep(position: number): number {
@@ -37,17 +51,17 @@ export class MSlider extends ModulVue {
     }
 
     private onClick(event: MouseEvent): void {
-        this.selectedValue = Math.max(this.min, Math.min(this.max, this.computeNearestStep(event.clientX)));
-        this.$emit('change', this.selectedValue);
+        this.model = Math.max(this.min, Math.min(this.max, this.computeNearestStep(event.clientX)));
+        this.$emit('change', this.model);
     }
 
     private onPanmove(event): void {
-        this.selectedValue = Math.max(this.min, Math.min(this.max, this.computeNearestStep(event.center.x)));
-        this.$emit('slide', this.selectedValue);
+        this.model = Math.max(this.min, Math.min(this.max, this.computeNearestStep(event.center.x)));
+        this.$emit('change', this.model);
     }
 
     private onPanend(): void {
-        this.$emit('change', this.selectedValue);
+        this.$emit('change', this.model);
     }
 
     private onKeydown(event: KeyboardEvent): void {
@@ -59,15 +73,15 @@ export class MSlider extends ModulVue {
                 this.increment(this.step);
                 break;
         }
-        this.$emit('change', this.selectedValue);
+        this.$emit('change', this.model);
     }
 
     private decrement(value: number): void {
-        this.selectedValue = Math.max(this.min, this.selectedValue - value);
+        this.model = Math.max(this.min, this.internalValue - value);
     }
 
     private increment(value: number): void {
-        this.selectedValue = Math.min(this.max, this.selectedValue + value);
+        this.model = Math.min(this.max, this.internalValue + value);
     }
 }
 
