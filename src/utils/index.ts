27f1f9128c90790af70f2fa5c121d6d49447f197
Jean-Francois Nadeau <jean-francois.nadeau@dti.ulaval.ca>
2018-02-14 10:25:27 -0500

    Fix missing import

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,12 +4,12 @@ import MediaQueriesPlugin from './media-queries/media-queries';
 import ModulPlugin from './modul/modul';
 import MessagesPlugin from './i18n/i18n';
 import HttpPlugin from './http/http';
-import PortalPlugin from 'portal-vue';
 import SecurityPlugin, { SecurityPluginOptions } from './http/security';
 import SpritesPlugin from './svg/sprites';
 import * as TouchPlugin from 'vue-touch';
 import ConfirmPlugin from './modal/confirm';
 import AlertPlugin from './modal/alert';
+import { PortalPluginInstall } from 'portal-vue';
 
 export interface UtilsPluginOptions {
     securityPluginOptions: SecurityPluginOptions;
