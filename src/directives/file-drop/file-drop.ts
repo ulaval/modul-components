f6266790b860e32c1943bd44a427a7fcb2ce7d6a
Jean-Francois Nadeau <jean-francois.nadeau@dti.ulaval.ca>
2018-02-19 08:03:37 -0500

    file-drop

---
+++ src/directives/file-drop/file-drop.ts
@@ -0,0 +1,68 @@
+import { DirectiveOptions, VNodeDirective, VNode, PluginObject } from 'vue';
+import { ModulVue } from '../../utils/vue/vue';
+import { FileService } from '../../utils/file/file';
+import { FILE_DROP_NAME } from '../directive-names';
+
+interface MFileDropElement extends HTMLElement {
+    cleanupMFileDropDirective();
+}
+
+const MFileDropDirective: DirectiveOptions = {
+    bind(
+        el: MFileDropElement,
+        binding: VNodeDirective,
+        vnode: VNode,
+        oldVnode: VNode
+    ) {
+        const $file: FileService = (vnode.context as ModulVue).$file;
+
+        const onDragEnterOver = (e: DragEvent) => {
+            el.classList.add('m--is-drag-over');
+            e.stopPropagation();
+            e.preventDefault();
+        };
+
+        const onDragLeave = (e: DragEvent) => {
+            el.classList.remove('m--is-drag-over');
+            e.stopPropagation();
+            e.preventDefault();
+        };
+
+        const onDrop = (e: DragEvent) => {
+            el.classList.remove('m--is-drag-over');
+            e.preventDefault();
+            $file.add(e.dataTransfer.files);
+        };
+
+        const cleanup = () => {
+            el.removeEventListener('dragenter', onDragEnterOver);
+            el.removeEventListener('dragover', onDragEnterOver);
+            el.removeEventListener('dragleave', onDragLeave);
+            el.removeEventListener('drop', onDrop);
+        };
+
+        el.addEventListener('dragenter', onDragEnterOver);
+        el.addEventListener('dragover', onDragEnterOver);
+        el.addEventListener('dragleave', onDragLeave);
+        el.addEventListener('drop', onDrop);
+        el.cleanupMFileDropDirective = cleanup;
+    },
+    unbind(
+        el: MFileDropElement,
+        binding: VNodeDirective,
+        vnode: VNode,
+        oldVnode: VNode
+    ) {
+        el.cleanupMFileDropDirective();
+        const $file: FileService = (vnode.context as ModulVue).$file;
+        $file.clear();
+    }
+};
+
+const FileDropPlugin: PluginObject<any> = {
+    install(v, options) {
+        v.directive(FILE_DROP_NAME, MFileDropDirective);
+    }
+};
+
+export default FileDropPlugin;
