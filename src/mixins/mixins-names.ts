de61d5f648d8d3d1f668c38fe312182d80fa792a
Martin Simard <simardo@gmail.com>
2018-02-21 13:37:27 -0500

    Fix portal-vue version; remove mixins-names (moved to website) (#79)

--- src/mixins/mixins-names.ts
+++
@@ -1,9 +0,0 @@
-// necessary because of minification, should match class names
-export const ELEMENT_QUERIES: string = 'ElementQueries';
-export const INPUT_MANAGEMENT: string = 'InputManagement';
-export const INPUT_POPUP: string = 'InputPopup';
-export const INPUT_STATE: string = 'InputState';
-export const MEDIA_QUERIES: string = 'MediaQueries';
-export const OPEN_TRIGGER: string = 'OpenTrigger';
-export const PORTAL: string = 'Portal';
-export const TRANSITION_ACCORDION: string = 'TransitionAccordion';
