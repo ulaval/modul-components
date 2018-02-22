eb5275ca836ea28292b3e6ebc9cf74c49aa675b4
Jean-Francois Nadeau <jean-francois.nadeau@dti.ulaval.ca>
2018-02-22 13:57:08 -0500

    test

--- tests/app/viewer.ts
+++ tests/app/viewer.ts
@@ -1,17 +1,45 @@
+<<<<<<< HEAD
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { Watch } from 'vue-property-decorator';
 
+=======
+import Component from 'vue-class-component';
+import { Watch } from 'vue-property-decorator';
+
+import { ModulVue } from '../../src/utils/vue/vue';
+>>>>>>> test
 import WithRender from './viewer.html';
 
 // import { ModulVue } from '../../utils/vue/vue';
 @WithRender
 @Component
-export class Viewer extends Vue {
+export class Viewer extends ModulVue {
     public tag: string = '';
 
     public mounted(): void {
         this.buildTag();
+    }
+
+    private onFilesReady(files) {
+        files.forEach(f => {
+            const fd = new FormData();
+            fd.append('file', f.file);
+            this.$file.upload(f.uid, {
+                url: 'http://localhost:8989/upload',
+                config: {
+                    data: fd
+                }
+            });
+        });
+    }
+
+    private onFileUploadCancel(file) {
+        this.$file.cancelUpload(file.uid);
+    }
+
+    private onFilesUploadCompleted(files) {
+        // Do something with uploaded files !
     }
 
     @Watch('$route')
