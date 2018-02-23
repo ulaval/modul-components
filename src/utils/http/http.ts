e1f9aded3c9d2cc98ac9f4284014ab76d7326942
Martin Simard <martin.simard@dti.ulaval.ca>
2018-02-23 13:42:24 -0500

    use axios instance instead of static class

--- src/utils/http/http.ts
+++ src/utils/http/http.ts
@@ -1,29 +1,71 @@
 import { PluginObject } from 'vue';
-import axios, { AxiosRequestConfig, AxiosPromise, AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosPromise, AxiosResponse } from 'axios';
 import qs from 'qs/lib';
 import { RestAdapter, RequestConfig } from './rest';
 
 export class HttpService implements RestAdapter {
+    private instance: AxiosInstance;
+    private responseInterceptor: number | undefined;
+
     constructor() {
-        // default options needed? should be initialized by the plugin
-        // this.axiosInstance = axios.create({
-        //     baseURL: 'https://some-domain.com/api/',
-        //     timeout: 1000,
-        //     headers: { 'X-Custom-Header': 'foobar' }
-        // });
+        this.instance = axios.create({
+            timeout: 30000 // TODO configure from plugin options
+        });
     }
 
-    public execute<T>(
-        config: RequestConfig,
-        axiosOptions?: AxiosRequestConfig
-    ): Promise<AxiosResponse<T>> {
-        let mergedConfig: AxiosRequestConfig = this.buildConfig(config);
-        mergedConfig = {
-            ...mergedConfig,
-            ...axiosOptions
-        };
+    public setupResponseInterceptor<T>(onFulfilled: (value: any) => any, onRejected: (error: any) => any): void {
+        if (this.responseInterceptor !== undefined) {
+            throw new Error('An interceptor is already defined');
+        }
+        this.responseInterceptor = this.instance.interceptors.response.use((response: AxiosResponse) => {
+            return onFulfilled(response);
+        }, (error: Error) => {
+            return onRejected(error);
+        });
+    }
 
-        return axios.request<T>(mergedConfig);
+    public ejectResponseInterceptor(): void {
+        if (this.responseInterceptor !== undefined) {
+            this.instance.interceptors.response.eject(this.responseInterceptor);
+            this.responseInterceptor = undefined;
+        }
+    }
+
+    public execute<T>(config: RequestConfig, axiosOptions?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+        return new Promise<AxiosResponse<T>>((resolve, reject) => {
+            let mergedConfig: AxiosRequestConfig = this.buildConfig(config);
+            mergedConfig = {
+                ...mergedConfig,
+                ...axiosOptions
+            };
+
+            return this.instance.request<T>(mergedConfig);
+
+            // this.instance.request<T>(mergedConfig).then(response => {
+            //     resolve(response);
+            // }, error => {
+            //     reject(error);
+            // }).catch((error) => {
+            //     console.log('c');
+            //     if (error.response) {
+            //         // The request was made and the server responded with a status code
+            //         // that falls out of the range of 2xx
+            //         console.log(error.response.data);
+            //         console.log(error.response.status);
+            //         console.log(error.response.headers);
+            //     } else if (error.request) {
+            //         // The request was made but no response was received
+            //         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            //         // http.ClientRequest in node.js
+            //         console.log(error.request);
+            //     } else {
+            //         // Something happened in setting up the request that triggered an Error
+            //         console.log('Error', error.message);
+            //     }
+            //     console.log(error.config);
+            //     reject();
+            // });
+        });
     }
 
     private buildConfig(config: RequestConfig): AxiosRequestConfig {
