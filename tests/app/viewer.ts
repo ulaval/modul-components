052e4f8a7b01edb5081dcaa4036e742f1ca738e4
Jean-Francois Nadeau <jean-francois.nadeau@dti.ulaval.ca>
2018-03-02 13:37:34 -0500

    Remove viewer test code

--- tests/app/viewer.ts
+++ tests/app/viewer.ts
@@ -1,37 +1,15 @@
 import Component from 'vue-class-component';
 import { Watch } from 'vue-property-decorator';
 
-import { ModulVue } from '../../src/utils/vue/vue';
 import WithRender from './viewer.html';
 
 @WithRender
 @Component
-export class Viewer extends ModulVue {
+export class Viewer extends Vue {
     public tag: string = '';
 
     public mounted(): void {
         this.buildTag();
-    }
-
-    private onFilesReady(files): void {
-        files.forEach(f => {
-            const fd = new FormData();
-            fd.append('file', f.file);
-            this.$file.upload(f.uid, {
-                url: 'http://localhost:8989/upload',
-                config: {
-                    data: fd
-                }
-            });
-        });
-    }
-
-    private onFileUploadCancel(file): void {
-        this.$file.cancelUpload(file.uid);
-    }
-
-    private onFilesUploadCompleted(files): void {
-        // Do something with uploaded files !
     }
 
     @Watch('$route')
