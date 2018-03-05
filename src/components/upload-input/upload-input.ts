2667aa49ffe37c802b57902f90be4c61a79c06d8
Martin Simard <simardo@gmail.com>
2018-03-05 08:35:05 -0500

    revert old uploader removal (#122)

---
+++ src/components/upload-input/upload-input.ts
@@ -0,0 +1,35 @@
+import Vue, { PluginObject } from 'vue';
+import Component from 'vue-class-component';
+import { Prop, Watch } from 'vue-property-decorator';
+import WithRender from './upload-input.html?style=./upload-input.scss';
+import { UPLOAD_INPUT_NAME } from '../component-names';
+import { MUploadInterface } from '../upload/upload';
+
+@WithRender
+@Component
+export class MUploadInput extends Vue {
+
+    private filesInput($event: Event): void {
+        let files: FileList = ($event.target as any).files;
+
+        (this.$parent as MUploadInterface).addNewFiles(files);
+        this.$emit('filesInputed', files);
+    }
+
+    private get extensions(): string {
+        return (this.$parent as MUploadInterface).extensionsProp;
+    }
+
+    private get multiple(): boolean {
+        return (this.$parent as MUploadInterface).multipleProp;
+    }
+}
+
+const UploadInputPlugin: PluginObject<any> = {
+    install(v, options): void {
+        console.error(UPLOAD_INPUT_NAME + ' is deprecated');
+        v.component(UPLOAD_INPUT_NAME, MUploadInput);
+    }
+};
+
+export default UploadInputPlugin;
