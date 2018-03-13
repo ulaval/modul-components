a5a08e9af91d3078b51160d88a61f9ff331a5dc4
Jean-Francois Nadeau <jean-francois.nadeau@dti.ulaval.ca>
2018-03-13 11:01:43 -0400

    KISS max-length

--- src/components/textarea/textarea.spec.ts
+++ src/components/textarea/textarea.spec.ts
@@ -15,4 +15,32 @@ describe('MTextArea', () => {
 
         return expect(renderComponent(txtarea.vm)).resolves.toMatchSnapshot();
     });
+
+    describe('max-length', () => {
+        it('should render correctly state when text length is lesser than max length', () => {
+            const txtarea = mount(MTextarea, {
+                propsData: {
+                    maxLength: 8,
+                    value: '1'
+                }
+            });
+
+            return expect(
+                renderComponent(txtarea.vm)
+            ).resolves.toMatchSnapshot();
+        });
+
+        it('should render invalid state when text length is greater than max length', () => {
+            const txtarea = mount(MTextarea, {
+                propsData: {
+                    maxLength: 8,
+                    value: '123456789'
+                }
+            });
+
+            return expect(
+                renderComponent(txtarea.vm)
+            ).resolves.toMatchSnapshot();
+        });
+    });
 });
--- src/components/textarea/textarea.ts
+++ src/components/textarea/textarea.ts
@@ -4,7 +4,7 @@ import { Prop } from 'vue-property-decorator';
 
 import { ElementQueries } from '../../mixins/element-queries/element-queries';
 import { InputLabel } from '../../mixins/input-label/input-label';
-import { InputManagement } from '../../mixins/input-management/input-management';
+import { InputManagement, InputManagementData } from '../../mixins/input-management/input-management';
 import { InputState } from '../../mixins/input-state/input-state';
 import { InputWidth } from '../../mixins/input-width/input-width';
 import { ModulVue } from '../../utils/vue/vue';
@@ -24,46 +24,45 @@ import WithRender from './textarea.html?style=./textarea.scss';
         ElementQueries
     ]
 })
-export class MTextarea extends ModulVue {
+export class MTextarea extends ModulVue implements InputManagementData {
     @Prop()
-    public maxlength: number;
+    public maxLength?: number;
 
-    private internalTextareaError: boolean = false;
-    private textareaHeight: string;
+    readonly internalValue: string;
 
-    protected mounted(): void {
-        this.as<ElementQueries>().$on('resize', this.resizeInput);
+    // protected mounted(): void {
+        // this.as<ElementQueries>().$on('resize', this.resizeInput);
+    // }
+
+    // protected beforeDestroy(): void {
+        // this.as<ElementQueries>().$off('resize', this.resizeInput);
+    // }
+
+    private get valueLength(): number {
+        return this.internalValue.length;
     }
 
-    protected beforeDestroy(): void {
-        this.as<ElementQueries>().$off('resize', this.resizeInput);
+    public get hasMaxLength(): boolean {
+        return this.maxLength ? this.maxLength > 0 : false;
     }
 
-    private get valueLenght(): number {
-        let lenght: number = this.as<InputManagement>().internalValue.length;
-        if (lenght > this.maxlength) {
-            this.internalTextareaError = true;
-        } else {
-            this.internalTextareaError = false;
-        }
-        return lenght;
+    private get hasTextAreaError(): boolean {
+        return !this.isLengthValid || this.as<InputState>().hasError;
     }
 
-    private get hasMaxlenght(): boolean {
-        return this.maxlength > 0;
+    private get isTextAreaValid(): boolean {
+        return this.isLengthValid && this.as<InputState>().isValid;
     }
 
-    private get textareaError(): boolean {
-        return this.internalTextareaError || this.as<InputState>().hasError;
+    private get isLengthValid(): boolean {
+        return this.maxLength
+            ? this.internalValue.length < this.maxLength
+            : true;
     }
 
-    private get textareaValid(): boolean {
-        return !this.textareaError && this.as<InputState>().isValid;
-    }
-
-    private resizeInput(): void {
-      //  (this.$refs.textarea as MTextareaResize).resize();
-    }
+    // private resizeInput(): void {
+        //  (this.$refs.textarea as MTextareaResize).resize();
+    // }
 }
 
 const TextareaPlugin: PluginObject<any> = {
