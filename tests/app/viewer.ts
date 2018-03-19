848739e301823c36e2b136403ed713ee5f2602cb
Mathieu Boulianne <mathieu.boulianne1@gmail.com>
2018-03-19 09:58:34 -0400

    Repair viewer.ts (shouldn't have commited that)

--- tests/app/viewer.ts
+++ tests/app/viewer.ts
@@ -8,23 +8,9 @@ import WithRender from './viewer.html';
 @Component
 export class Viewer extends Vue {
     public tag: string = '';
-    public items: any[] = [];
-    private counter = 0;
 
     public mounted(): void {
         this.buildTag();
-    }
-
-    public addItem(): void {
-        this.counter++;
-        this.items.push({ id: this.counter, text: `Item #${this.counter}` });
-    }
-
-    public getSubItems(item): any {
-        if (item.id % 3 === 0) {
-            return [{ id: item.id * 1000, text:  `Item #${item.id}.1` }, { id: item.id * 1000, text:  `Item #${item.id}.2` }];
-        }
-        return [];
     }
 
     @Watch('$route')
