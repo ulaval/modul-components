a654ffaeba27e3012faa523c18011650bed1e3fb
Mathieu Boulianne <mathieu.boulianne1@gmail.com>
2018-03-23 07:28:55 -0400

    better tests

--- tests/app/viewer.ts
+++ tests/app/viewer.ts
@@ -1,50 +1,56 @@
-import { MSortEvent } from './../../src/directives/sortable/sortable';
-import { MDropEvent } from './../../src/directives/droppable/droppable';
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { Watch } from 'vue-property-decorator';
 
 import WithRender from './viewer.html';
+import { MDropEvent } from '../../src/directives/droppable/droppable';
+import { MSortEvent } from '../../src/directives/sortable/sortable';
 
-export class MyObject {
-    public readonly id: number;
+class MyObject {
+    public id: number;
     public text: string;
+    public counter: number;
 
-    constructor(id: number) {
+    constructor(id: number, text: string) {
         this.id = id;
-        this.text = `Hello from ${ this.id }`;
+        this.text = text;
+        this.counter = 0;
     }
 }
-
 @WithRender
 @Component
 export class Viewer extends Vue {
     public tag: string = '';
-    public items: MyObject[] = [];
+    public items: any[] = [];
+    public items2: any[] = [];
+    public items3: any[] = [];
 
     public mounted(): void {
         this.buildTag();
     }
 
-    public changeItem(item: MyObject): void {
-        item.text += ' changed';
-    }
-
-    public addItem(event: MDropEvent): void {
-        this.items.push(new MyObject(this.items.length));
-    }
-
-    public moveItem(event: MSortEvent): void {
-        this.items.splice(event.sortInfo.oldPosition, 1);
-        this.items.splice(event.sortInfo.newPosition, 0, event.sortInfo.data);
-    }
-
-    public get newItem(): MyObject {
-        return new MyObject(this.items.length);
-    }
-
     @Watch('$route')
     private buildTag(): void {
         this.tag = `<${this.$route.meta}></${this.$route.meta}>`;
+    }
+
+    private handleDrop(event: MDropEvent): void {
+        console.log('handle the drop here plz.', event.dropInfo);
+    }
+
+    private getNewItem(actionName: string): MyObject {
+        return new MyObject(0, `Hello from ${actionName}`);
+    }
+
+    private handleSortableAdd(event: MSortEvent, list: any[]): void {
+        list.push(event.sortInfo.data);
+    }
+
+    private handleSortableMove(event: MSortEvent, list: any[]): void {
+        list.push(event.sortInfo.data);
+    }
+
+    private deleteItem(): void {
+        this.items.pop();
     }
 }
