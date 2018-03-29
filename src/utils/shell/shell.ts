60bbe836b9f994c82604d5a72fe29c74fc7bf4a3
Martin Simard <martin.simard@dti.ulaval.ca>
2018-03-29 10:34:49 -0400

    expose $shell service + onError method

--- src/utils/shell/shell.ts
+++ src/utils/shell/shell.ts
@@ -19,6 +19,17 @@ class ShellExtension {
         this.setupHttpInterceptors();
     }
 
+    /**
+     * Calls the window.onerror function. Use this method for errors that cannot be handled by the window.onerror standard handler.
+     * Mostly, this method will be used to provide retroaction on Promise errors.
+     * @param error The unhandled error.
+     * @param defer True will delay the execution of the call to the next cyle, allowing, for example, stopping the propagation of the error in the promise chain. Default is false.
+     */
+    public onError(error: Error | PromiseError, defer: boolean = false): void {
+        let call = () => window.onerror.call(this, error);
+        defer ? setTimeout(() => call(), 0) : call();
+    }
+
     private setupErrorHandlers(): void {
         // Vue parser errors do not propagate to window.onError
         Vue.config.errorHandler = (err, vm, info) => this.onError(err);
@@ -40,17 +51,12 @@ class ShellExtension {
             return Promise.reject(promiseError);
         });
     }
-
-    private onError(error: Error | PromiseError, defer: boolean = false): void {
-        // defering will allow to stop the propagation of a PromiseError within the promise chain
-        let call = () => window.onerror.call(this, error);
-        defer ? setTimeout(() => call(), 0) : call();
-    }
 }
 
 const ShellExtensionPlugin: PluginObject<any> = {
     install(v, options): void {
         let shellExtension = new ShellExtension();
+        (v.prototype as any).$shell = shellExtension;
     }
 };
 
