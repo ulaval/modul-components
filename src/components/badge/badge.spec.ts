0d529fe1ec02f68b8e3208d2c4d4c056907e69bf
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2018-04-12 12:41:40 -0400

    testing code

--- src/components/badge/badge.spec.ts
+++ src/components/badge/badge.spec.ts
@@ -2,7 +2,7 @@ import { createLocalVue, mount, Wrapper } from '@vue/test-utils';
 import Vue, { VueConstructor } from 'vue';
 
 import { renderComponent } from '../../../tests/helpers/render';
-import BadgePlugin, { MBadge } from './badge';
+import BadgePlugin, { MBadge, MBadgeState } from './badge';
 
 describe('MBadge', () => {
     let localVue: VueConstructor<Vue>;
@@ -13,10 +13,84 @@ describe('MBadge', () => {
     });
 
     it('should render correctly', () => {
-        const badge = mount(MBadge, {
+        const iconFile = mount(MBadge, {
             localVue: localVue
         });
 
-        return expect(renderComponent(badge.vm)).resolves.toMatchSnapshot();
+        return expect(renderComponent(iconFile.vm)).resolves.toMatchSnapshot();
+    });
+
+    it('should render correctly when state is completed', () => {
+        const iconFile = mount(MBadge, {
+            localVue: localVue,
+            propsData: {
+                state: MBadgeState.Completed
+            }
+        });
+
+        return expect(renderComponent(iconFile.vm)).resolves.toMatchSnapshot();
+    });
+    it('should render correctly when state is warning', () => {
+        const iconFile = mount(MBadge, {
+            localVue: localVue,
+            propsData: {
+                state: MBadgeState.Warning
+            }
+        });
+
+        return expect(renderComponent(iconFile.vm)).resolves.toMatchSnapshot();
+    });
+
+    it('should render correctly when state is error', () => {
+        const iconFile = mount(MBadge, {
+            localVue: localVue,
+            propsData: {
+                state: MBadgeState.Error
+            }
+        });
+
+        return expect(renderComponent(iconFile.vm)).resolves.toMatchSnapshot();
+    });
+
+    describe('icon', () => {
+        let iconFile: Wrapper<MBadge>;
+        beforeEach(() => {
+            iconFile = mount(MBadge, {
+                localVue: localVue,
+                propsData: {
+                    name: 'pdf'
+                }
+            });
+        });
+
+        it('should render correctly', () => {
+            return expect(renderComponent(iconFile.vm)).resolves.toMatchSnapshot();
+        });
+
+        it('should render correctly when size is set', () => {
+            iconFile.setProps({ size: '40px' });
+            return expect(renderComponent(iconFile.vm)).resolves.toMatchSnapshot();
+        });
+    });
+
+    describe('chip', () => {
+        let iconFile: Wrapper<MBadge>;
+        beforeEach(() => {
+            iconFile = mount(MBadge, {
+                localVue: localVue,
+                propsData: {
+                    state: MBadgeState.Completed
+                }
+            });
+        });
+
+        it('should render correctly', () => {
+            return expect(renderComponent(iconFile.vm)).resolves.toMatchSnapshot();
+        });
+
+        it('should render correctly when size is set', () => {
+            iconFile.setProps({ chipSize: '40px' });
+            return expect(renderComponent(iconFile.vm)).resolves.toMatchSnapshot();
+        });
     });
 });
--- src/components/badge/badge.ts
+++ src/components/badge/badge.ts
@@ -6,14 +6,42 @@ import { BADGE_NAME } from '../component-names';
 import IconPlugin from '../icon/icon';
 import SpinnerPlugin from '../spinner/spinner';
 
+export enum MBadgeState {
+    Completed = 'completed',
+    Warning = 'warning',
+    Error = 'error'
+}
+
 @WithRender
 @Component
 export class MBadge extends Vue {
 
     @Prop()
+    public name: string;
+    @Prop({ default: '30px' })
+    public size: string;
+    @Prop({ default: '16px' })
+    public chipSize: string;
+    @Prop()
+    public svgTitle: string;
+    @Prop()
     public disabled: boolean;
     @Prop()
     public waiting: boolean;
+    @Prop({
+        default: MBadgeState.Completed,
+        validator: value =>
+            value == MBadgeState.Completed ||
+            value == MBadgeState.Warning ||
+            value == MBadgeState.Error
+    })
+    public state: MBadgeState;
+
+    private mounted(): void {
+        let id = this.$children[0]['name'];
+        let svg = (document.getElementById(id) as HTMLElement).dataset.format;
+        console.log(id, svg);
+    }
 
     private onClick(event: Event): void {
         this.$emit('click', event);
@@ -26,6 +54,42 @@ export class MBadge extends Vue {
 
     private onBlur(event: Event): void {
         this.$emit('blur');
+    }
+
+    private getIcon(): string {
+        let icon: string = '';
+        switch (this.state) {
+            case MBadgeState.Completed:
+                icon = 'chip-check';
+                break;
+            case MBadgeState.Warning:
+                icon = 'chip-warning';
+                break;
+            case MBadgeState.Error:
+                icon = 'chip-error';
+                break;
+            default:
+                break;
+        }
+        return icon;
+    }
+
+    private get rightDistance(): string {
+        let size = parseInt(this.size, 10);
+        let chipSise = parseInt(this.chipSize, 10);
+        let realSize = (size / (24 / 18));
+        let distance = ((size - realSize) / 2) - (chipSise / 2);
+        return distance + 'px';
+    }
+
+    private get bottomDistance(): string {
+        let chipSise = parseInt(this.chipSize, 10);
+        let distance = chipSise * (1 / 4);
+        return '-' + distance + 'px';
+    }
+
+    private get hasState(): boolean {
+        return !!this.state;
     }
 }
 
