3eb76a67e71d5d2f918000416dfad10461c2741c
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2018-04-13 08:32:05 -0400

    Optimation switch case

--- src/components/icon-file/icon-file.ts
+++ src/components/icon-file/icon-file.ts
@@ -4,11 +4,35 @@ import { Prop } from 'vue-property-decorator';
 import WithRender from './icon-file.html';
 import { ICON_FILE_NAME } from '../component-names';
 
-export const TYPE_PDF: string[] = ['pdf'];
-export const TYPE_DOC: string[] = ['doc', 'txt'];
-export const TYPE_VIDEO: string[] = ['mp4'];
-export const TYPE_IMG: string[] = ['img'];
-export const TYPE_ARCHIVE: string[] = ['zip'];
+// Extensions list
+export const TYPE_PDF: string = 'pdf';
+export const TYPE_DOC: string = 'doc';
+export const TYPE_TXT: string = 'txt';
+export const TYPE_MP4: string = 'mp4';
+export const TYPE_IMG: string = 'img';
+export const TYPE_GIF: string = 'gif';
+export const TYPE_ZIP: string = 'zip';
+
+// Extensions group
+export const GROUP_PDF: string = 'modulsvg-file-pdf';
+export const GROUP_DOC: string = 'modulsvg-file-other';
+export const GROUP_VIDEO: string = 'modulsvg-file-other';
+export const GROUP_IMAGE: string = 'modulsvg-file-other';
+export const GROUP_ARCHIVE: string = 'modulsvg-file-other';
+
+export type FileGroup = {
+    [key: string]: string
+};
+
+const FILES_ASSOCIATIONS: FileGroup = {
+    [TYPE_PDF]: GROUP_PDF,
+    [TYPE_DOC]: GROUP_DOC,
+    [TYPE_TXT]: GROUP_DOC,
+    [TYPE_MP4]: GROUP_VIDEO,
+    [TYPE_IMG]: GROUP_IMAGE,
+    [TYPE_GIF]: GROUP_IMAGE,
+    [TYPE_ZIP]: GROUP_ARCHIVE
+};
 
 @WithRender
 @Component
@@ -20,38 +44,36 @@ export class MIconFile extends Vue {
     @Prop({ default: '24px' })
     public size: string;
 
-    private get hasSvgTitle(): boolean {
-        return !!this.svgTitle;
-    }
-
     private get spriteId(): string {
         let cleanExtension = this.extension.split('.').join('').toLowerCase();
         let id: string = '';
         switch (cleanExtension) {
-            case this.checkExtension(cleanExtension, TYPE_PDF):
-                id = 'modulsvg-file-pdf';
+            case TYPE_PDF:
+                id = FILES_ASSOCIATIONS[TYPE_PDF];
                 break;
-            case this.checkExtension(cleanExtension, TYPE_DOC):
-                id = 'information';
+            case TYPE_DOC:
+                id = FILES_ASSOCIATIONS[TYPE_DOC];
                 break;
-            case this.checkExtension(cleanExtension, TYPE_VIDEO):
-                id = 'warning';
+            case TYPE_TXT:
+                id = FILES_ASSOCIATIONS[TYPE_TXT];
                 break;
-            case this.checkExtension(cleanExtension, TYPE_IMG):
-                id = 'error';
+            case TYPE_MP4:
+                id = FILES_ASSOCIATIONS[TYPE_MP4];
                 break;
-            case this.checkExtension(cleanExtension, TYPE_ARCHIVE):
-                id = 'options';
+            case TYPE_IMG:
+                id = FILES_ASSOCIATIONS[TYPE_IMG];
+                break;
+            case TYPE_GIF:
+                id = FILES_ASSOCIATIONS[TYPE_GIF];
+                break;
+            case TYPE_ZIP:
+                id = FILES_ASSOCIATIONS[TYPE_ZIP];
                 break;
             default:
                 id = 'modulsvg-file-other';
                 break;
         }
         return id;
-    }
-
-    private checkExtension(ext: string, type: string[]): string {
-        return type.indexOf(ext) > -1 ? ext : '';
     }
 
     private onClick(event): void {
