0126502332eb36584f9ed5102a76c1de2bb2a908
gauthier bramardi <gauthier.bramardi@dti.ulaval.ca>
2018-04-20 09:54:03 -0400

    ENA2-724 - created filter plugin and corrected tests

--- src/filters/i18n/i18n.spec.ts
+++ src/filters/i18n/i18n.spec.ts
@@ -1,24 +1,25 @@
 import { resetModulPlugins } from '../../../tests/helpers/component';
 import Vue, { VueConstructor } from 'vue';
 import { Wrapper, mount } from '@vue/test-utils';
-import I18nPlugin, { I18nPluginOptions } from '../../utils/i18n/i18n';
+import I18nPlugin, { I18nPluginOptions, FORMAT_MODE } from '../../utils/i18n/i18n';
 import { addMessages } from '../../../tests/helpers/lang';
-import './i18n';
+import I18nFilterPlugin from './i18n';
 
 describe(`Étant donné le filtre f-m-i18n`, () => {
     let element: Wrapper<Vue>;
     beforeEach(() => {
         let localVue: VueConstructor<Vue>;
         let options: I18nPluginOptions = {
-            formatMode: 'vsprintf'
+            formatMode: FORMAT_MODE.VSSPRINTF
         };
 
         resetModulPlugins();
         Vue.use(I18nPlugin, options);
+        Vue.use(I18nFilterPlugin);
         addMessages(Vue, ['filters/i18n/i18n.spec.lang.fr.json']);
     });
 
-    describe(`Étant donné le filtre {{ exemples-avec-nombre-genre:medaille-olympique | f-m-i18n({}, 2, 'f') }}`, () => {
+    describe(`giving the filter {{ exemples-avec-nombre-genre:medaille-olympique | f-m-i18n({}, 2, 'f') }}`, () => {
         beforeEach(() => {
             element = mount(
                 {
@@ -28,27 +29,12 @@ describe(`Étant donné le filtre f-m-i18n`, () => {
                 { localVue: Vue }
             );
         });
-        it(`le span devrait contenir "Les deux seules médaillées olympiques"`, () => {
+        it(`the span's content should be "Les deux seules médaillées olympiques"`, () => {
             expect(element.vm.$el.textContent).toEqual('Les deux seules médaillées olympiques');
         });
     });
 
-    describe(`Étant donné l'attribut :title="'exemples-avec-nombre-genre:medaille-olympique' | f-m-i18n({}, 2, 'f')"`, () => {
-        beforeEach(() => {
-            element = mount(
-                {
-                    template:
-                        `<span :title="'exemples-avec-nombre-genre:medaille-olympique' | f-m-i18n({}, 2, 'f')"></span>`
-                },
-                { localVue: Vue }
-            );
-        });
-        it(`l'élément devrait avoir le titre "Les deux seules médaillées olympiques"`, () => {
-            expect(element.vm.$el.getAttribute('title')).toEqual('Les deux seules médaillées olympiques');
-        });
-    });
-
-    describe(`Étant donné l'attribut :title="'exemples-avec-nombre-genre:medaille-olympique' | f-m-i18n({nb:2, modifier:'f'})"`, () => {
+    describe(`Giving the attribute :title="'exemples-avec-nombre-genre:medaille-olympique' | f-m-i18n({nb:2, modifier:'f'})"`, () => {
         beforeEach(() => {
             element = mount(
                 {
@@ -58,7 +44,7 @@ describe(`Étant donné le filtre f-m-i18n`, () => {
                 { localVue: Vue }
             );
         });
-        it(`l'élément devrait avoir le titre "Les deux seules médaillées olympiques"`, () => {
+        it(`the element should have the title attribute "Les deux seules médaillées olympiques"`, () => {
             expect(element.vm.$el.getAttribute('title')).toEqual('Les deux seules médaillées olympiques');
         });
     });
--- src/filters/i18n/i18n.ts
+++ src/filters/i18n/i18n.ts
@@ -1,9 +1,18 @@
-import Vue from 'vue';
+import Vue, { PluginObject } from 'vue';
+import { I18N_NAME } from '../filter-names';
 
-Vue.filter('f-m-i18n', (key: string,
+const i18nFilter = (key: string,
     params: any[] = [],
     nb?: number,
     modifier?: string,
     htmlEncodeParams: boolean = true) => {
     return Vue.prototype.$i18n.translate(key, params, nb, modifier, htmlEncodeParams);
-});
+};
+
+const I18nFilterPlugin: PluginObject<any> = {
+    install(v, options): void {
+        v.filter(I18N_NAME, i18nFilter);
+    }
+};
+
+export default I18nFilterPlugin;
