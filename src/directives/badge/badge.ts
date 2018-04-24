b9d52ec587560a1427663addbfef03d4cf25fa02
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2018-04-24 11:38:04 -0400

    Add default badge position

--- src/directives/badge/badge.ts
+++ src/directives/badge/badge.ts
@@ -3,6 +3,8 @@ import Component from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
 import { BADGE } from '../directive-names';
 import { MIcon } from '../../components/icon/icon';
+import { MIconFile } from '../../components/icon-file/icon-file';
+import { ComponentMeta } from '../../meta/meta';
 
 // Icon state
 export enum MBadgeState {
@@ -39,10 +41,26 @@ const MBadgeDirective: DirectiveOptions = {
     ): void {
 
         // Element data
+        let elSize = parseInt((vnode.componentOptions as ComponentMeta)['propsData']['size'], 10);
         let elWidth = parseInt(el.attributes['width']['value'], 10);
         let elHeight = parseInt(el.attributes['height']['value'], 10);
-        let elID = (vnode.componentInstance as MIcon).name;
-        let elFormat = (document.getElementById(elID) as HTMLElement).dataset.format;
+        let elTag = (vnode.componentOptions as ComponentMeta).tag;
+        let elID;
+        if (elTag == 'm-icon') {
+            elID = (vnode.componentInstance as MIcon).name;
+        } else if (elTag == 'm-icon-file') {
+            elID = (vnode.componentInstance as MIconFile).spriteId;
+        }
+        let elBadgeOrigin;
+        if ((document.getElementById(elID) as HTMLElement).dataset.badgeOrigin != undefined) {
+            elBadgeOrigin = (((document.getElementById(elID) as HTMLElement).dataset.badgeOrigin) as string).split(',');
+        } else {
+            elBadgeOrigin = ['23.5','23.5'];
+        }
+
+        let elLeftOrigin = Number(parseFloat(elBadgeOrigin[0].replace(/,/g, '.')).toFixed(2));
+        let elTopOrigin = Number(parseFloat(elBadgeOrigin[1].replace(/,/g, '.')).toFixed(2));
+
         el.style.overflow = 'visible';
 
         let iconInstance = new MIcon({
@@ -50,48 +68,37 @@ const MBadgeDirective: DirectiveOptions = {
         });
 
         // Badge settings
-        let badgeSise = elWidth / (30 / 16) ;
-        let portraitRatio = (24 / 18);
-        let landscapeRatio = (24 / 18);
+        let badgeSize = elSize * (16 / 30) ;
         let offsetX = 0;
         let offsetY = 0;
-
+        // Badge state settings
         if (binding.value.state != undefined) {
             iconInstance.name = BADGE_ICON[binding.value.state];
             iconInstance.$el.style.color = BADGE_COLOR[binding.value.state];
         }
-
+        // Badge offsetX settings
         if (binding.value.offsetX != undefined) {
             offsetX = parseInt(binding.value.offsetX, 10);
         }
-
+        // Badge offsetY settings
         if (binding.value.offsetY != undefined) {
             offsetY = parseInt(binding.value.offsetY, 10);
         }
-
-        iconInstance.size = badgeSise + 'px';
-
-        if (elFormat != undefined) {
-            if (elFormat == 'portrait') {
-                let leftDistance = (elWidth - ((elWidth - (elWidth / portraitRatio)) * 0.5) - (badgeSise / 2)) + offsetX;
-                let topDistance = (elHeight - (badgeSise * 0.5)) + offsetY;
-                iconInstance.$el.setAttribute('x', leftDistance + 'px');
-                iconInstance.$el.setAttribute('y', topDistance + 'px');
-            } else if (elFormat == 'landscape') {
-                let leftDistance = (elWidth - (badgeSise * 0.5)) + offsetX;
-                let topDistance = (elHeight - ((elHeight - (elHeight / landscapeRatio)) * 0.5) - (badgeSise / 2)) + offsetY;
-                iconInstance.$el.setAttribute('x', leftDistance + 'px');
-                iconInstance.$el.setAttribute('y', topDistance + 'px');
-            } else {
-                console.error('Svg data-format is not recognized for the svg having the id #' + elID + '');
-            }
+        // Set badge size
+        iconInstance.size = badgeSize + 'px';
+        // Set badge position
+        if (elSize == 24) {
+            let leftDistance = elLeftOrigin - (badgeSize * 0.5) + offsetX;
+            let topDistance = elTopOrigin - (badgeSize * (2 / 3)) + offsetY;
+            iconInstance.$el.setAttribute('x', leftDistance + '');
+            iconInstance.$el.setAttribute('y', topDistance + '');
         } else {
-            let leftDistance = (elWidth - (badgeSise * 0.5)) + offsetX;
-            let topDistance = (elHeight - (badgeSise * 0.5)) + offsetY;
-            iconInstance.$el.setAttribute('x', leftDistance + 'px');
-            iconInstance.$el.setAttribute('y', topDistance + 'px');
+            let leftDistance = ((elLeftOrigin / 24) * elWidth) - (badgeSize * 0.5) + offsetX;
+            let topDistance = ((elTopOrigin / 24) * elHeight) - (badgeSize * (2 / 3)) + offsetY;
+            iconInstance.$el.setAttribute('x', leftDistance + '');
+            iconInstance.$el.setAttribute('y', topDistance + '');
         }
-
+        // Adding the badge on the element
         el.appendChild(iconInstance.$el);
     },
     unbind(
