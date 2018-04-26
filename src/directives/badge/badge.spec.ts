88316a9b123513440f980e183c3c01c1d6a4a37e
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2018-04-26 13:47:58 -0400

    unit test test

--- src/directives/badge/badge.spec.ts
+++ src/directives/badge/badge.spec.ts
@@ -8,20 +8,30 @@ import FilePlugin, { DEFAULT_STORE_NAME } from '../../utils/file/file';
 import { ModulVue } from '../../utils/vue/vue';
 import BadgePlugin from './badge';
 import { addMessages } from '../../../tests/helpers/lang';
-import { MIconFile } from '../../components/icon-file/icon-file';
+import IconFilePluggin, { MIconFile } from '../../components/icon-file/icon-file';
 
 describe('MBadge', () => {
     let localVue: VueConstructor<ModulVue>;
-    let badge: Wrapper<ModulVue>;
+    let iconFile: Wrapper<ModulVue>;
+
+    beforeEach(() => {
+        resetModulPlugins();
+        Vue.use(IconFilePluggin);
+
+        iconFile = mount(
+            {
+                template: '<m-icon-file v-m-badge=="{ state: state }"></m-icon-file>'
+            },
+            {
+                localVue: Vue,
+                data: {
+                    state: 'completed'
+                }
+            }
+        );
+    });
 
     it('should render correctly', () => {
-        const iconFile = mount(MIconFile, {
-            localVue: localVue,
-            propsData: {
-                extension: 'jpg'
-            }
-        });
-
         return expect(renderComponent(iconFile.vm)).resolves.toMatchSnapshot();
     });
 });
