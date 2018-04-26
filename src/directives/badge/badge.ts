47b7e20c396e39934acfb53f6dbf91523013a45c
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2018-04-26 09:19:17 -0400

    Update badge code and clean it

--- src/directives/badge/badge.ts
+++ src/directives/badge/badge.ts
@@ -1,4 +1,4 @@
-import { DirectiveOptions, VNodeDirective, VNode, PluginObject } from 'vue';
+import Vue, { DirectiveOptions, VNodeDirective, VNode, PluginObject } from 'vue';
 import Component from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
 import { BADGE } from '../directive-names';
@@ -7,115 +7,134 @@ import { MIconFile } from '../../components/icon-file/icon-file';
 import { ComponentMeta } from '../../meta/meta';
 
 // Icon state
-export enum MBadgeState {
+enum MBadgeState {
+    Empty = 'empty',
     Completed = 'completed',
-    Error = 'error'
+    Failed = 'failed'
 }
-// Icon name
-export const ICON_COMPLETED: string = 'chip-check';
-export const ICON_ERROR: string = 'chip-error';
-// Icon color
-export const COLOR_COMPLETED: string = '#00c77f';
-export const COLOR_ERROR: string = '#e30513';
-// Badge icon type
-export type BadgeIcon = {
+
+type BadgeIcon = {
     [key: string]: string
 };
 
+const ICON_COMPLETED: string = 'chip-check';
+const ICON_FAILED: string = 'chip-error';
+const COLOR_COMPLETED: string = '#00c77f';
+const COLOR_FAILED: string = '#e30513';
+
 const BADGE_ICON: BadgeIcon = {
     [MBadgeState.Completed]: ICON_COMPLETED,
-    [MBadgeState.Error]: ICON_ERROR
+    [MBadgeState.Failed]: ICON_FAILED
 };
 
 const BADGE_COLOR: BadgeIcon = {
     [MBadgeState.Completed]: COLOR_COMPLETED,
-    [MBadgeState.Error]: COLOR_ERROR
+    [MBadgeState.Failed]: COLOR_FAILED
+};
+
+const getBadgeOrigin: (vnode: VNode) => String[] = (vnode: VNode) => {
+    let elTag = (vnode.componentOptions as ComponentMeta).tag;
+    let elID;
+    if (elTag == 'm-icon') {
+        elID = (vnode.componentInstance as MIcon).name;
+    } else if (elTag == 'm-icon-file') {
+        elID = (vnode.componentInstance as MIconFile).spriteId;
+    }
+
+    if ((document.getElementById(elID) as HTMLElement).dataset.badgeOrigin != undefined) {
+        return (((document.getElementById(elID) as HTMLElement).dataset.badgeOrigin) as string).split(',');
+    } else {
+        return ['23.5', '23.5'];
+    }
+};
+
+interface BadgeOffset {
+    x: number;
+    y: number;
+}
+const getBadgeOffset: (binding: VNodeDirective) => BadgeOffset = (binding: VNodeDirective) => {
+    let offsetX = 0;
+    let offsetY = 0;
+    // Badge offsetX settings
+    if (binding.value.offsetX != undefined) {
+        offsetX = parseInt(binding.value.offsetX, 10);
+    }
+    // Badge offsetY settings
+    if (binding.value.offsetY != undefined) {
+        offsetY = parseInt(binding.value.offsetY, 10);
+    }
+    return { x: offsetX, y: offsetY };
+};
+
+interface BadgePosition {
+    size: number;
+    leftDistance: number;
+    topDistance: number;
+}
+const getBadgePosition: (element: HTMLElement, binding: VNodeDirective, vnode: VNode) => BadgePosition = (element: HTMLElement, binding: VNodeDirective, vnode: VNode) => {
+    let leftDistance;
+    let topDistance;
+    let badgeOrigin = getBadgeOrigin(vnode);
+    let badgeOffset = getBadgeOffset(binding);
+    let elSize = parseInt((vnode.componentOptions as ComponentMeta)['propsData']['size'], 10);
+    let badgeSize = elSize * (16 / 30);
+    let elLeftOrigin = Number(parseFloat(badgeOrigin[0].replace(/,/g, '.')).toFixed(2));
+    let elTopOrigin = Number(parseFloat(badgeOrigin[1].replace(/,/g, '.')).toFixed(2));
+    if (elSize == 24) {
+        leftDistance = elLeftOrigin - (badgeSize * 0.5) + badgeOffset.x;
+        topDistance = elTopOrigin - (badgeSize * (2 / 3)) + badgeOffset.y;
+    } else {
+        let elWidth = parseInt(element.attributes['width']['value'], 10);
+        let elHeight = parseInt(element.attributes['height']['value'], 10);
+        leftDistance = ((elLeftOrigin / 24) * elWidth) - (badgeSize * 0.5) + badgeOffset.x;
+        topDistance = ((elTopOrigin / 24) * elHeight) - (badgeSize * (2 / 3)) + badgeOffset.y;
+    }
+    return { size: badgeSize , leftDistance, topDistance };
+};
+
+const buildBadge = (element, binding, vnode) => {
+    element.style.overflow = 'visible';
+
+    let badge = getBadgePosition(element, binding, vnode);
+    const MyComponent = Vue.extend({
+        template: `<m-icon
+                        :name="'${BADGE_ICON[binding.value.state]}'"
+                        :size="'${badge.size}'"
+                        x="${badge.leftDistance}"
+                        y="${badge.topDistance}">
+                    </m-icon>`
+    });
+
+    const component = new MyComponent().$mount();
+    component.$el.style.color = BADGE_COLOR[binding.value.state];
+    element.appendChild(component.$el);
 };
 
 const MBadgeDirective: DirectiveOptions = {
     bind(
-        el: HTMLElement,
+        element: HTMLElement,
         binding: VNodeDirective,
         vnode: VNode,
         oldVnode: VNode
     ): void {
-
-        console.log(binding.modifiers);
-        console.log(binding.arg);
-        console.log(binding.expression);
-        console.log(binding.arg);
-        console.log(binding.name);
-        console.log(binding.value);
-        console.log(binding.oldValue);
-
-        // Element data
-        let elSize = parseInt((vnode.componentOptions as ComponentMeta)['propsData']['size'], 10);
-        let elWidth = parseInt(el.attributes['width']['value'], 10);
-        let elHeight = parseInt(el.attributes['height']['value'], 10);
-        let elTag = (vnode.componentOptions as ComponentMeta).tag;
-        let elID;
-        if (elTag == 'm-icon') {
-            elID = (vnode.componentInstance as MIcon).name;
-        } else if (elTag == 'm-icon-file') {
-            elID = (vnode.componentInstance as MIconFile).spriteId;
-        }
-        let elBadgeOrigin;
-        if ((document.getElementById(elID) as HTMLElement).dataset.badgeOrigin != undefined) {
-            elBadgeOrigin = (((document.getElementById(elID) as HTMLElement).dataset.badgeOrigin) as string).split(',');
-        } else {
-            elBadgeOrigin = ['23.5','23.5'];
-        }
-
-        let elLeftOrigin = Number(parseFloat(elBadgeOrigin[0].replace(/,/g, '.')).toFixed(2));
-        let elTopOrigin = Number(parseFloat(elBadgeOrigin[1].replace(/,/g, '.')).toFixed(2));
-
-        el.style.overflow = 'visible';
-
-        let iconInstance = new MIcon({
-            el: document.createElement('span')
-        });
-
-        // Badge settings
-        let badgeSize = elSize * (16 / 30) ;
-        let offsetX = 0;
-        let offsetY = 0;
-        // Badge state settings
-        if (binding.value.state != undefined) {
-            iconInstance.name = BADGE_ICON[binding.value.state];
-            iconInstance.$el.style.color = BADGE_COLOR[binding.value.state];
-        }
-        // Badge offsetX settings
-        if (binding.value.offsetX != undefined) {
-            offsetX = parseInt(binding.value.offsetX, 10);
-        }
-        // Badge offsetY settings
-        if (binding.value.offsetY != undefined) {
-            offsetY = parseInt(binding.value.offsetY, 10);
-        }
-        // Set badge size
-        iconInstance.size = badgeSize + 'px';
-        // Set badge position
-        if (elSize == 24) {
-            let leftDistance = elLeftOrigin - (badgeSize * 0.5) + offsetX;
-            let topDistance = elTopOrigin - (badgeSize * (2 / 3)) + offsetY;
-            iconInstance.$el.setAttribute('x', leftDistance + '');
-            iconInstance.$el.setAttribute('y', topDistance + '');
-        } else {
-            let leftDistance = ((elLeftOrigin / 24) * elWidth) - (badgeSize * 0.5) + offsetX;
-            let topDistance = ((elTopOrigin / 24) * elHeight) - (badgeSize * (2 / 3)) + offsetY;
-            iconInstance.$el.setAttribute('x', leftDistance + '');
-            iconInstance.$el.setAttribute('y', topDistance + '');
-        }
-        // Adding the badge on the element
-        el.appendChild(iconInstance.$el);
+        buildBadge(element, binding, vnode);
+    },
+    update(
+        element: HTMLElement,
+        binding: VNodeDirective,
+        vnode: VNode,
+        oldVnode: VNode
+    ): void {
+        element.removeChild(element.children[element.children.length - 1]);
+        buildBadge(element, binding, vnode);
     },
     unbind(
-        el: HTMLElement,
+        element: HTMLElement,
         binding: VNodeDirective,
         vnode: VNode,
         oldVnode: VNode
     ): void {
-        //
+        element.removeChild(element.children[element.children.length - 1]);
     }
 };
 
