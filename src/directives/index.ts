6e0f4caad8a1eb3a1f2fec5b652ef5bf6bd90772
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2018-04-26 09:25:12 -0400

    resolve conflic

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -6,14 +6,13 @@ import DraggableAllowScrollPlugin from './draggable/draggable-allow-scroll';
 import DroppablePlugin from './droppable/droppable';
 import DroppableGroupPlugin from './droppable/droppable-group';
 import FileDropPlugin from './file-drop/file-drop';
+import I18nDirectivePlugin from './i18n/i18n';
 import PopupPlugin from './popup/popup';
 import RippleEffectPlugin from './ripple-effect/ripple-effect';
 import ScrollToPlugin from './scroll-to/scroll-to';
 import SortablePlugin from './sortable/sortable';
 import TextAreaAutoHeightPlugin from './textarea-auto-height/textarea-auto-height';
 import RemoveUserSelectPlugin from './user-select/remove-user-select';
-import I18nDirectivePlugin from './i18n/i18n';
-import TextAreaAutoHeightPlugin from './textarea-auto-height/textarea-auto-height';
 
 const DirectivesPlugin: PluginObject<any> = {
     install(v, options): void {
