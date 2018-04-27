73674ba439a7621b524e71a46e2a69cb1ab0983b
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2018-04-27 13:17:05 -0400

    Remove test code

--- tests/app/viewer.ts
+++ tests/app/viewer.ts
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import { ModulVue } from '../../src/utils/vue/vue';
 import Component from 'vue-class-component';
 import { Watch } from 'vue-property-decorator';
 
@@ -7,38 +6,17 @@ import WithRender from './viewer.html';
 
 @WithRender
 @Component
-export class Viewer extends ModulVue {
+export class Viewer extends Vue {
     public tag: string = '';
     private test: boolean = false;
     private test2: string | undefined = undefined;
 
     public mounted(): void {
         this.buildTag();
-
-        setInterval(() => {
-            this.test2 = 'completed';
-            this.test = true;
-        }, 3000);
     }
 
     @Watch('$route')
     private buildTag(): void {
         this.tag = `<${this.$route.meta}></${this.$route.meta}>`;
-    }
-
-    private onFilesReady(files): void {
-        files.forEach(f => {
-            this.$file.upload(f.uid, {
-                url: 'http://localhost:8082/upload'
-            });
-        });
-    }
-
-    private onFileUploadCancel(file): void {
-        this.$file.cancelUpload(file.uid);
-    }
-
-    private onFilesUploadCompleted(files): void {
-        // Do something with uploaded files !
     }
 }
