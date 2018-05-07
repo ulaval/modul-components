6ed7622f3323d39bd5f20e8d3eda0bdb358479fe
Martin Simard <martin.simard@dti.ulaval.ca>
2018-05-07 11:02:24 -0400

    utilsPluginOption support for i18nOptions; hide debug log for unit tests

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ import Vue, { PluginObject } from 'vue';
 
 import MediaQueriesPlugin from './media-queries/media-queries';
 import ModulPlugin from './modul/modul';
-import MessagesPlugin from './i18n/i18n';
+import I18nPlugin, { I18nPluginOptions } from './i18n/i18n';
 import HttpPlugin, { HttpPluginOptions } from './http/http';
 import SpritesPlugin from './svg/sprites';
 import * as TouchPlugin from 'vue-touch';
@@ -15,6 +15,7 @@ import LoggerPlugin, { ConsoleOptions } from './logger/logger';
 export interface UtilsPluginOptions {
     httpPluginOptions?: HttpPluginOptions;
     consoleOptions?: ConsoleOptions;
+    i18PluginOptions?: I18nPluginOptions;
 }
 
 const UtilsPlugin: PluginObject<any> = {
@@ -27,7 +28,7 @@ const UtilsPlugin: PluginObject<any> = {
 
         Vue.use(MediaQueriesPlugin);
         Vue.use(ModulPlugin);
-        Vue.use(MessagesPlugin);
+        Vue.use(I18nPlugin, options ? options.i18PluginOptions : undefined);
         Vue.use(HttpPlugin, options ? options.httpPluginOptions : undefined);
         Vue.use({ install: PortalPluginInstall });
         Vue.use(SpritesPlugin);
