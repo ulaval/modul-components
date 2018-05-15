2f2ec9c32a7dfe78eb55b273b5b7d6fd2aa7bd24
Mathieu Boulianne <mathieu.boulianne1@gmail.com>
2018-05-15 15:08:35 -0400

    Add icon and icon-file sandbox.  First draft

--- src/components/sandbox/index.ts
+++ src/components/sandbox/index.ts
@@ -1,9 +1,13 @@
 import Vue, { PluginObject } from 'vue';
 
+import IconFileSandboxPlugin from '../icon-file/icon-file-sandbox';
+import IconSandboxPlugin from '../icon/icon-sandbox';
 import RadioSandboxPlugin from '../radio/radio-sandbox';
 
 const SandboxesPlugin: PluginObject<any> = {
     install(v, options): void {
+        Vue.use(IconFileSandboxPlugin);
+        Vue.use(IconSandboxPlugin);
         Vue.use(RadioSandboxPlugin);
     }
 };
