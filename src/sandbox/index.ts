1209c32b1f1106d5ce7aa1dc3eb4033db2d5f19f
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2018-05-16 16:05:52 -0400

    Add textfield sandbox

--- src/sandbox/index.ts
+++ src/sandbox/index.ts
@@ -3,12 +3,14 @@ import Vue, { PluginObject } from 'vue';
 import IconFileSandboxPlugin from '../components/icon-file/icon-file.sandbox';
 import IconSandboxPlugin from '../components/icon/icon.sandbox';
 import RadioSandboxPlugin from '../components/radio/radio.sandbox';
+import TextfieldSandboxPlugin from '../components/textfield/textfield.sandbox';
 
 const SandboxesPlugin: PluginObject<any> = {
     install(v, options): void {
         Vue.use(IconFileSandboxPlugin);
         Vue.use(IconSandboxPlugin);
         Vue.use(RadioSandboxPlugin);
+        Vue.use(TextfieldSandboxPlugin);
     }
 };
 
