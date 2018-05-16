8c8ad8c88888ef71610a9dfa14caa87ce6052a98
Mathieu Boulianne <mathieu.boulianne1@gmail.com>
2018-05-16 10:10:24 -0400

    Repair build

--- tests/app/router.ts
+++ tests/app/router.ts
@@ -11,7 +11,7 @@ Vue.use(Router);
 
 type RouterFactoryFn = () => Router;
 
-const routerFactory: () => void = () => {
+const routerFactory: () => Router = () => {
     const componentRoutes: RouteConfig[] = [
         {
             path: '/',
