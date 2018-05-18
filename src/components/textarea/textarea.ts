abe8b1053458ca3ecb602191d66f084b08a91ae0
Martin Simard <martin.simard@dti.ulaval.ca>
2018-05-18 11:39:56 -0400

    remove Watch import

--- src/components/textarea/textarea.ts
+++ src/components/textarea/textarea.ts
@@ -1,6 +1,6 @@
 import { PluginObject } from 'vue';
 import Component from 'vue-class-component';
-import { Prop, Watch } from 'vue-property-decorator';
+import { Prop } from 'vue-property-decorator';
 import { ElementQueries } from '../../mixins/element-queries/element-queries';
 import { InputLabel } from '../../mixins/input-label/input-label';
 import { InputManagement, InputManagementData } from '../../mixins/input-management/input-management';
@@ -11,6 +11,7 @@ import { TEXTAREA_NAME } from '../component-names';
 import InputStyle from '../input-style/input-style';
 import ValidationMesagePlugin from '../validation-message/validation-message';
 import WithRender from './textarea.html?style=./textarea.scss';
+
 @WithRender
 @Component({
     mixins: [
