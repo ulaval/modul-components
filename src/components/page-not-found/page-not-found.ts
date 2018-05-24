b89f5ce878790f5653725e4da5d95dda45b3a248
Martin Simard <martin.simard@dti.ulaval.ca>
2018-05-24 09:59:09 -0400

    Fix missing plugins

--- src/components/page-not-found/page-not-found.ts
+++ src/components/page-not-found/page-not-found.ts
@@ -1,6 +1,9 @@
 import Vue, { PluginObject } from 'vue';
 import Component from 'vue-class-component';
 import WithRender from './page-not-found.html';
+import I18nPlugin from '../i18n/i18n';
+import LinkPlugin from '../link/link';
+import MessagePlugin from '../message/message';
 import { Prop } from 'vue-property-decorator';
 import { PAGE_NOT_FOUND_NAME } from '../component-names';
 
@@ -10,12 +13,15 @@ export class MPageNotFound extends Vue {
     @Prop({
         default: () => Vue.prototype.$i18n.translate('m-page-not-found:back-to-portal')
     })
-    public backToLabel: string | undefined;
+    public backToLabel: string;
 }
 
 const PageNotFoundPlugin: PluginObject<any> = {
     install(v, options): void {
         v.prototype.$log.debug(PAGE_NOT_FOUND_NAME, 'plugin.install');
+        v.use(I18nPlugin);
+        v.use(LinkPlugin);
+        v.use(MessagePlugin);
         v.component(PAGE_NOT_FOUND_NAME, MPageNotFound);
     }
 };
--- src/components/session-expired/session-expired.ts
+++ src/components/session-expired/session-expired.ts
@@ -1,6 +1,9 @@
 import Vue, { PluginObject } from 'vue';
 import Component from 'vue-class-component';
 import WithRender from './session-expired.html';
+import I18nPlugin from '../i18n/i18n';
+import LinkPlugin from '../link/link';
+import MessagePlugin from '../message/message';
 import { Prop } from 'vue-property-decorator';
 import { SESSION_EXPIRED_NAME } from '../component-names';
 
@@ -10,12 +13,15 @@ export class MSessionExpired extends Vue {
     @Prop({
         default: () => Vue.prototype.$i18n.translate('m-session-expired:back-to-portal')
     })
-    public backToLabel: string | undefined;
+    public backToLabel: string;
 }
 
 const SessionExpiredPlugin: PluginObject<any> = {
     install(v, options): void {
         v.prototype.$log.debug(SESSION_EXPIRED_NAME, 'plugin.install');
+        v.use(I18nPlugin);
+        v.use(LinkPlugin);
+        v.use(MessagePlugin);
         v.component(SESSION_EXPIRED_NAME, MSessionExpired);
     }
 };
