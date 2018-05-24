ecfb8a2fb1a226147865e5450b5e544ec2052d7e
Mathieu Boulianne <mathieu.boulianne1@gmail.com>
2018-05-24 15:13:33 -0400

    review

--- tests/app/viewer.ts
+++ tests/app/viewer.ts
@@ -1,14 +1,11 @@
 import Vue from 'vue';
-import Component from 'vue-class-component';
 import { Watch } from 'vue-property-decorator';
 
 import WithRender from './viewer.html';
 
 @WithRender
-@Component
 export class Viewer extends Vue {
     public tag: string = '';
-    public model: string = '';
 
     public mounted(): void {
         this.buildTag();
