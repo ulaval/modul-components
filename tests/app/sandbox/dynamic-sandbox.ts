aa6b9433da73ec243fa9046d723f4d0719c2726c
Mathieu Boulianne <mathieu.boulianne1@gmail.com>
2018-05-24 14:26:40 -0400

    working dynamic sandbox

--- tests/app/sandbox/dynamic-sandbox.ts
+++ tests/app/sandbox/dynamic-sandbox.ts
@@ -10,28 +10,56 @@ import WithRender from './dynamic-sandbox.html';
 export class DynamicSandbox extends ModulVue {
     @Prop()
     public ignoreProps: string[];
+    @Prop()
+    public tag: string | undefined;
 
     public showSandbox: boolean = false;
     public meta: any = {};
     public data: any = {};
 
-    protected mounted(): void {
-        const defaultSlot: VNode = this.$slots.default ? this.$slots.default[0] : this.$scopedSlots.default(this.data) as any;
-        const componentOptions: VNodeComponentOptions | undefined = defaultSlot.componentOptions;
-        const componentTag: string = componentOptions && componentOptions.tag ? componentOptions.tag : '';
+    protected hasErrors: boolean = false;
 
-        if (defaultSlot && componentOptions !== undefined && componentTag.includes('m-')) {
-            componentOptions.propsData = { disabled: false };
+    protected mounted(): void {
+        if (this.hasSpecificTemplate() && !this.hasTagProp()) {
+            const defaultSlot: VNode = this.$slots.default ? this.$slots.default[0] : this.$scopedSlots.default(this.data) as any;
+            const componentOptions: VNodeComponentOptions | undefined = defaultSlot.componentOptions;
+            const componentTag: string = componentOptions && componentOptions.tag ? componentOptions.tag : '';
+
             this.buildDynamicSandbox(componentTag);
-            this.showSandbox = true;
+            this.fillSlots(componentTag);
+        } else if (this.hasTagProp() && !this.hasSpecificTemplate()) {
+            this.buildDynamicSandbox(this.tag as string);
+            this.fillSlots(this.tag);
         } else {
-            console.error('dynamic-sandbox expected a slot of length 1 of type ModulVue.');
+            this.hasErrors = true;
+            console.error('dynamic-sandbox expected a slot of length 1 of type ModulVue OR a defined tag property.');
         }
     }
 
+    protected assignArrayProp(propName: string, value: string): void {
+        this.data[propName] = value.split(',').map(value => {
+            value = value.replace(',', '');
+            value = value.trim();
+            return value;
+        });
+    }
+
+    private hasSpecificTemplate(): boolean {
+        if (!Object.keys(this.$slots).length && !Object.keys(this.$scopedSlots).length) { return false; }
+
+        const defaultSlot: VNode = Object.keys(this.$slots).length ? this.$slots.default[0] : this.$scopedSlots.default(this.data) as any;
+        const componentOptions: VNodeComponentOptions | undefined = defaultSlot.componentOptions;
+        const componentTag: string = componentOptions && componentOptions.tag ? componentOptions.tag : '';
+
+        return defaultSlot && componentOptions !== undefined && componentTag.includes('m-');
+    }
+
+    private hasTagProp(): boolean { return this.tag !== undefined; }
+
     private buildDynamicSandbox(tag: string): void {
         const componentName: string = tag.replace('m-', '');
-        this.meta = require(`../../../dist/components/${componentName}/${componentName}.meta.json`);
+        this.meta = require(`!!./../../../dist/components/${componentName}/${componentName}.meta.json`);
+
         if (this.meta.mixins) {
             const mixins: any[] = this.meta.mixins.map(mixin => {
                 return require(`../../../dist/mixins/${this.toKebabCase(mixin)}/${this.toKebabCase(mixin)}.meta.json`);
@@ -44,10 +72,27 @@ export class DynamicSandbox extends ModulVue {
             delete this.meta.mixins;
         }
 
-        const testComponentVue: Vue = (this.$el.children[0] as any).__vue__ as Vue;
+        const testComponentVue: Vue = this.getTestComponentVue();
         Object.keys(this.meta.attributes).forEach(key => {
             this.$set(this.data, key, testComponentVue[key]);
         });
+    }
+
+    private fillSlots(tag): void {
+        const componentName: string = tag.replace('m-', '');
+        const testComponentTemplate: string = require(`!html-loader!./../../../dist/components/${componentName}/${componentName}.html`);
+        const testComponentVue: Vue = this.getTestComponentVue();
+
+        const slotRegex: RegExp = /<slot(?: name=")?([a-zA-Z]*)"?>/g;
+        let match: RegExpExecArray | null = slotRegex.exec(testComponentTemplate);
+        while (match) {
+            const slotName: string = match[1] || 'default';
+            if (!testComponentVue.$slots[slotName]) {
+                testComponentVue.$slots[slotName] = [this.$createElement('div', `slot ${slotName}`)];
+            }
+            match = slotRegex.exec(testComponentTemplate);
+        }
+        testComponentVue.$forceUpdate();
     }
 
     private toKebabCase(param: string): string {
@@ -66,5 +111,9 @@ export class DynamicSandbox extends ModulVue {
         }
 
         return str;
+    }
+
+    private getTestComponentVue(): any {
+        return (this.$el.children[0] as any).__vue__ as Vue;
     }
 }
