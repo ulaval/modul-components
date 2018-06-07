e7729394f7b0e14d1108c75b9b46c123a63578cf
Martin Simard <simardo@gmail.com>
2018-06-07 15:43:44 -0400

    refactor error handling (vue parser + http interceptors) (#280)

--- src/utils/http/http.ts
+++ src/utils/http/http.ts
@@ -1,16 +1,19 @@
-import axios, { AxiosInstance, AxiosPromise, AxiosRequestConfig } from 'axios';
+import axios, { AxiosInstance, AxiosPromise, AxiosRequestConfig, AxiosResponse } from 'axios';
 import qs from 'qs/lib';
 import { PluginObject } from 'vue';
 
+import { ErrorHandler } from '../errors/error-handler';
+import { PromiseError } from '../errors/promise-error';
 import * as strUtils from '../str/str';
 import { RequestConfig, RestAdapter } from './rest';
 
 const AUTHORIZATION_HEADER: string = 'Authorization';
 
 export interface HttpPluginOptions {
-    protectedUrls: string[];
-    authorizationFn: () => string;
+    protectedUrls?: string[];
+    authorizationFn?: () => string;
     timeout?: number;
+    createPromiseError?: boolean;
 }
 
 export const NO_TIMEOUT: number = 0;
@@ -22,6 +25,7 @@ export class HttpService implements RestAdapter {
         this.instance = axios.create();
 
         if (this.options) {
+            // timeout
             if (!this.options.timeout) {
                 this.instance.defaults.timeout = 30000;
             } else if (this.options.timeout === NO_TIMEOUT) {
@@ -29,20 +33,37 @@ export class HttpService implements RestAdapter {
             } else {
                 this.instance.defaults.timeout = this.options.timeout;
             }
-            let opt: HttpPluginOptions = this.options;
-            this.instance.interceptors.request.use(config => {
-                opt.protectedUrls.every(url => {
-                    if (strUtils.startsWith(config.url, url)) {
-                        let token: string = opt.authorizationFn();
-                        config.headers = Object.assign({
-                            [AUTHORIZATION_HEADER]: token
-                        });
-                        return false;
-                    }
-                    return true;
+
+            // request interceptor for authorization header
+            if (this.options.protectedUrls && this.options.authorizationFn) {
+                let protectedUrls: string[] = this.options.protectedUrls;
+                let authFn: () => string = this.options.authorizationFn;
+                this.instance.interceptors.request.use(config => {
+                    protectedUrls.every(url => {
+                        if (strUtils.startsWith(config.url, url)) {
+                            let token: string = authFn();
+                            config.headers = Object.assign({
+                                [AUTHORIZATION_HEADER]: token
+                            });
+                            return false;
+                        }
+                        return true;
+                    });
+                    return config;
                 });
-                return config;
-            });
+            }
+
+            // response interceptor to progagate PromiseError
+            if (this.options.createPromiseError === undefined || this.options.createPromiseError) {
+                this.instance.interceptors.response.use((response: AxiosResponse) => response, (error: Error) => {
+                    // wrap to a PromiseError, so that propagation can be stopped
+                    let promiseError: PromiseError = new PromiseError(error);
+                    // delay onError
+                    ErrorHandler.onError(promiseError, true);
+
+                    return Promise.reject(promiseError);
+                });
+            }
         }
     }
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,25 +1,32 @@
-import Vue, { PluginObject } from 'vue';
-
-import MediaQueriesPlugin from './media-queries/media-queries';
-import ModulPlugin from './modul/modul';
-import I18nPlugin, { I18nPluginOptions } from './i18n/i18n';
-import HttpPlugin, { HttpPluginOptions } from './http/http';
-import SpritesPlugin from './svg/sprites';
-import * as TouchPlugin from 'vue-touch';
-import ConfirmPlugin from './modal/confirm';
-import AlertPlugin from './modal/alert';
-import FilePlugin from '../utils/file/file';
 import { PortalPluginInstall } from 'portal-vue';
+import Vue, { PluginObject } from 'vue';
+import * as TouchPlugin from 'vue-touch';
+
+import FilePlugin from '../utils/file/file';
+import { ErrorHandler } from './errors/error-handler';
+import HttpPlugin, { HttpPluginOptions } from './http/http';
+import I18nPlugin, { I18nPluginOptions } from './i18n/i18n';
 import LoggerPlugin, { ConsoleOptions } from './logger/logger';
+import MediaQueriesPlugin from './media-queries/media-queries';
+import AlertPlugin from './modal/alert';
+import ConfirmPlugin from './modal/confirm';
+import ModulPlugin from './modul/modul';
+import SpritesPlugin from './svg/sprites';
 
 export interface UtilsPluginOptions {
     httpPluginOptions?: HttpPluginOptions;
     consoleOptions?: ConsoleOptions;
     i18PluginOptions?: I18nPluginOptions;
+    propagateVueParserErrors?: boolean;
 }
 
 const UtilsPlugin: PluginObject<any> = {
     install(v, options): void {
+        if (!options || options.propagateVueParserErrors === undefined || options.propagateVueParserErrors) {
+            // Vue parser errors do not propagate to window.onError by default
+            Vue.config.errorHandler = (err, vm, info) => ErrorHandler.onError(err);
+        }
+
         if (!v.prototype.$log) {
             Vue.use(LoggerPlugin, options ? options.consoleOptions : undefined);
         } else if (options) {
