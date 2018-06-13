4e3e5f6f5579556f2213797bbee8ec4900f0467a
Jonathan Matte <jonathan.matte@dti.ulaval.ca>
2018-06-13 12:54:14 -0400

    MODUL-165 initialize props

--- src/components/tree/tree.ts
+++ src/components/tree/tree.ts
@@ -1,14 +1,47 @@
-import { PluginObject } from 'vue';
+import Vue, { PluginObject } from 'vue';
 import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
 
 import { ModulVue } from '../../utils/vue/vue';
 import { TREE_NAME } from '../component-names';
 import IconFilePlugin from '../icon-file/icon-file';
 import WithRender from './tree.html';
 
+export interface MNodeStructureArchive extends Vue {
+    relativePath: string;
+    idFile: string;
+    fileName: string;
+    childs: MNodeStructureArchive[];
+}
+
+export enum MSelectOption {
+    NONE = 0,
+    SINGLE = 1,
+    MULTIPLE = 2
+}
+
 @WithRender
 @Component
 export class MTree extends ModulVue {
+
+    @Prop()
+    tree: MNodeStructureArchive[];
+
+    @Prop({
+        default: MSelectOption.NONE,
+        validator: value =>
+            value === MSelectOption.NONE ||
+            value === MSelectOption.SINGLE ||
+            value === MSelectOption.MULTIPLE
+    })
+    selected: MSelectOption;
+
+    @Prop()
+    icon: string;
+
+    created(): void {
+        console.log(this.selected);
+    }
 
 }
 
