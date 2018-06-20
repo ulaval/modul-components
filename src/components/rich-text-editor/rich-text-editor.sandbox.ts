1a6a6017586a57995d98d4f473b5100166eeaff3
gauthier bramardi <gauthier.bramardi@dti.ulaval.ca>
2018-06-20 14:53:20 -0400

    faire fonctionner fontawesome

---
+++ src/components/rich-text-editor/rich-text-editor.sandbox.ts
@@ -0,0 +1,17 @@
+import Vue, { PluginObject } from 'vue';
+import { Component } from 'vue-property-decorator';
+
+import { RICH_TEXT_EDITOR_NAME } from '../component-names';
+import WithRender from './rich-text-editor.sandbox.html';
+
+@WithRender
+@Component
+export class MRichTextEditorSandBox extends Vue {}
+
+const RichTextEditorSandBoxPlugin: PluginObject<any> = {
+    install(v, options): void {
+        v.component(`${RICH_TEXT_EDITOR_NAME}-sandbox`, MRichTextEditorSandBox);
+    }
+};
+
+export default RichTextEditorSandBoxPlugin;
--- src/components/rich-text-editor/rich-text-editor.ts
+++ src/components/rich-text-editor/rich-text-editor.ts
@@ -1,22 +1,27 @@
+/* tslint:disable:no-console */
 import { PluginObject } from 'vue';
 import Component from 'vue-class-component';
 import VueFroala from 'vue-froala-wysiwyg';
 import { Prop } from 'vue-property-decorator';
 
 import { ModulVue } from '../../utils/vue/vue';
-import { RICH_TEXT_EDITOR } from './../component-names';
+import { RICH_TEXT_EDITOR_NAME } from './../component-names';
 import WithRender from './rich-text-editor.html?style=./rich-text-editor.scss';
+
+// Import fontawesome
+require('@fortawesome/fontawesome');
+require('@fortawesome/fontawesome-free-solid');
 
 // Import froala editor.
 require('froala-editor/js/froala_editor.pkgd.min');
 require('froala-editor/css/froala_editor.pkgd.min.css');
-require('font-awesome/css/font-awesome.css');
 require('froala-editor/css/froala_style.min.css');
 
 export interface MRichTextEditorOptions {}
 
 class MRichTextEditorDefaultOptions {
     public immediateVueModelUpdate: boolean = true;
+    public iconsTemplate: string = 'font_awesome_5';
 }
 
 @WithRender
@@ -40,7 +45,7 @@ export class MRichTextEditor extends ModulVue {
 const RichTextEditorPlugin: PluginObject<any> = {
     install(v, options): void {
         v.use(VueFroala);
-        v.component(RICH_TEXT_EDITOR, MRichTextEditor);
+        v.component(RICH_TEXT_EDITOR_NAME, MRichTextEditor);
     }
 };
 
