0465dcc2f30f49ec2a34c7553fe046982d2c9d91
Mathieu Boulianne <mathieu.boulianne1@gmail.com>
2018-06-22 05:59:36 -0400

    Enable bottom autoscroll when dragging (#299)
    * Enable bottom autoscroll when dragging
    
    * fix enum code style

--- src/directives/draggable/draggable-allow-scroll.ts
+++ src/directives/draggable/draggable-allow-scroll.ts
@@ -1,35 +1,47 @@
-import { DirectiveOptions, PluginObject, VNode, VNodeDirective } from 'vue';
+import { DirectiveOptions, PluginObject, VNodeDirective } from 'vue';
 
+import { mousePositionElement } from '../../utils/mouse/mouse';
 import { polyFillActive } from '../../utils/polyfills';
 import { DRAGGABLE_ALLOW_SCROLL_NAME } from '../directive-names';
 import { MDOMPlugin, MElementDomPlugin, MountFunction, RefreshFunction } from '../domPlugin';
 import { ScrollTo } from '../scroll-to/scroll-to-lib';
 import { CancelableScrollTo, ScrollToDuration } from './../scroll-to/scroll-to-lib';
 
-interface ScrollPosition { left: number; top: number; }
+export enum MDraggableAllowScrollDirection {
+    Top,
+    Bottom
+}
+
+export interface MDraggableAllowScrollOptions {
+    allowScroll: boolean;
+    scrollDirection: MDraggableAllowScrollDirection;
+}
 
 // Naive implementation for a single use case: allow autoscroll on a fixed element that appears at the absolute top of the viewport.
 // Furthermore, the user must be dragging something for it to work.
-export class MDraggableAllowScroll extends MElementDomPlugin<boolean> {
+export class MDraggableAllowScroll extends MElementDomPlugin<MDraggableAllowScrollOptions> {
     public static currentDraggableScroll?: MDraggableAllowScroll;
     public static defaultMountPoint: string = '__mdraggableallowscroll__';
 
     private activeScroll: CancelableScrollTo | undefined;
 
     public attach(mount: MountFunction): void {
+        if (polyFillActive.dragDrop) { return; }
         // This is cheesy but we only need to fix the scrolling behavior if the polyfill is not active.
         // The polyfill already includes a scroll behavior for mobile devices.  However, it doesn't allow to enable the scrolling behavior for desktops.
-        if (polyFillActive.dragDrop) { return; }
-
-        if (this._options === undefined) { this._options = true; }
-        mount(() => {
-            this.addEventListener('dragover', (event: DragEvent) => this.handleScroll(event));
-        });
+        this.cleanUpOptions();
+        if (this.options.allowScroll) {
+            mount(() => {
+                this.addEventListener('dragover', (event: DragEvent) => this.handleScroll(event));
+            });
+        }
     }
 
-    public update(options: boolean, refresh: RefreshFunction): void {
-        if (options === undefined) { options = true; }
-        refresh(() => this._options = options);
+    public update(options: MDraggableAllowScrollOptions, refresh: RefreshFunction): void {
+        this.cleanUpOptions();
+        if (this.options.allowScroll) {
+            refresh(() => this._options = options);
+        }
     }
 
     public detach(): void {
@@ -41,23 +53,53 @@ export class MDraggableAllowScroll extends MElementDomPlugin<boolean> {
         this.cancelScroll();
     }
 
-    private handleScroll(event: DragEvent): void {
-        const scrollThreshold: number = 20;
+    private cleanUpOptions(): void {
+        if (this._options === undefined) { this._options = { allowScroll: true, scrollDirection: MDraggableAllowScrollDirection.Top }; }
+        if (this._options.allowScroll === undefined) { this._options.allowScroll = true; }
+        if (this._options.scrollDirection === undefined) { this._options.scrollDirection = MDraggableAllowScrollDirection.Top; }
+    }
 
+    private handleScroll(event: DragEvent): void {
         const watchMouseMove: (event: MouseEvent) => void = (event: MouseEvent) => {
-            if (event.clientY >= scrollThreshold && this.activeScroll) {
+            if ((!this.shouldScroll(event) && this.activeScroll) || MDraggableAllowScroll.currentDraggableScroll !== this) {
                 if (this.activeScroll) {
-                    document.removeEventListener('dragover', watchMouseMove);
                     this.cancelScroll();
                 }
-            } else if (!this.activeScroll) {
-                this.activeScroll = new ScrollTo().scrollToTop(document.documentElement, ScrollToDuration.Slower);
-            }
+                document.removeEventListener('dragover', watchMouseMove);
+            } else if (!this.activeScroll) { this.activateScroll(); }
         };
 
-        if (event.clientY < scrollThreshold && window.getComputedStyle(this.element).position === 'fixed') {
+        if (this.shouldScroll(event) && window.getComputedStyle(this.element).position === 'fixed') {
+            if (MDraggableAllowScroll.currentDraggableScroll && MDraggableAllowScroll.currentDraggableScroll !== this) {
+                MDraggableAllowScroll.currentDraggableScroll.doCleanUp();
+                MDraggableAllowScroll.currentDraggableScroll = undefined;
+                return;
+            }
+
             MDraggableAllowScroll.currentDraggableScroll = this;
             document.addEventListener('dragover', watchMouseMove);
+        }
+    }
+
+    private shouldScroll(event: MouseEvent): boolean {
+        const scrollThreshold: number = 20;
+
+        switch (this.options.scrollDirection) {
+            case MDraggableAllowScrollDirection.Top: return event.clientY < scrollThreshold;
+            case MDraggableAllowScrollDirection.Bottom: return mousePositionElement(event, this.element).y >= this.element.offsetHeight - scrollThreshold;
+            default: throw new Error(`Unhandled value for scrollDirection: ${this.options.scrollDirection}`);
+        }
+    }
+
+    private activateScroll(): void {
+        switch (this.options.scrollDirection) {
+            case MDraggableAllowScrollDirection.Top:
+                this.activeScroll = new ScrollTo().scrollToTop(document.documentElement, ScrollToDuration.Slower);
+                break;
+            case MDraggableAllowScrollDirection.Bottom:
+                this.activeScroll = new ScrollTo().scrollToBottom(document.documentElement, ScrollToDuration.Slower);
+                break;
+            default: throw new Error(`Unhandled value for scrollDirection: ${this.options.scrollDirection}`);
         }
     }
 
@@ -68,29 +110,29 @@ export class MDraggableAllowScroll extends MElementDomPlugin<boolean> {
             this.doCleanUp();
         }
     }
-
-    private getViewportScrollPosition(): ScrollPosition {
-        const doc: HTMLElement = document.documentElement;
-        const left: number = doc.scrollLeft - (doc.clientLeft || 0);
-        const top: number = doc.scrollTop - (doc.clientTop || 0);
-        return { left, top };
-    }
 }
 
+const extractOptions: (binding: VNodeDirective) => MDraggableAllowScrollOptions = (binding: VNodeDirective) => {
+    return {
+        allowScroll: binding.value as boolean,
+        scrollDirection: MDraggableAllowScrollDirection[binding.arg.toUpperCase() as keyof typeof MDraggableAllowScrollDirection]
+    };
+};
+
 const Directive: DirectiveOptions = {
-    inserted(element: HTMLElement, binding: VNodeDirective, node: VNode): void {
-        MDOMPlugin.attach(MDraggableAllowScroll, element, binding.value);
+    inserted(element: HTMLElement, binding: VNodeDirective): void {
+        MDOMPlugin.attach(MDraggableAllowScroll, element, extractOptions(binding));
     },
-    update(element: HTMLElement, binding: VNodeDirective, node: VNode): void {
-        MDOMPlugin.attach(MDraggableAllowScroll, element, binding.value);
+    update(element: HTMLElement, binding: VNodeDirective): void {
+        MDOMPlugin.attach(MDraggableAllowScroll, element, extractOptions(binding));
     },
-    unbind(element: HTMLElement, binding: VNodeDirective): void {
+    unbind(element: HTMLElement): void {
         MDOMPlugin.detach(MDraggableAllowScroll, element);
     }
 };
 
 const DraggableAllowScrollPlugin: PluginObject<any> = {
-    install(v, options): void {
+    install(v): void {
         v.directive(DRAGGABLE_ALLOW_SCROLL_NAME, Directive);
     }
 };
