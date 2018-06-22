8b43b5354f18459c5b63eaf0bc3c522f716b7d44
Martin Simard <simardo@gmail.com>
2018-06-22 06:01:02 -0400

    Fix WindowErrorHandler window.onerror might be undefined (#300)
    * error-message missing plugin
    
    * rename ErrorHandler to WindowErrorHandler
    
    * refactor PromiseError to ErrorEvent

--- src/utils/errors/window-error-handler.ts
+++ src/utils/errors/window-error-handler.ts
@@ -1,14 +1,16 @@
-import { PromiseError } from './promise-error';
-
-export class ErrorHandler {
+export class WindowErrorHandler {
     /**
      * Calls the window.onerror function. Use this method for errors that cannot be handled by the window.onerror standard handler.
      * Mostly, this method will be used to provide retroaction on Promise errors.
      * @param error The unhandled error.
      * @param defer True will delay the execution of the call to the next cyle, allowing, for example, stopping the propagation of the error in the promise chain. Default is false.
      */
-    public static onError(error: Error | PromiseError, defer: boolean = false): void {
-        let call: () => any = () => window.onerror.call(this, error);
+    public static onError(error: ErrorEvent, defer: boolean = false): void {
+        let call: () => any = () => {
+            if (!error.cancelBubble) {
+                window.dispatchEvent(error);
+            }
+        };
         defer ? setTimeout(() => call(), 0) : call();
     }
 }
