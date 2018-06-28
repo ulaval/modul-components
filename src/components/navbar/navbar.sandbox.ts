0f99f6c0b5419236a981475f2b6866deaf1baad5
Philippe Rail <philipperail@gmail.com>
2018-06-28 14:19:20 -0400

    Quick clean up

--- src/components/navbar/navbar.sandbox.ts
+++ src/components/navbar/navbar.sandbox.ts
@@ -7,22 +7,7 @@ import WithRender from './navbar.sandbox.html';
 @WithRender
 @Component
 export class MNavbarSandbox extends Vue {
-
     public selectedItem = 'item4';
-    public item1 = 'item1';
-    public showMenu: boolean = false;
-
-    update(value, event): void {
-        if (value === 'item3') {
-            this.showMenu = true;
-        } else {
-            this.showMenu = false;
-        }
-    }
-
-    private get test(): string {
-        return this.item1;
-    }
 }
 
 const NavbarSandboxPlugin: PluginObject<any> = {
