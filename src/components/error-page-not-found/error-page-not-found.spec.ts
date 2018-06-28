62c8103030cac585b1dcc5e991ffa9e8ba116078
jean-sebastien roy <jean-sebastien.roy@dti.ulaval.ca>
2018-06-28 10:15:18 -0400

    add snapshot test for page not found

--- src/components/error-page-not-found/error-page-not-found.spec.ts
+++ src/components/error-page-not-found/error-page-not-found.spec.ts
@@ -1,9 +1,49 @@
-describe(``, () => {
-    describe(``, () => {
-        describe(``, () => {
-            it(``, () => {
+import { shallow, Wrapper } from '@vue/test-utils';
 
+import { renderComponent } from '../../../tests/helpers/render';
+import { Link } from '../error-template/error-template';
+import { MErrorPageNotFound } from './error-page-not-found';
+
+let wrapper: Wrapper<MErrorPageNotFound>;
+
+const getStubs: any = () => {
+    return {
+        ['m-error-template']: '<div><slot /></div>',
+    };
+};
+
+const initializeShallowWrapper: any = () => {
+    wrapper = shallow(MErrorPageNotFound, {
+        stubs: getStubs(),
+        propsData: {
+        }
+    });
+};
+
+describe(`Page not found - test`, () => {
+    describe(`Given default values`, () => {
+        it(`Should render with default values`, async () => {
+            wrapper = shallow(MErrorPageNotFound, { stubs: getStubs() });
+
+            await expect(renderComponent(wrapper.vm)).resolves.toMatchSnapshot();
+        });
+    });
+    describe(`Given custom values`, () => {
+        it(`Should render with custom values provided`, async () => {
+            const A_CUSTOM_TITLE: string = 'An error title.';
+            const A_HINT: string = 'aHint';
+            const A_LINK: Link = new Link('aLabel', 'anUrl');
+
+            wrapper = shallow(MErrorPageNotFound, {
+                stubs: getStubs(),
+                propsData: {
+                    title: A_CUSTOM_TITLE,
+                    hints: [A_HINT],
+                    links: [A_LINK]
+                }
             });
+
+            await expect(renderComponent(wrapper.vm)).resolves.toMatchSnapshot();
         });
     });
 });
