76bbb264352c5592696a8097270c86efbe021459
Mathieu Boulianne <mathieu.boulianne1@gmail.com>
2018-07-17 10:35:59 -0400

    Remove unwanted options

--- src/components/rich-text-edit/rich-text-edit-options.ts
+++ src/components/rich-text-edit/rich-text-edit-options.ts
@@ -3,15 +3,13 @@ import faAlignCenter from '@fortawesome/fontawesome-free-solid/faAlignCenter';
 import faAlignJustify from '@fortawesome/fontawesome-free-solid/faAlignJustify';
 import faAlignLeft from '@fortawesome/fontawesome-free-solid/faAlignLeft';
 import faAlignRight from '@fortawesome/fontawesome-free-solid/faAlignRight';
-import faArrowsAltV from '@fortawesome/fontawesome-free-solid/faArrowsAltV';
-import faBars from '@fortawesome/fontawesome-free-solid/faBars';
+import faArrowLeft from '@fortawesome/fontawesome-free-solid/faArrowLeft';
 import faBold from '@fortawesome/fontawesome-free-solid/faBold';
 import faCompress from '@fortawesome/fontawesome-free-solid/faCompress';
 import faEdit from '@fortawesome/fontawesome-free-solid/faEdit';
 import faEraser from '@fortawesome/fontawesome-free-solid/faEraser';
 import faExpand from '@fortawesome/fontawesome-free-solid/faExpand';
 import faExternalLinkAlt from '@fortawesome/fontawesome-free-solid/faExternalLinkAlt';
-import faHeading from '@fortawesome/fontawesome-free-solid/faHeading';
 import faIndent from '@fortawesome/fontawesome-free-solid/faIndent';
 import faItalic from '@fortawesome/fontawesome-free-solid/faItalic';
 import faLink from '@fortawesome/fontawesome-free-solid/faLink';
@@ -21,14 +19,11 @@ import faMousePointer from '@fortawesome/fontawesome-free-solid/faMousePointer';
 import faOutdent from '@fortawesome/fontawesome-free-solid/faOutdent';
 import faParagraph from '@fortawesome/fontawesome-free-solid/faParagraph';
 import faQuestion from '@fortawesome/fontawesome-free-solid/faQuestion';
-import faQuoteLeft from '@fortawesome/fontawesome-free-solid/faQuoteLeft';
 import faRedo from '@fortawesome/fontawesome-free-solid/faRedo';
 import faSearch from '@fortawesome/fontawesome-free-solid/faSearch';
-import faSquare from '@fortawesome/fontawesome-free-solid/faSquare';
 import faStrikethrough from '@fortawesome/fontawesome-free-solid/faStrikethrough';
 import faSubscript from '@fortawesome/fontawesome-free-solid/faSubscript';
 import faSuperScript from '@fortawesome/fontawesome-free-solid/faSuperScript';
-import faTable from '@fortawesome/fontawesome-free-solid/faTable';
 import faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
 import faTrash from '@fortawesome/fontawesome-free-solid/faTrash';
 import faUnderline from '@fortawesome/fontawesome-free-solid/faUnderline';
@@ -36,8 +31,8 @@ import faUndo from '@fortawesome/fontawesome-free-solid/faUndo';
 import faUnlink from '@fortawesome/fontawesome-free-solid/faUnlink';
 
 library.add(faExpand, faCompress, faBold, faItalic, faUnderline, faStrikethrough, faSubscript, faSuperScript, faParagraph, faAlignLeft, faAlignCenter,
-    faAlignRight, faAlignJustify, faListOl, faListUl, faOutdent, faIndent, faQuoteLeft, faLink, faExternalLinkAlt, faEdit, faUnlink, faSearch, faTable, faHeading, faTrash, faBars,
-    faSquare, faArrowsAltV, faTimes, faMousePointer, faEraser, faQuestion, faUndo, faRedo);
+    faAlignRight, faAlignJustify, faListOl, faListUl, faOutdent, faIndent, faLink, faExternalLinkAlt, faEdit, faUnlink, faSearch, faTrash,
+    faTimes, faMousePointer, faEraser, faQuestion, faUndo, faRedo, faArrowLeft);
 
 export abstract class MRichTextEditorDefaultOptions {
     public immediateVueModelUpdate: boolean = true;
@@ -49,6 +44,8 @@ export abstract class MRichTextEditorDefaultOptions {
     public toolbarSticky: boolean = true;
     public scrollableContainer: string = 'body';
     public toolbarStickyOffset: number = 0;
+    public wordDeniedTags: string[] = ['img'];
+    public pluginsEnabled: string[] = ['align', 'draggable', 'embedly', 'entities', 'file', 'fontFamily', 'fontSize', 'fullscreen', 'inlineStyle', 'lineBreaker', 'link', 'lists', 'paragraphFormat', 'paragraphStyle', 'quickInsert', 'quote', 'save', 'table', 'url', 'wordPaste'];
 
     constructor(public key: string, public language: string = 'en_CA') {
         if (!key) {
@@ -59,11 +56,8 @@ export abstract class MRichTextEditorDefaultOptions {
 
 export class MRichTextEditorStandardOptions extends MRichTextEditorDefaultOptions {
     public toolbarButtons: string[] = ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'paragraphFormat', 'align', 'formatOL',
-        'formatUL', 'outdent', 'indent', 'quote', '|', 'insertLink', 'insertTable', '|', 'specialCharacters', 'selectAll', 'clearFormatting', '|',
+        'formatUL', 'outdent', 'indent', '|', 'insertLink', '|', 'specialCharacters', 'selectAll', 'clearFormatting', '|',
         'help', '|', 'undo', 'redo' ];
-
-    public tableEditButtons: string[] = ['tableHeader', 'tableRemove', '|', 'tableRows', 'tableColumns', '|', 'tableCells',
-        'tableCellVerticalAlign', 'tableCellHorizontalAlign'];
 
     public linkEditButtons: string[] = ['linkOpen', 'linkEdit', 'linkRemove'];
 
