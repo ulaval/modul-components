f972d466f712f09942b81b50db6fe5a15eba01c6
Jonathan Matte <jonathan.matte@dti.ulaval.ca>
2018-07-19 15:32:49 -0400

    MODUL-165 - Tests for the open/close all

--- src/components/tree/tree.spec.ts
+++ src/components/tree/tree.spec.ts
@@ -6,9 +6,12 @@ import { MTree, MTreeFormat, TreeNode } from './tree';
 const EMPTY_TREE_REF: RefSelector = { ref: 'empty-tree-txt' };
 const ERROR_TREE_REF: RefSelector = { ref: 'error-tree-txt' };
 const TREE_NODE_REF: RefSelector = { ref: 'tree-node' };
+const BUTTON_TOGGLE_VISIBILITY_REF: RefSelector = { ref: 'button-toggle-visibility' };
 
 const TXT_EMPTY_TREE: string = 'm-tree:empty';
 const TXT_ERROR_TREE: string = 'm-tree:error';
+const TXT_VISIBILITY_TREE_OPEN: string = 'm-tree:all-open';
+const TXT_VISIBILITY_TREE_CLOSE: string = 'm-tree:all-close';
 
 const TREE_NODE_SELECTED: string[] = ['/medias/Videos/video-dog.mov'];
 const NEW_TREE_NODE_SELECTED: string[] = ['/index.html'];
@@ -38,13 +41,15 @@ const TREE_WITH_DATA: TreeNode<MTreeFormat>[] = [
 ];
 
 let tree: TreeNode<MTreeFormat>[];
+let externalIsAllOpen: boolean = false;
 let wrapper: Wrapper<MTree<MTreeFormat>>;
 
 const initializeShallowWrapper: any = () => {
     wrapper = shallow(MTree, {
         propsData: {
             tree: tree,
-            externalSelectedNode: TREE_NODE_SELECTED
+            externalSelectedNode: TREE_NODE_SELECTED,
+            externalIsAllOpen
         }
     });
 };
@@ -146,6 +151,57 @@ describe(`MTree`, () => {
                 wrapper.find(TREE_NODE_REF).trigger('selectedNodeFound');
 
                 expect(wrapper.vm.selectedNodeFound).toHaveBeenCalled();
+            });
+
+        });
+
+        describe(`When you click on the button to show/hide every nodes`, () => {
+
+            it(`Then call the function toggleAllVisibility`, () => {
+                initializeShallowWrapper();
+
+                wrapper.setMethods({ toggleAllVisibility: jest.fn() });
+                wrapper.find(BUTTON_TOGGLE_VISIBILITY_REF).trigger('click');
+
+                expect(wrapper.vm.toggleAllVisibility).toHaveBeenCalled();
+            });
+
+            describe(`and isAllOpen is false`, () => {
+
+                beforeEach(() => {
+                    externalIsAllOpen = false;
+                    initializeShallowWrapper();
+
+                    wrapper.find(BUTTON_TOGGLE_VISIBILITY_REF).trigger('click');
+                });
+
+                it(`All nodes should be open`, () => {
+                    expect(wrapper.vm.isAllOpen).toBeTruthy();
+                });
+
+                it(`The button label should become "m-tree:all-close"`, () => {
+                    expect(wrapper.vm.treeVisibilityTxt).toEqual(TXT_VISIBILITY_TREE_CLOSE);
+                });
+
+            });
+
+            describe(`and isAllOpen is true`, () => {
+
+                beforeEach(() => {
+                    externalIsAllOpen = true;
+                    initializeShallowWrapper();
+
+                    wrapper.find(BUTTON_TOGGLE_VISIBILITY_REF).trigger('click');
+                });
+
+                it(`All nodes should be hidden`, () => {
+                    expect(wrapper.vm.isAllOpen).toBeFalsy();
+                });
+
+                it(`The button label should become "m-tree:all-open"`, () => {
+                    expect(wrapper.vm.treeVisibilityTxt).toEqual(TXT_VISIBILITY_TREE_OPEN);
+                });
+
             });
 
         });
