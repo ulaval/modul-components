d23148535b0047e300ee030569f94f2c57f8cd0f
Jean-Sébastien Roy <jean-sebastien.roy.5@ulaval.ca>
2018-07-20 13:14:22 -0400

    Feature/modul 206 composante page erreur specifique (#325)
    * MODUL-205 ajouter une composante commune pour les page d'erreur, prenant tous les paramètres requis pour les différents type d'erreur identifié dans le cadre de prisme
    
    * début réflexion sur les tests unitaires - copie et cleanup des tests pour error-message
    
    * MODUL-205 retirer le détail de l'erreur pour les laisser seulement dans l'Erreur technique
    
    * MODUL-205 cleanup indentation
    
    * modifier pour correspondre aux nouvelles maquettes (icone en haut au lieu de message, et titre au lieu du message)
    
    * MODUL-205
    ajouter tests unitaires et compléter les fonctionnalités requises du template
    Manque la taille finale de l'icone.
    arranger l'enum pour matcher avec celle de message
    
    * simplifier tests unitaires et retirer les |undefined dans le .ts
    
    * MODUL-206_débuter pages pour problème technique et pour page not found
    
    * MODUL-206 test unitaires pour error-technical difficulty
    
    * MODUL-205 remettre error-message à ses valeur d'origine pour ne pas briser le code l'utilisant déjà
    
    * MODUL-206 débuter page not found
    
    * MODUL-206 terminer page not found
    
    * add snapshot test for page not found
    
    * ajouter browser not supported, config not supported et access denied
    
    * ajouter page pour cookies désactivés
    
    * MODUL-205 - Composant gabarit de message d'erreur
    
    * remettre icones fonctionnels
    
    * remettre icones fonctionnels suite à update dans modul
    
    * modifier classes
    
    * update snapshot
    
    * ajuster l'ouvertur du link selon si l'url est externe ou non
    
    * MODUL-205 - suite
    
    
    MODUL-205 - quick fix comment
    
    * arranger tests brisés
    
    * fix package-lock
    
    * Apply code review:
    renaming getters
    make getters private
    simplify conditions in getter
    put factory method in default
    
    * modification PR Simon
    
    * fin CR simon O.
    
    * apply code review
    
    * fix package-lock.json

---
+++ src/components/error-cookies-not-supported/error-cookies-not-supported.ts
@@ -0,0 +1,48 @@
+import Vue, { PluginObject } from 'vue';
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+import { ModulVue } from '../../utils/vue/vue';
+import { ERROR_COOKIES_NOT_SUPPORTED_NAME } from '../component-names';
+import ErrorTemplatePlugin, { Link, MErrorTemplateSkin } from '../error-template/error-template';
+import I18nPlugin from '../i18n/i18n';
+import LinkPlugin from '../link/link';
+import WithRender from './error-cookies-not-supported.html';
+
+@WithRender
+@Component
+export class MErrorCookiesNotSupported extends ModulVue {
+
+    @Prop({
+        default: () => (Vue.prototype as any).$i18n.translate('m-error-cookies-not-supported:title')
+    })
+    public title: string;
+
+    @Prop({
+        default: () => [
+            new Link((Vue.prototype as any).$i18n.translate('m-error-cookies-not-supported:home-label'), '\\')]
+    })
+    public links: Link[];
+
+    @Prop({
+        default: () => [
+            (Vue.prototype as any).$i18n.translate('m-error-cookies-not-supported:hint.primary')]
+    })
+    public hints: string[];
+
+    readonly skin: string = MErrorTemplateSkin.Warning;
+
+    readonly iconName: string = 'm-svg__error-filled';
+}
+
+const ErrorCookiesNotSupportedPlugin: PluginObject<any> = {
+    install(v, options): void {
+        v.prototype.$log.debug(ERROR_COOKIES_NOT_SUPPORTED_NAME, 'plugin.install');
+        v.use(I18nPlugin);
+        v.use(LinkPlugin);
+        v.use(ErrorTemplatePlugin);
+        v.component(ERROR_COOKIES_NOT_SUPPORTED_NAME, MErrorCookiesNotSupported);
+    }
+};
+
+export default ErrorCookiesNotSupportedPlugin;
--- src/components/error-template/error-template.sandbox.ts
+++ src/components/error-template/error-template.sandbox.ts
@@ -16,7 +16,7 @@ export class MErrorTemplateSandbox extends Vue {
     oneHint: string[] = ['My only hint'];
     manyHints: string[] = ['My first hint', 'My second long hint.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla egestas urna rhoncus ipsum congue lobortis. Mauris vel neque condimentum, dignissim lectus ac, vehicula justo. Aliquam nunc leo, tristique hendrerit aliquam sagittis, scelerisque quis libero. Integer non augue nec lacus aliquet porttitor in nec lacus. Cras ultrices tellus est, condimentum gravida orci pulvinar quis. Integer eget turpis arcu. Curabitur consequat porta urna, at hendrerit justo consectetur non. Aenean venenatis ornare nulla, a vulputate erat eleifend ut.'];
     oneLink: Link[] = [new Link('the only link', 'http://www.ulaval.ca')];
-    manyLinks: Link[] = [new Link('the first link', 'http://www.ulaval.ca'), new Link('the second link', 'http://www.google.com')];
+    manyLinks: Link[] = [new Link('the first link', 'http://www.ulaval.ca', true), new Link('the second link', 'http://www.google.com', true)];
     errorDate: moment.Moment = moment().subtract(moment.duration(2, 'days'));
     stackTrace: string = `(l1) This is a multiline stack
 (l2) to be displayed
