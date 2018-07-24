0abef50f9e7ff6d5530efe181e72adf58195b98e
gauthier bramardi <gauthier.bramardi@dti.ulaval.ca>
2018-07-24 14:16:01 -0400

    delete useless fonts

--- src/components/rich-text-edit/rich-text-edit-options.ts
+++ src/components/rich-text-edit/rich-text-edit-options.ts
@@ -1,13 +1,8 @@
 import { library } from '@fortawesome/fontawesome';
-import faAlignCenter from '@fortawesome/fontawesome-free-solid/faAlignCenter';
-import faAlignJustify from '@fortawesome/fontawesome-free-solid/faAlignJustify';
-import faAlignLeft from '@fortawesome/fontawesome-free-solid/faAlignLeft';
-import faAlignRight from '@fortawesome/fontawesome-free-solid/faAlignRight';
 import faArrowLeft from '@fortawesome/fontawesome-free-solid/faArrowLeft';
 import faBold from '@fortawesome/fontawesome-free-solid/faBold';
 import faCompress from '@fortawesome/fontawesome-free-solid/faCompress';
 import faEdit from '@fortawesome/fontawesome-free-solid/faEdit';
-import faEraser from '@fortawesome/fontawesome-free-solid/faEraser';
 import faExpand from '@fortawesome/fontawesome-free-solid/faExpand';
 import faExternalLinkAlt from '@fortawesome/fontawesome-free-solid/faExternalLinkAlt';
 import faIndent from '@fortawesome/fontawesome-free-solid/faIndent';
@@ -15,25 +10,17 @@ import faItalic from '@fortawesome/fontawesome-free-solid/faItalic';
 import faLink from '@fortawesome/fontawesome-free-solid/faLink';
 import faListOl from '@fortawesome/fontawesome-free-solid/faListOl';
 import faListUl from '@fortawesome/fontawesome-free-solid/faListUl';
-import faMousePointer from '@fortawesome/fontawesome-free-solid/faMousePointer';
 import faOutdent from '@fortawesome/fontawesome-free-solid/faOutdent';
 import faParagraph from '@fortawesome/fontawesome-free-solid/faParagraph';
 import faPlus from '@fortawesome/fontawesome-free-solid/faPlus';
-import faQuestion from '@fortawesome/fontawesome-free-solid/faQuestion';
-import faRedo from '@fortawesome/fontawesome-free-solid/faRedo';
-import faSearch from '@fortawesome/fontawesome-free-solid/faSearch';
-import faStrikethrough from '@fortawesome/fontawesome-free-solid/faStrikethrough';
 import faSubscript from '@fortawesome/fontawesome-free-solid/faSubscript';
 import faSuperScript from '@fortawesome/fontawesome-free-solid/faSuperScript';
 import faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
 import faTrash from '@fortawesome/fontawesome-free-solid/faTrash';
-import faUnderline from '@fortawesome/fontawesome-free-solid/faUnderline';
-import faUndo from '@fortawesome/fontawesome-free-solid/faUndo';
 import faUnlink from '@fortawesome/fontawesome-free-solid/faUnlink';
 
-library.add(faExpand, faCompress, faBold, faItalic, faUnderline, faStrikethrough, faSubscript, faSuperScript, faParagraph, faAlignLeft, faAlignCenter,
-    faAlignRight, faAlignJustify, faListOl, faListUl, faOutdent, faIndent, faLink, faExternalLinkAlt, faEdit, faUnlink, faSearch, faTrash,
-    faTimes, faMousePointer, faEraser, faQuestion, faUndo, faRedo, faArrowLeft, faPlus);
+library.add(faExpand, faCompress, faBold, faItalic, faSubscript, faSuperScript, faParagraph, faListOl, faListUl, faOutdent, faIndent, faLink, faExternalLinkAlt, faEdit, faUnlink, faTrash,
+    faTimes, faArrowLeft, faPlus);
 
 export abstract class MRichTextEditorDefaultOptions {
     public immediateVueModelUpdate: boolean = true;
