269876fe2ca26eabe24ca1dacfe35e3b25169ae8
gauthier bramardi <gauthier.bramardi@dti.ulaval.ca>
2018-07-31 10:48:57 -0400

    delete richTextLicencePlugin doublon

--- src/components/rich-text-edit/rich-text-edit.ts
+++ src/components/rich-text-edit/rich-text-edit.ts
@@ -1,4 +1,3 @@
-import { PluginObject } from 'vue';
 import Component from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
 
@@ -6,16 +5,12 @@ import { ElementQueries } from '../../mixins/element-queries/element-queries';
 import { InputLabel } from '../../mixins/input-label/input-label';
 import { InputState, InputStateInputSelector } from '../../mixins/input-state/input-state';
 import { InputWidth } from '../../mixins/input-width/input-width';
-import MessagePlugin from '../../utils/i18n/i18n';
-import LicensePlugin from '../../utils/license/license';
 import uuid from '../../utils/uuid/uuid';
 import { ModulVue } from '../../utils/vue/vue';
-import { RICH_TEXT_EDIT_NAME } from '../component-names';
 import { InputManagement, InputManagementData } from './../../mixins/input-management/input-management';
 import VueFroala from './adapter/vue-froala';
 import { MRichTextEditorDefaultOptions, MRichTextEditorStandardOptions } from './rich-text-edit-options';
 import WithRender from './rich-text-edit.html?style=./rich-text-edit.scss';
-import { RichTextLicensePluginOptions } from './rich-text-license-plugin';
 
 const RICH_TEXT_LICENSE_KEY: string = 'm-rich-text-license-key';
 
@@ -81,16 +76,3 @@ export class MRichTextEdit extends ModulVue implements InputManagementData, Inpu
     }
 
 }
-
-export class RichTextLicensePlugin implements PluginObject<RichTextLicensePluginOptions | undefined> {
-    install(v, options: RichTextLicensePluginOptions | undefined = { key: '' }): void {
-        v.use(LicensePlugin);
-        v.use(MessagePlugin);
-        if (options.key) {
-            (v.prototype as ModulVue).$license.addLicense(RICH_TEXT_LICENSE_KEY, options.key);
-        }
-        v.use(RICH_TEXT_EDIT_NAME, MRichTextEdit);
-    }
-}
-
-export default RichTextLicensePlugin;
