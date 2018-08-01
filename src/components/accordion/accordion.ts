2a904789a12eca619a42add9ac8649a1ca210fff
Raphaël Paré <raph.pare@outlook.com>
2018-08-01 16:31:54 -0400

    MODUL-286 - Update accordion styles

--- src/components/accordion/accordion.ts
+++ src/components/accordion/accordion.ts
@@ -4,7 +4,7 @@ import { Prop, Watch } from 'vue-property-decorator';
 
 import uuid from '../../utils/uuid/uuid';
 import { ModulVue } from '../../utils/vue/vue';
-import { ACCORDION_NAME } from '../component-names';
+import { ACCORDION_NAME, BUTTON_GROUP_NAME, CHECKBOX_NAME, INPLACE_EDIT_NAME, INPUT_STYLE_NAME, LINK_NAME, RADIO_GROUP_NAME, RADIO_NAME } from '../component-names';
 import I18nPlugin from '../i18n/i18n';
 import PlusPlugin from '../plus/plus';
 import AccordionTransitionPlugin from './accordion-transition';
@@ -41,6 +41,8 @@ export interface AccordionGroupGateway {
     removeAccordion(id: string): void;
     closeAllAccordions(): any;
 }
+
+const COMPONENT_IN_CLOSEST: string = '.' + BUTTON_GROUP_NAME + ', .' + INPUT_STYLE_NAME + ', .' + CHECKBOX_NAME + ', .' + RADIO_GROUP_NAME + ', .' + RADIO_NAME + ', .' + LINK_NAME + ', .' + INPLACE_EDIT_NAME;
 
 function isAccordionGroup(parent: any): parent is AccordionGroupGateway {
     return parent && 'addAccordion' in parent;
@@ -152,7 +154,7 @@ export class MAccordion extends ModulVue implements AccordionGateway {
     }
 
     private toggleAccordion(event: Event): void {
-        let target: Element | null = (event.target as HTMLElement).closest('[href], [onclick], a, button, input, textarea, radio, .m-input-style, .m-link, .m-radio, .m-checkbox');
+        let target: Element | null = (event.target as HTMLElement).closest('[href], [onclick], a, button, input, textarea, radio, ' + COMPONENT_IN_CLOSEST);
 
         if (!this.propDisabled && !target) {
             const initialState: boolean = this.internalPropOpen;
