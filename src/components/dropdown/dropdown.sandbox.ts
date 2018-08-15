18527820f1ea84f50f704e31fef9937c5da5ff7d
Jean-Philippe Goydadin <jean-philippe.goydadin@dti.ulaval.ca>
2018-08-15 16:26:06 -0400

    Ajout de quelques cas de tests.

--- src/components/dropdown/dropdown.sandbox.ts
+++ src/components/dropdown/dropdown.sandbox.ts
@@ -8,8 +8,14 @@ import WithRender from './dropdown.sandbox.html';
 @Component
 export class MDropdownSandbox extends Vue {
     // tslint:disable-next-line:no-null-keyword
-    public nullValue: null = null;
-    public undefinedValue: undefined = undefined;
+    public nullValue1: null = null;
+    // tslint:disable-next-line:no-null-keyword
+    public nullValue2: null = null;
+    // tslint:disable-next-line:no-null-keyword
+    public nullValue3: null = null;
+    public undefinedValue1: undefined = undefined;
+    public undefinedValue2: undefined = undefined;
+    public undefinedValue3: undefined = undefined;
 }
 
 const DropdownSandboxPlugin: PluginObject<any> = {
