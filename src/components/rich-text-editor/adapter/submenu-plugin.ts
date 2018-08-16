4a305b59b8ae8a2eba36721f94db1d7f11bc3e81
gauthier bramardi <gauthier.bramardi@dti.ulaval.ca>
2018-08-16 16:40:49 -0400

    use constants

--- src/components/rich-text-editor/adapter/submenu-plugin.ts
+++ src/components/rich-text-editor/adapter/submenu-plugin.ts
@@ -1,6 +1,11 @@
 export default class SubMenuPlugin {
     private subButtons: any[];
     private isSubmenuVisible: boolean = false;
+    private HIDE_ANIMATION_DURATION: number = 200;
+    private SHOW_ANIMATION_DURATION: number = 200;
+    private FULLSCREEN_CMD: string = 'fullscreen';
+    private HIDE_CMD: string = 'hide';
+    private SUBMENU_CMD: string = '-sub-menu';
 
     constructor(private editor: any, buttonNames: string[]) {
         this.subButtons = buttonNames.map(name => this.editor.$tb.find(`.fr-command[data-cmd="${name}"]`));
@@ -9,13 +14,13 @@ export default class SubMenuPlugin {
     public showSubMenu(): void {
         if (!this.isSubmenuVisible) {
             // hide fullscreen buttons
-            this.editor.$tb.find(`.fr-command[data-cmd="fullscreen"]`).hide(200);
+            this.editor.$tb.find(`.fr-command[data-cmd="${this.FULLSCREEN_CMD}"]`).hide(this.HIDE_ANIMATION_DURATION);
             // hide toggle buttons
-            this.editor.$tb.find(`.fr-command[data-cmd*="-sub-menu"]`).hide(200, () => {
+            this.editor.$tb.find(`.fr-command[data-cmd*="${this.SUBMENU_CMD}"]`).hide(this.HIDE_ANIMATION_DURATION, () => {
                 // show hide button
-                this.editor.$tb.find(`.fr-command[data-cmd="hide"]`).show(250);
+                this.editor.$tb.find(`.fr-command[data-cmd="${this.HIDE_CMD}"]`).show(this.SHOW_ANIMATION_DURATION);
                 // show sub-menu buttons
-                this.subButtons.forEach(btn => btn.show(250));
+                this.subButtons.forEach(btn => btn.show(this.SHOW_ANIMATION_DURATION));
             });
             this.isSubmenuVisible = true;
         }
@@ -25,14 +30,14 @@ export default class SubMenuPlugin {
     public hideSubMenu(): void {
         if (this.isSubmenuVisible) {
             // show sub-menu buttons
-            this.subButtons.forEach(btn => btn.hide(200));
+            this.subButtons.forEach(btn => btn.hide(this.HIDE_ANIMATION_DURATION));
 
             // hide hide button
-            this.editor.$tb.find(`.fr-command[data-cmd="hide"]`).hide(200, () => {
+            this.editor.$tb.find(`.fr-command[data-cmd="${this.HIDE_CMD}"]`).hide(this.HIDE_ANIMATION_DURATION, () => {
                 // show toggle buttons
-                this.editor.$tb.find(`.fr-command[data-cmd*="-sub-menu"]`).show(250);
+                this.editor.$tb.find(`.fr-command[data-cmd*="${this.SUBMENU_CMD}"]`).show(this.SHOW_ANIMATION_DURATION);
                 // show fullscreen buttons
-                this.editor.$tb.find(`.fr-command[data-cmd="fullscreen"]`).show(250);
+                this.editor.$tb.find(`.fr-command[data-cmd="${this.FULLSCREEN_CMD}"]`).show(this.SHOW_ANIMATION_DURATION);
             });
             this.isSubmenuVisible = false;
         }
