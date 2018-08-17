1585b72aaccb98914b273865c6a468372dbfd4db
gauthier bramardi <gauthier.bramardi@dti.ulaval.ca>
2018-08-17 10:35:50 -0400

    comment insertion submenu

--- src/components/rich-text-editor/adapter/vue-froala.ts
+++ src/components/rich-text-editor/adapter/vue-froala.ts
@@ -146,10 +146,12 @@ enum FroalaElements {
     protected addSubMenus(): void {
         this.$log.log(this.$i18n.translate('m-rich-text-editor:styles'));
          // add mobile mode popups
-        $.FroalaEditor.DefineIcon('plus', { NAME: 'plus' });
         this.addSubMenu(this.$i18n.translate('m-rich-text-editor:styles'), 'bold', ['bold', 'italic', 'subscript', 'superscript']);
         this.addSubMenu(this.$i18n.translate('m-rich-text-editor:lists'), 'formatUL', ['formatUL', 'formatOL', 'outdent', 'indent']);
-        this.addSubMenu(this.$i18n.translate('m-rich-text-editor:insert'), 'plus', ['insertLink', 'specialCharacters']);
+
+        // we'll use this submodule when we'll support images,tables,...
+        //  $.FroalaEditor.DefineIcon('plus', { NAME: 'plus' });
+        // this.addSubMenu(this.$i18n.translate('m-rich-text-editor:insert'), 'plus', ['insertLink', 'specialCharacters']);
 
         // add "hide sub-menu" button
         $.FroalaEditor.DefineIcon('angle-left', { NAME: 'angle-left' });
