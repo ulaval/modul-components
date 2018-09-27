1f8a7ec2f7ecd20da85acb37c18157758d1fb875
Philippe Rail <philipperail@gmail.com>
2018-09-27 15:46:35 -0400

    MODUL-338 - Android keyboard
    stick the footer at the window bottom and not have the footer get pushed by android keyboard

--- src/components/edit-window/edit-window.ts
+++ src/components/edit-window/edit-window.ts
@@ -1,11 +1,11 @@
 import { PluginObject } from 'vue';
-import { ModulVue } from '../../utils/vue/vue';
 import Component from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
+
+import { BackdropMode, Portal, PortalMixin, PortalTransitionDuration } from '../../mixins/portal/portal';
+import { ModulVue } from '../../utils/vue/vue';
 import { EDIT_WINDOW_NAME } from '../component-names';
-import { Portal, PortalMixin, PortalMixinImpl, BackdropMode, PortalTransitionDuration } from '../../mixins/portal/portal';
 import WithRender from './edit-window.html?style=./edit-window.scss';
-import { log } from 'util';
 
 @WithRender
 @Component({
@@ -27,12 +27,43 @@ export class MEditWindow extends ModulVue {
     @Prop({ default: false })
     public disableSaveButton: boolean;
 
+    public $refs: {
+        dialogWrap: HTMLElement,
+        header: HTMLElement,
+        body: HTMLElement,
+        footer: HTMLElement,
+        article: Element
+    };
+
     protected mounted(): void {
         this.as<Portal>().transitionDuration = PortalTransitionDuration.Regular + PortalTransitionDuration.XSlow;
+
+        if (this.isAndroid) {
+            this.$on('open', this.stickFooter);
+        }
     }
 
     public get popupBody(): any {
         return (this.$refs.article as Element).querySelector('.m-popup__body');
+    }
+
+    public get isAndroid(): boolean {
+        return /(android)/i.test(window.navigator.userAgent);
+    }
+
+    // to stick the footer at the window bottom and not have the footer get pushed by android keyboard
+    private stickFooter(): void {
+
+        // waiting until the opening animation has finished before doing anything DOM related
+        setTimeout(() => {
+            let wrapH: number = this.$refs.dialogWrap.clientHeight;
+            let headerH: number = this.$refs.header.clientHeight;
+            let footerH: number = this.$refs.footer.clientHeight;
+
+            this.$refs.body.style.height = wrapH - (headerH + footerH) + 'px';
+
+        }, this.as<Portal>().transitionDuration);
+
     }
 
     private handlesFocus(): boolean {
