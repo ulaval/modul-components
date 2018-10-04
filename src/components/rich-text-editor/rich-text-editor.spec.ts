9f4d1300acf9f3447cecf9e48cbef7b7f18b9d41
Jean-Francois Dion <jean-francois.dion@dti.ulaval.ca>
2018-10-04 14:34:13 -0400

    Test skeleton for new behaviours

--- src/components/rich-text-editor/rich-text-editor.spec.ts
+++ src/components/rich-text-editor/rich-text-editor.spec.ts
@@ -36,6 +36,39 @@ describe('MRichTextEditor', () => {
         expect(richTextEditor.froalaLicenseKey).toEqual(froalaLicenseKey);
     });
 
+    describe('custom validation for HTML content', () => {
+        ['', '<p></p>', '<p>&nbsp;</p>', ' &nbsp; ', '<p><strong></strong></p>']
+        .forEach((value: string) => {
+            it(`A RTE with '${value}' is considered to be empty`, () => {
+                expect(richTextEditor.customHasValue(value)).toBeFalsy();
+            });
+        });
+
+        ['a', '<p>abc</p>', '<p>a&nbsp;</p>', ' &nbsp; abc', '<p><strong>abc</strong></p>']
+        .forEach((value: string) => {
+            it(`A RTE with '${value}' is considered NOT to be empty`, () => {
+                expect(richTextEditor.customHasValue(value)).toBeTruthy();
+            });
+        });
+    });
+
+    describe('when refreshing model', () => {
+        it('should refresh with real value if has value', () => {
+            const content: string = '<p>abc123</p>';
+
+            richTextEditor.$emit('input', content);
+
+            expect(wrapper.emitted().input[0]).toEqual([content]);
+        });
+        it(`should refresh with an empty string if it doesn't have value`, () => {
+            const content: string = '<p><strong> &nbsp; </strong></p>';
+
+            richTextEditor.$emit('input', content);
+
+            expect(wrapper.emitted().input[0]).toEqual(['']);
+        });
+    });
+
     describe('In standard Mode', () => {
         beforeEach(() => {
             wrapper.setProps({
