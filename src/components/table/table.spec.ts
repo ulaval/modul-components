f596a852b45156e1dcb747e5a351b36acad8c892
Jonathan Matte <jonathan.matte@dti.ulaval.ca>
2018-10-18 09:11:23 -0400

    MODUL-505 - Basic table unit tests

--- src/components/table/table.spec.ts
+++ src/components/table/table.spec.ts
@@ -1,128 +1,87 @@
-// import { MTable, TableHeader } from './table';
-// import { shallow, Wrapper } from '@vue/test-utils';
-// import { renderComponent } from '../../../tests/helpers/render';
+import { MTable } from './table';
+import { shallow, Wrapper } from '@vue/test-utils';
+import { renderComponent } from '../../../tests/helpers/render';
 
-// let data: any[];
-// let headers: TableHeader[];
-// let rowsMax: number;
+let data: any[];
+let maxRows: number;
 
-// const width: string = '10%';
-// const data1: any = { data: '1' };
+const width: string = '10%';
+const data1: any = { data: '1' };
 
-// const DATA_WITH_4_ROWS: any[] = [
-//     data1,
-//     { data: '2' },
-//     { data: '3' },
-//     { data: '4' }
-// ];
+const DATA_WITH_4_ROWS: any[] = [
+    data1,
+    { data: '2' },
+    { data: '3' },
+    { data: '4' }
+];
 
-// const HEADER_WITH_WIDTH: TableHeader = {
-//     slot: 'test',
-//     title: 'Test',
-//     width
-// };
+let wrapper: Wrapper<MTable>;
 
-// const HEADER_WITHOUT_WIDTH: TableHeader = {
-//     slot: 'test2',
-//     title: 'Test2'
-// };
+const initializeShallowWrapper: any = () => {
+    wrapper = shallow(MTable, {
+        propsData: {
+            data,
+            maxRows
+        }
+    });
+};
 
-// const COMPLETE_HEADERS: TableHeader[] = [HEADER_WITH_WIDTH, HEADER_WITHOUT_WIDTH];
+afterEach(() => {
+    maxRows = 50;
+});
 
-// let wrapper: Wrapper<MTable>;
+describe(`MTable`, () => {
 
-// const initializeShallowWrapper: any = () => {
-//     wrapper = shallow(MTable, {
-//         propsData: {
-//             headers,
-//             data,
-//             rowsMax
-//         }
-//     });
-// };
+    beforeEach(() => {
+        data = DATA_WITH_4_ROWS;
+    });
 
-// afterEach(() => {
-//     data = [];
-//     headers = [];
-//     rowsMax = 50;
-// });
+    describe(`Given a table full of data`, () => {
 
-// describe(`MTable`, () => {
+        describe(`When rowsMax < data.length`, () => {
 
-//     beforeEach(() => {
-//         headers = COMPLETE_HEADERS;
-//         data = DATA_WITH_4_ROWS;
-//     });
+            beforeEach(() => {
+                maxRows = 2;
+                initializeShallowWrapper();
+            });
 
-//     describe(`Given a header`, () => {
+            it(`Should render correctly`, () => {
+                expect(renderComponent(wrapper.vm)).resolves.toMatchSnapshot();
+            });
 
-//         beforeEach(() => {
-//             initializeShallowWrapper();
-//         });
+            it(`Then show a number of rows equal to rowsMax`, () => {
+                expect(wrapper.vm.propRows).toEqual(2);
+            });
 
-//         describe(`When the width is defined`, () => {
+        });
 
-//             it(`Then should set the width of the column`, () => {
-//                 expect(wrapper.vm.getWidth(HEADER_WITH_WIDTH)).toEqual({ width });
-//             });
+        describe(`When rowsMax > data.length`, () => {
 
-//         });
+            beforeEach(() => {
+                maxRows = 8;
+                initializeShallowWrapper();
+            });
 
-//         describe(`When the width is undefined`, () => {
+            it(`Should render correctly`, () => {
+                expect(renderComponent(wrapper.vm)).resolves.toMatchSnapshot();
+            });
 
-//             it(`Then should not set a width`, () => {
-//                 expect(wrapper.vm.getWidth(HEADER_WITHOUT_WIDTH)).toEqual({ width: '' });
-//             });
+            it(`Then show a number of rows equal to the legnth of data`, () => {
+                expect(wrapper.vm.propRows).toEqual(4);
+            });
 
-//         });
-//     });
+        });
 
-//     describe(`Given a table full of data`, () => {
+        describe(`When looping the data`, () => {
 
-//         describe(`When rowsMax < data.length`, () => {
+            it(`Then should return the right data`, () => {
+                initializeShallowWrapper();
 
-//             beforeEach(() => {
-//                 rowsMax = 2;
-//                 initializeShallowWrapper();
-//             });
+                expect(wrapper.vm.getDataValue(1)).toEqual(data1);
+            });
 
-//             it(`Should render correctly`, () => {
-//                 expect(renderComponent(wrapper.vm)).resolves.toMatchSnapshot();
-//             });
+        });
 
-//             it(`Then show a number of rows equal to rowsMax`, () => {
-//                 expect(wrapper.vm.propRows).toEqual(2);
-//             });
+    });
 
-//         });
-
-//         describe(`When rowsMax > data.length`, () => {
-
-//             beforeEach(() => {
-//                 rowsMax = 8;
-//                 initializeShallowWrapper();
-//             });
-
-//             it(`Should render correctly`, () => {
-//                 expect(renderComponent(wrapper.vm)).resolves.toMatchSnapshot();
-//             });
-
-//             it(`Then show a number of rows equal to the legnth of data`, () => {
-//                 expect(wrapper.vm.propRows).toEqual(4);
-//             });
-
-//         });
-
-//         describe(`When looping the data`, () => {
-
-//             it(`Then should return the right data`, () => {
-//                 initializeShallowWrapper();
-
-//                 expect(wrapper.vm.getDataValue(1)).toEqual(data1);
-//             });
-
-//         });
-
-//     });
-
-// });
+});
