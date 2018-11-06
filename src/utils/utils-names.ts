9d482947ad413166238a2a8a0233b3c0819cd056
Charles Maheu <CHMAH33@ulaval.ca>
2018-11-06 10:25:42 -0500

    Added services to sandboxes

---
+++ src/utils/utils-names.ts
@@ -0,0 +1,11 @@
+export const ALERT: string = 'alert';
+export const CONFIRM: string = 'confirm';
+export const FILE: string = 'file';
+export const HTTP: string = 'http';
+export const I18N: string = 'i18n';
+export const LICENSE: string = 'license';
+export const LOGGER: string = 'logger';
+export const MODUL: string = 'modul';
+export const MQ: string = 'mq';
+export const SCROLL_TO: string = 'scrollTo';
+export const SVG: string = 'svg';
