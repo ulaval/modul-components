9c3b9f394bdfd26b9d8b8feadfc8b1e9bb025391
Unknown <jean-philippe.guilmette@dti.ulaval.ca>
2018-11-08 11:16:42 -0500

    Lien mode RouterLink et disabled ne doit plus naviguer lorsque cliquer

--- src/components/link/link.sandbox.ts
+++ src/components/link/link.sandbox.ts
@@ -1,12 +1,12 @@
 import Vue, { PluginObject } from 'vue';
 import { Component } from 'vue-property-decorator';
-
 import { LINK_NAME } from '../component-names';
 import WithRender from './link.sandbox.html';
 
 @WithRender
 @Component
 export class MLinkSandbox extends Vue {
+    public routerLinkDisabled: boolean = false;
 }
 
 const LinkSandboxPlugin: PluginObject<any> = {
