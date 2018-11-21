3aa384f47310151b529d950b5950e87c207bd9d6
jpguilmette <jpguilmette@gmail.com>
2018-11-21 14:04:29 -0500

    Afficher crochet lorsque disabled (#567)

--- src/components/checkbox/checkbox.sandbox.ts
+++ src/components/checkbox/checkbox.sandbox.ts
@@ -1,12 +1,13 @@
 import Vue, { PluginObject } from 'vue';
 import { Component } from 'vue-property-decorator';
-
 import { CHECKBOX_NAME } from '../component-names';
 import WithRender from './checkbox.sandbox.html';
 
 @WithRender
 @Component
 export class MCheckboxSandbox extends Vue {
+    public disabledCheckboxValue: boolean = false;
+    public disabledCheckbox: boolean = false;
 }
 
 const CheckboxSandboxPlugin: PluginObject<any> = {
