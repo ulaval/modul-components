557bf3aca304d267e8c38ffdb335aa494fa050ee
Jonathan Matte <jonathan.matte@dti.ulaval.ca>
2018-11-28 16:26:10 -0500

    MODUL-597 - Revert changes from PR 569

--- src/utils/modul/modul.ts
+++ src/utils/modul/modul.ts
@@ -268,8 +268,9 @@ export class Modul {
             this.bodyStyle.left = '0';
             this.bodyStyle.bottom = '0'; // --- Added bug in IE11 --- Added to fix edge case where showed contents through popper/portal are hidden when page content isn't high enough to stretch the body.
             this.bodyStyle.height = '100%';
-            this.htmlEl.style.overflow = 'hidden';
+            this.bodyStyle.overflow = 'hidden';
             this.bodyEl.scrollTop = this.stopScrollPosition; // Overflow hidden must be applied on <body> and the <html> before "scrollTop"
+            this.htmlEl.style.overflow = 'hidden';
         }
         return uuid.generate();
     }
