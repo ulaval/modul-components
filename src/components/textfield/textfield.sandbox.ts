9fb67d00a9e230c2f564791d94fe302d34e709d7
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2018-12-12 14:26:32 -0500

    Feature/modul 581 Switching font type (#587)
    * Adjust font-size for mobile
    
    * Update font-size in title
    
    * Test
    
    * Test
    
    * test2
    
    * test 3
    
    * Ajout mixin
    
    * MODUL-420 - MAJ des mixins gérant les niveaux de titre
    
    * MODUL-420 - MAJ des mixins pour les nivaux de titre
    
    * Utilisation font-weight
    
    * oublie
    
    * Ajustements type de graisse
    
    * Enlever les tests
    
    * Ajout d'un sandbox pour la typo
    
    * Correction de la navigation dans la liste des  sandboxes
    
    * Headers correction
    
    * fix sandboxes
    
    * Ajout des classes de style m-u--h
    
    * Test pour le build sur Openshift
    
    * le nom appName devrait être tout en minuscules
    
    * appName ne devrait pas contenir de '_'
    
    * Correction mineure dans les sandboxes
    
    * MODUL-581: reduire la font en mobile
    
    * MODUL-581 - Arial fallback
    
    * fixed font should be in asset
    
    * MODUL-581 - remove $m-font-weight--regular
    
    * MODUL-581 - Add font-path variable

--- src/components/textfield/textfield.sandbox.ts
+++ src/components/textfield/textfield.sandbox.ts
@@ -1,6 +1,5 @@
 import Vue, { PluginObject } from 'vue';
 import { Component } from 'vue-property-decorator';
-
 import { TEXTFIELD_NAME } from '../component-names';
 import TextfieldPlugin from './textfield';
 import WithRender from './textfield.sandbox.html';
