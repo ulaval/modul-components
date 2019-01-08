74677cb285966eb61200414f9c5dccba55f8576b
Vincent Dallaire <vincent.dallaire@dti.ulaval.ca>
2019-01-08 11:06:11 -0500

    MODUL-628 - Ne pas enforcer la présence du numéro du billet dans le message du commit

--- hooks/commit-msg.js
+++ hooks/commit-msg.js
@@ -8,13 +8,9 @@ if (!commitMessage) {
 }
 
 utils.getBranchname((err, stdout, stderr) => {
-    const regex = new RegExp(`^Revert |^(${utils.billet}(, )?)+ - .+|^Merge branch .+`);
     const numBilletDansBranche = utils.billetRegex.exec(stdout);
 
-    if (!regex.test(commitMessage)) {
-        console.log(`Le message du commit ne respecte pas ce format : ` + regex);
-        process.exit(1);
-    } else if (numBilletDansBranche != null && !new RegExp(numBilletDansBranche[0]).test(commitMessage)) {
+    if (numBilletDansBranche != null && !new RegExp(numBilletDansBranche[0]).test(commitMessage)) {
         console.log(`Le message du commit ne contient pas le numéro du billet Jira de la branche courante : ` + numBilletDansBranche[0]);
         process.exit(1);
     }
