e3da4059034448ccfbbb0e94ec4f9a3f6e220c16
vincent guillemette <vincentguillemette1981@gmail.com>
2019-01-14 15:58:42 -0500

    * ajout des principaux contexts

--- src/components/icon-button/icon-button.sandbox.ts
+++ src/components/icon-button/icon-button.sandbox.ts
@@ -1,12 +1,21 @@
 import Vue, { PluginObject } from 'vue';
 import { Component } from 'vue-property-decorator';
-
+import { toArray } from '../../utils/enum/enum';
 import { ICON_BUTTON_NAME } from '../component-names';
+import { MIconButtonSkin } from './icon-button';
 import WithRender from './icon-button.sandbox.html';
 
 @WithRender
 @Component
 export class MIconButtonSandbox extends Vue {
+    private buttonSize: string = '44px';
+    private iconSize: string = '20px';
+    private iconButtonSkin: MIconButtonSkin = MIconButtonSkin.Dark;
+    private mIconButtonSkin = MIconButtonSkin;
+
+    get iconButtonSkinAsArray(): { key: string | number, value: string }[] {
+        return toArray(MIconButtonSkin);
+    }
 }
 
 const IconButtonSandboxPlugin: PluginObject<any> = {
