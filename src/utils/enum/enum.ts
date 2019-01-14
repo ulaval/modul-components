1f7ec77414bfbbbb314591f66c64ed734a9e10f6
vincent guillemette <vincentguillemette1981@gmail.com>
2019-01-14 10:46:08 -0500

    * ajouter button avec style et position d'icone

---
+++ src/utils/enum/enum.ts
@@ -0,0 +1,11 @@
+/**
+ * @param {Object}  The enum to convert
+ * @return {string} The array of objects containing the pairs of key/value of the enum
+ */
+export function toArray(e: Object): { key: string | number, value: string }[] {
+    return Object.keys(e)
+        .map(key => ({
+            key: key.toString(),
+            value: e[key].toString()
+        }));
+}
