0a1f5bb8c624308ad8156c24623ad4b806849835
vincent guillemette <vincentguillemette1981@gmail.com>
2019-01-14 10:13:33 -0500

    * ajouter button avec style et position d'icone

--- src/components/button/button.sandbox.ts
+++ src/components/button/button.sandbox.ts
@@ -1,8 +1,9 @@
 import Vue, { PluginObject } from 'vue';
 import { Component, Emit } from 'vue-property-decorator';
+import { toArray } from '../../utils/enum/enum';
 import { BUTTON_NAME } from '../component-names';
+import { MButtonIconPosition, MButtonSkin, MButtonType } from './button';
 import WithRender from './button.sandbox.html';
-
 
 @WithRender
 @Component
@@ -11,12 +12,28 @@ export class MButtonSandbox extends Vue {
     messageInialValue: string = 'Mon message';
     message: string = this.messageInialValue;
 
+    private buttonType: MButtonType = MButtonType.Button;
+    private buttonSkin: MButtonSkin = MButtonSkin.Primary;
+    private buttonIconPosition: MButtonIconPosition = MButtonIconPosition.Left;
+
     toogleAfficher(): void {
         this.afficherbouton1 = !this.afficherbouton1;
     }
 
     get libelle(): string {
-        return 'patate';
+        return 'texte 1';
+    }
+
+    get buttonTypeToArray(): { key: string | number, value: string }[] {
+        return toArray(MButtonType);
+    }
+
+    get buttonSkinToArray(): { key: string | number, value: string }[] {
+        return toArray(MButtonSkin);
+    }
+
+    get buttonIconPositionToArray(): { key: string | number, value: string }[] {
+        return toArray(MButtonIconPosition);
     }
 
     @Emit('submit')
