fae59f3d02d1d601594e9507c62035d6f9c16a29
Charles Maheu <CHMAH33@ulaval.ca>
2019-01-22 11:41:45 -0500

    splitted declaration in corresponding modul

--- src/utils/utils-plugin.ts
+++ src/utils/utils-plugin.ts
@@ -1,27 +1,14 @@
 import Vue, { PluginObject } from 'vue';
 import { WindowErrorHandler } from './errors/window-error-handler';
-import HttpPlugin, { HttpPluginOptions, HttpService } from './http/http';
-import I18nPlugin, { I18nPluginOptions, Messages } from './i18n/i18n';
-import LoggerPlugin, { ConsoleOptions, Logger } from './logger/logger';
-import MediaQueriesPlugin, { MediaQueries } from './media-queries/media-queries';
-import ModulPlugin, { Modul } from './modul/modul';
-import ScrollToPlugin, { ScrollTo } from './scroll-to/scroll-to';
+import HttpPlugin, { HttpPluginOptions } from './http/http';
+import I18nPlugin, { I18nPluginOptions } from './i18n/i18n';
+import LoggerPlugin, { ConsoleOptions } from './logger/logger';
+import MediaQueriesPlugin from './media-queries/media-queries';
+import ModulPlugin from './modul/modul';
+import ScrollToPlugin from './scroll-to/scroll-to';
 import SpritesPlugin from './svg/sprites';
 
-/**
- * Augment the typings of Vue.js
- */
 
-declare module 'vue/types/vue' {
-    interface Vue {
-        $i18n: Messages;
-        $http: HttpService;
-        $mq: MediaQueries;
-        $scrollTo: ScrollTo;
-        $modul: Modul;
-        $log: Logger;
-    }
-}
 
 export interface UtilsPluginOptions {
     httpPluginOptions?: HttpPluginOptions;
