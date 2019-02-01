3b12cde41bcd2c9802868eece1fb4672877e42db
Jean-Francois Dion <jean-francois.dion@dti.ulaval.ca>
2019-02-01 14:09:50 -0500

    MODUL-723 - Added polyfill to allow Object.assign to be supported in IE 11

--- src/utils/polyfills.ts
+++ src/utils/polyfills.ts
@@ -115,3 +115,38 @@ if (!String.prototype['startsWith']) {
         return this.substr(position || 0, searchString.length) === searchString;
     };
 }
+
+// Polyfill to allow IE 11 to support Object.assign
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+if (typeof Object.assign !== 'function') {
+    // Must be writable: true, enumerable: false, configurable: true
+    Object.defineProperty(Object, 'assign', {
+        value: function assign(target: any, _varArgs: any): any { // .length of function is 2
+            // == allow for null and undefined check
+            // tslint:disable-next-line:triple-equals no-null-keyword
+            if (target == null) { // TypeError if undefined or null
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            const to: any = Object(target);
+
+            for (let index: number = 1; index < arguments.length; index++) {
+                let nextSource: any = arguments[index];
+                // != allow for null and undefined check
+                // tslint:disable-next-line:triple-equals no-null-keyword
+                if (nextSource != null) { // Skip over if undefined or null
+                    for (let nextKey in nextSource) {
+                        // Avoid bugs when hasOwnProperty is shadowed
+                        if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                            to[nextKey] = nextSource[nextKey];
+                        }
+                    }
+                }
+            }
+            return to;
+        },
+        writable: true,
+        configurable: true
+    });
+}
+
