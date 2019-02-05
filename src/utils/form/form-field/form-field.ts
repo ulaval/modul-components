ac5d23caabbcc0827cbaeb6a6378fa6b25610cb5
vincent guillemette <vincent.guillemette.1@ulaval.ca>
2019-02-05 10:21:39 -0500

    MODUL-735 - * force validation on object type

--- src/utils/form/form-field/form-field.ts
+++ src/utils/form/form-field/form-field.ts
@@ -141,7 +141,7 @@ export class FormField<T> {
      * @param value the new value of the field
      */
     private change(value: T): void {
-        if (value !== this.oldValue) {
+        if (value !== this.oldValue || typeof value === 'object') {
             this.internalValue = value;
             this.oldValue = this.internalValue;
             this.validate();
