f9616c204894253ba468c6971d5df14d05d5c61a
Vincent Guillemette <vincentguillemette1981@gmail.com>
2019-02-08 10:36:18 -0500

    MODUL-742 - gestion reponse serveur pour formulaire  (#723)
    * MODUL-742 - * handling form server response
    
    * MODUL-742 - * updated snapshots
    * remove server response on message closed
    
    * MODUL-742 - * remove server response in favor of slot
    
    * MODUL-742 - * remove accordion-transition above the slot
    
    * MODUL-742 - * removed unused translations
    
    * MODUL-742 - * updated snap
    * use of style instead of class

--- src/components/form/form.sandbox.ts
+++ src/components/form/form.sandbox.ts
@@ -5,12 +5,20 @@ import { FormFieldValidation } from '../../utils/form/form-field-validation/form
 import { FormField } from '../../utils/form/form-field/form-field';
 import { FormValidation } from '../../utils/form/form-validation/form-validation';
 import { FORM } from '../component-names';
+import { MMessageState } from '../message/message';
 import FormPlugin from './form';
 import WithRender from './form.sandbox.html';
 
 @WithRender
 @Component
 export class MFormSandbox extends Vue {
+    serverResponses: any[] = [
+        { status: 100, messageState: MMessageState.Information, title: 'Info', message: 'Here is some information about your request treament..' },
+        { status: 400, messageState: MMessageState.Warning, title: 'There was an error on your end', message: 'The error was...' },
+        { status: 500, messageState: MMessageState.Error, title: 'There was an error on our end', message: 'Please try again later' }
+    ];
+    serverResponse: any = this.serverResponses[0];
+    hasServerResponse: boolean = false;
     forms: Form[] = [
         new Form({
             'field-1': new FormField<string>((): string => '', [])
@@ -122,13 +130,33 @@ export class MFormSandbox extends Vue {
         }),
         new Form({
             'field-1': new FormField<string>((): string => '', [])
+        }),
+        new Form({
+            'field-1': new FormField<string>((): string => '', [(value: string): FormFieldValidation => {
+                if (!value) {
+                    return new FormFieldValidation(true, [`the <strong>field-1</strong> is required`], ['this field is required']);
+                }
+                return new FormFieldValidation();
+            }]),
+            'field-2': new FormField<string>((): string => '', [(value: string): FormFieldValidation => {
+                if (!value) {
+                    return new FormFieldValidation(true, [`the <strong>field-2</strong> is required`], ['this field is required']);
+                }
+                return new FormFieldValidation();
+            }])
         })
     ];
 
     submit(formIndex: number): void {
+        if (formIndex === 12) {
+            this.hasServerResponse = true;
+        }
     }
 
     reset(formIndex: number): void {
+        if (formIndex === 12) {
+            this.hasServerResponse = false;
+        }
     }
 }
 
