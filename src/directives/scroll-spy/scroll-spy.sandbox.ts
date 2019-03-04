4eee0143b54c0a96a4f223f006910f9691d43e0f
Maxime Turcotte <maxime.turcotte@dti.ulaval.ca>
2019-03-04 15:53:00 -0500

    MODUL-788 - continuer test

--- src/directives/scroll-spy/scroll-spy.sandbox.ts
+++ src/directives/scroll-spy/scroll-spy.sandbox.ts
@@ -5,27 +5,45 @@ import { ModulVue } from '../../utils/vue/vue';
 import { SCROLL_SPY_NAME } from '../directive-names';
 import WithRender from './scroll-spy.sandbox.html?style=./scroll-spy.scss';
 
+const map: Map<string, boolean> = new Map<string, boolean>();
 @WithRender
 @Component
 export class MScrollSpySandbox extends ModulVue {
+
+    currentID: string = '';
 
     mounted(): void {
         const elements: NodeListOf<HTMLElement> = document.querySelectorAll('.watch');
         const observer: IntersectionObserver = new IntersectionObserver(this.handleIntersect);
         elements.forEach(element => {
             observer.observe(element);
+            map.set(element.id, false);
         });
     }
 
-    handleIntersect(entries: IntersectionObserverEntry[], observer: IntersectionObserverInit): void {
+    handleIntersect(entries: IntersectionObserverEntry[]): void {
         entries.forEach(entry => {
-            console.log(entry);
             if (entry.isIntersecting) {
-                console.log(entry.target.id);
+                map.set(entry.target.id, entry.isIntersecting);
             } else {
-
+                map.set(entry.target.id, entry.isIntersecting);
             }
         });
+        this.searchFirstCurrent();
+    }
+
+    searchFirstCurrent(): void {
+        let elementFound: Boolean = false;
+        map.forEach((value: boolean, key: string) => {
+            if (value && !elementFound) {
+                this.currentID = key;
+                elementFound = true;
+            }
+        });
+    }
+
+    isCurrent(id: string): boolean {
+        return id === this.currentID;
     }
 
     scrolling(event: Event): void {
