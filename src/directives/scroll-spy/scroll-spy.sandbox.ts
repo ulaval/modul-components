892648b0d629356475dc07a0350a45ef966e9a43
Maxime Turcotte <maxime.turcotte@dti.ulaval.ca>
2019-03-04 09:09:41 -0500

    MODUL-788 - test

--- src/directives/scroll-spy/scroll-spy.sandbox.ts
+++ src/directives/scroll-spy/scroll-spy.sandbox.ts
@@ -5,9 +5,25 @@ import { ModulVue } from '../../utils/vue/vue';
 import { SCROLL_SPY_NAME } from '../directive-names';
 import WithRender from './scroll-spy.sandbox.html?style=./scroll-spy.scss';
 
+const options: any = {
+    root: document.querySelector('.observeElement'),
+    rootMargin: '0px',
+    threshold: 1.0
+};
+
 @WithRender
 @Component
 export class MScrollSpySandbox extends ModulVue {
+
+    mounted(): any {
+        let observer: any = new IntersectionObserver(([entry]) => {
+            console.log(entry);
+            if (entry && entry.isIntersecting) {
+                console.log(entry);
+            }
+        }, options);
+        observer.observe(this.$el);
+    }
 
     scrolling(event: Event): void {
         let myId: any = (event.target as HTMLElement).getAttribute('href');
