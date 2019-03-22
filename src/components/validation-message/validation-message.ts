5e8b77fce5db7259112885fc8b51beb3c57caf48
Raphaël Paré <raph.pare@outlook.com>
2019-03-22 11:24:30 -0400

    MODUL-852: MAJ du balisage du m-validation-message

--- src/components/validation-message/validation-message.ts
+++ src/components/validation-message/validation-message.ts
@@ -1,6 +1,6 @@
 import { PluginObject } from 'vue';
 import Component from 'vue-class-component';
-import { Prop } from 'vue-property-decorator';
+import { Emit, Prop } from 'vue-property-decorator';
 import { InputState } from '../../mixins/input-state/input-state';
 import { ModulVue } from '../../utils/vue/vue';
 import AccordionTransitionPlugin from '../accordion/accordion-transition';
@@ -16,11 +16,11 @@ export class MValidationMessage extends ModulVue {
     @Prop({ default: true })
     public transition: boolean = true;
 
-    private titleErrorIcon: string = this.$i18n.translate('m-validation-message:title-error-icon');
-    private titleValidIcon: string = this.$i18n.translate('m-validation-message:title-valid-icon');
+    public titleErrorIcon: string = this.$i18n.translate('m-validation-message:title-error-icon');
+    public titleValidIcon: string = this.$i18n.translate('m-validation-message:title-valid-icon');
 
-    private onClick(event: MouseEvent): void {
-        this.$emit('click', event);
+    @Emit('click')
+    public onClick(event: Event): void {
     }
 }
 
