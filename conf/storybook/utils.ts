6fa1658c9ec5d140940979488414a02aa019313f
Frédéric Duguay <frederic.duguay@dti.ulaval.ca>
2019-03-26 16:21:45 -0400

    MODUL-856 - Fix Camelcase Typo in exports

--- conf/storybook/utils.ts
+++ conf/storybook/utils.ts
@@ -1,4 +1,4 @@
 export const hierarchyRootSeparatorRegex: RegExp = /\|/;
-export const componentshierarchyRootSeparator: string = 'components|';
+export const componentsHierarchyRootSeparator: string = 'components|';
 export const hierarchySeparatorRegex: RegExp = /\//;
 export const hierarchySeparator: string = '/';
