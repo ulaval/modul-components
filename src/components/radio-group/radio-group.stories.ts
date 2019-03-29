7d8bb4005029bce85b7b11bf4a6aafb60cc49650
Frédéric Duguay <frederic.duguay@dti.ulaval.ca>
2019-03-29 17:04:03 -0400

    MODUL-860 - Add radio group stories to storybook, remove snapshots from SB

--- src/components/radio-group/radio-group.stories.ts
+++ src/components/radio-group/radio-group.stories.ts
@@ -27,50 +27,153 @@ storiesOf(`${componentsHierarchyRootSeparator}${RADIO_GROUP_NAME}`, module)
         },
         template: '<m-radio-group>{{ text }}</m-radio-group>'
     }))
-    //TODO value
-    .add('value', () => ({
-        template: `<m-radio-group>
-                        <m-radio value="1">1
-                        <m-radio value="2">2
-                        <m-radio value="3">3
-                   </m-radio></m-radio-group>`
-    }))
     .add('radiosPosition=right', () => ({
-        template: `<m-radio-group radiosPosition="right">
-                        <m-radio value="1">1
-                        <m-radio value="2">2
-                        <m-radio value="3">3
-                   </m-radio></m-radio-group>`
-    }))
-    //TODO inline
-    .add('inline', () => ({
         template: `<m-radio-group>
-                        <m-radio value="1">1
-                        <m-radio value="2">2
-                        <m-radio value="3">3
-                   </m-radio></m-radio-group>`
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
     }))
-    .add('radiosVerticalAlign', () => ({
-        template: `<m-radio-group radiosVerticalAlign="true">
-                        <m-radio value="1">1
-                        <m-radio value="2">2
-                        <m-radio value="3">3
-                   </m-radio></m-radio-group>`
+    .add('inline', () => ({
+        template: `<m-radio-group inline="true">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('radiosVerticalAlign=center', () => ({
+        template: `<m-radio-group radiosVerticalAlign=""center">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
     }))
     .add('radiosMarginTop=10px', () => ({
         template: `<m-radio-group radiosMarginTop="10px">
-                        <m-radio value="1">1
-                        <m-radio value="2">2
-                        <m-radio value="3">3
-                   </m-radio></m-radio-group>`
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('disabled', () => ({
+        template: `<m-radio-group :disabled="true">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('readonly', () => ({
+        template: `<m-radio-group :readonly="true">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('label', () => ({
+        template: `<m-radio-group label="This is a label">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('focus', () => ({
+        template: `<m-radio-group :focus="true">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('error', () => ({
+        template: `<m-radio-group :error="true">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('valid', () => ({
+        template: `<m-radio-group :valid="true">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('error-message', () => ({
+        template: `<m-radio-group error-message="This is an error message">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
     }));
 
 storiesOf(`${componentsHierarchyRootSeparator}${RADIO_GROUP_NAME}/radiosPosition=right`, module)
     .addDecorator(withA11y)
-    .add('TBD', () => ({
-        template: `<m-radio-group radiosPosition="right">
-                        <m-radio value="1">1
-                        <m-radio value="2">2
-                        <m-radio value="3">3
-                   </m-radio></m-radio-group>`
+    .add('inline', () => ({
+        template: `<m-radio-group inline="true" radiosPosition="right">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('radiosVerticalAlign=center', () => ({
+        template: `<m-radio-group radiosPosition="right" radiosVerticalAlign=center>
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('radiosMarginTop=10px', () => ({
+        template: `<m-radio-group radiosMarginTop="10px" radiosPosition="right">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('disabled', () => ({
+        template: `<m-radio-group :disabled="true" radiosPosition="right">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('readonly', () => ({
+        template: `<m-radio-group :readonly="true" radiosPosition="right">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('label', () => ({
+        template: `<m-radio-group label="This is a label" radiosPosition="right">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('focus', () => ({
+        template: `<m-radio-group :focus="true" radiosPosition="right">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('error', () => ({
+        template: `<m-radio-group :error="true" radiosPosition="right">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('valid', () => ({
+        template: `<m-radio-group :valid="true" radiosPosition="right">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
+    }))
+    .add('error-message', () => ({
+        template: `<m-radio-group error-message="This is an error message" radiosPosition="right">
+                        <m-radio>Radio Option 1</m-radio>
+                        <m-radio>Radio Option 2</m-radio>
+                        <m-radio>Radio Option 3</m-radio>
+                   </m-radio-group>`
     }));
