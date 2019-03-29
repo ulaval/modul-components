aeb8699f398675999b6dc2d2eb556ad94dd1cf93
Raphaël Paré <raph.pare@outlook.com>
2019-03-29 14:14:53 -0400

    MODUL-852: Optimiser le balisage du composant m-validation-message (#835)
    * MODUL-852: MAJ du balisage du m-validation-message
    
    * MODUL-852: MAj des snapshot
    
    * MODUL-852: MAJ validation-message
    
    * MODUL-852: ajustement revue de code
    
    * MODUL-852, MODUL-882: update snapshot

--- src/components/validation-message/validation-message.sandbox.ts
+++ src/components/validation-message/validation-message.sandbox.ts
@@ -1,13 +1,16 @@
 import Vue, { PluginObject } from 'vue';
 import { Component } from 'vue-property-decorator';
-
 import { VALIDATION_MESSAGE_NAME } from '../component-names';
-import WithRender from './validation-message.sandbox.html';
 import ValidationMessagePlugin from './validation-message';
+import WithRender from './validation-message.sandbox.html';
+
 
 @WithRender
 @Component
 export class MValidationMessageSandbox extends Vue {
+    public errorMessage: string = 'Error message';
+    public validMessage: string = 'Valid message';
+    public helperMessage: string = 'Helper message';
 }
 
 const ValidationMessageSandboxPlugin: PluginObject<any> = {
