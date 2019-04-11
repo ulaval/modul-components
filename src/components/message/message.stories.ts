fd88cccbc7d28dea6e43bd8ccf2c98133e4de0e2
Frédéric Duguay <frederic.duguay@dti.ulaval.ca>
2019-04-11 16:09:53 -0400

    MODUL-906 - Tweak Titles and Messages for skin substories

--- src/components/message/message.stories.ts
+++ src/components/message/message.stories.ts
@@ -51,110 +51,110 @@ storiesOf(`${componentsHierarchyRootSeparator}${MESSAGE_NAME}/state`, module)
 storiesOf(`${componentsHierarchyRootSeparator}${MESSAGE_NAME}/skin="default"`, module)
     .addDecorator(withA11y)
     .add('state="confirmation"', () => ({
-        template: '<m-message  state="confirmation">A message for testing purposes</m-message>'
+        template: '<m-message  state="confirmation">A message for confirmation purposes</m-message>'
     }))
     .add('state="confirmation" && title', () => ({
-        template: '<m-message  state="confirmation" title="Confirmation Title">A message for testing purposes</m-message>'
+        template: '<m-message  state="confirmation" title="Confirmation Title">A message for confirmation purposes</m-message>'
     }))
     .add('state="information"', () => ({
-        template: '<m-message  state="information">A message for testing purposes</m-message>'
+        template: '<m-message  state="information">A message for information purposes</m-message>'
     }))
     .add('state="information" && title', () => ({
-        template: '<m-message  state="information" title="Information Title">A message for testing purposes</m-message>'
+        template: '<m-message  state="information" title="Information Title">A message for information purposes</m-message>'
     }))
     .add('state="warning"', () => ({
-        template: '<m-message  state="warning">A message for testing purposes</m-message>'
+        template: '<m-message  state="warning">A message for warning purposes</m-message>'
     }))
     .add('state="warning" && title', () => ({
-        template: '<m-message  state="warning" title="Warning">A message for testing purposes</m-message>'
+        template: '<m-message  state="warning" title="Warning Title">A message for warning purposes</m-message>'
     }))
     .add('state="error"', () => ({
-        template: '<m-message state="error">A message for testing purposes</m-message>'
+        template: '<m-message state="error">A message for error purposes</m-message>'
     }))
     .add('state="error" && title', () => ({
-        template: '<m-message  state="error" title="Confirmation Title">A message for testing purposes</m-message>'
+        template: '<m-message  state="error" title="Error Title">A message for error purposes</m-message>'
     }));
 
 storiesOf(`${componentsHierarchyRootSeparator}${MESSAGE_NAME}/skin="light"`, module)
     .addDecorator(withA11y)
     .add('state="confirmation"', () => ({
-        template: '<m-message skin="light" state="confirmation">A message for testing purposes</m-message>'
+        template: '<m-message skin="light" state="confirmation">A message for confirmation purposes</m-message>'
     }))
     .add('state="confirmation" && title', () => ({
-        template: '<m-message skin="light" state="confirmation" title="Confirmation Title">A message for testing purposes</m-message>'
+        template: '<m-message skin="light" state="confirmation" title="Confirmation Title">A message for confirmation purposes</m-message>'
     }))
     .add('state="information"', () => ({
-        template: '<m-message skin="light" state="information">A message for testing purposes</m-message>'
+        template: '<m-message skin="light" state="information">A message for information purposes</m-message>'
     }))
     .add('state="information" && title', () => ({
-        template: '<m-message skin="light" state="information" title="Information Title">A message for testing purposes</m-message>'
+        template: '<m-message skin="light" state="information" title="Information Title">A message for information purposes</m-message>'
     }))
     .add('state="warning"', () => ({
-        template: '<m-message skin="light" state="warning">A message for testing purposes</m-message>'
+        template: '<m-message skin="light" state="warning">A message for warning purposes</m-message>'
     }))
     .add('state="warning" && title', () => ({
-        template: '<m-message skin="light" state="warning" title="Warning">A message for testing purposes</m-message>'
+        template: '<m-message skin="light" state="warning" title="Warning Title">A message for warning purposes</m-message>'
     }))
     .add('state="error"', () => ({
-        template: '<m-message skin="light" skinstate="error">A message for testing purposes</m-message>'
+        template: '<m-message skin="light" state="error">A message for error purposes</m-message>'
     }))
     .add('state="error" && title', () => ({
-        template: '<m-message skin="light" state="error" title="Confirmation Title">A message for testing purposes</m-message>'
+        template: '<m-message skin="light" state="error" title="Error Title">A message for error purposes</m-message>'
     }));
 
 storiesOf(`${componentsHierarchyRootSeparator}${MESSAGE_NAME}/skin="page-light"`, module)
     .addDecorator(withA11y)
     .add('state="confirmation"', () => ({
-        template: '<m-message skin="page-light" state="confirmation">A message for testing purposes</m-message>'
+        template: '<m-message skin="page-light" state="confirmation">A message for confirmation purposes</m-message>'
     }))
     .add('state="confirmation" && title', () => ({
-        template: '<m-message skin="page-light" state="confirmation" title="Confirmation Title">A message for testing purposes</m-message>'
+        template: '<m-message skin="page-light" state="confirmation" title="Confirmation Title">A message for confirmation purposes</m-message>'
     }))
     .add('state="information"', () => ({
-        template: '<m-message skin="page-light" state="information">A message for testing purposes</m-message>'
+        template: '<m-message skin="page-light" state="information">A message for information purposes</m-message>'
     }))
     .add('state="information" && title', () => ({
-        template: '<m-message skin="page-light" state="information" title="Information Title">A message for testing purposes</m-message>'
+        template: '<m-message skin="page-light" state="information" title="Information Title">A message for information purposes</m-message>'
     }))
     .add('state="warning"', () => ({
-        template: '<m-message skin="page-light" state="warning">A message for testing purposes</m-message>'
+        template: '<m-message skin="page-light" state="warning">A message for warning purposes</m-message>'
     }))
     .add('state="warning" && title', () => ({
-        template: '<m-message skin="page-light" state="warning" title="Warning">A message for testing purposes</m-message>'
+        template: '<m-message skin="page-light" state="warning" title="Warning Title">A message for warning purposes</m-message>'
     }))
     .add('state="error"', () => ({
-        template: '<m-message skin="page-light" skinstate="error">A message for testing purposes</m-message>'
+        template: '<m-message skin="page-light" state="error">A message for error purposes</m-message>'
     }))
     .add('state="error" && title', () => ({
-        template: '<m-message skin="page-light" state="error" title="Confirmation Title">A message for testing purposes</m-message>'
+        template: '<m-message skin="page-light" state="error" title="Error Title">A message for error purposes</m-message>'
     }));
 
 
-storiesOf(`${componentsHierarchyRootSeparator}${MESSAGE_NAME}/skin="page-page"`, module)
+storiesOf(`${componentsHierarchyRootSeparator}${MESSAGE_NAME}/skin="page"`, module)
     .addDecorator(withA11y)
     .add('state="confirmation"', () => ({
-        template: '<m-message skin="page" state="confirmation">A message for testing purposes for testing purposes</m-message>'
+        template: '<m-message skin="page" state="confirmation">A message for confirmation purposes</m-message>'
     }))
     .add('state="confirmation" && title', () => ({
-        template: '<m-message skin="page" state="confirmation" title="Confirmation Title">A message for testing purposes for testing purposes</m-message>'
+        template: '<m-message skin="page" state="confirmation" title="Confirmation Title">A message for confirmation purposes</m-message>'
     }))
     .add('state="information"', () => ({
-        template: '<m-message skin="page" state="information">A message for testing purposes for testing purposes</m-message>'
+        template: '<m-message skin="page" state="information">A message for information purposes</m-message>'
     }))
     .add('state="information" && title', () => ({
-        template: '<m-message skin="page" state="information" title="Information Title">A message for testing purposes for testing purposes</m-message>'
+        template: '<m-message skin="page" state="information" title="Information Title">A message for information purposes</m-message>'
     }))
     .add('state="warning"', () => ({
-        template: '<m-message skin="page" state="warning">A message for testing purposes for testing purposes</m-message>'
+        template: '<m-message skin="page" state="warning">A message for warning purposes</m-message>'
     }))
     .add('state="warning" && title', () => ({
-        template: '<m-message skin="page" state="warning" title="Warning">A message for testing purposes for testing purposes</m-message>'
+        template: '<m-message skin="page" state="warning" title="Warning Title">A message for warning purposes</m-message>'
     }))
     .add('state="error"', () => ({
-        template: '<m-message skin="page" skinstate="error">A message for testing purposes for testing purposes</m-message>'
+        template: '<m-message skin="page" state="error">A message for error purposes</m-message>'
     }))
     .add('state="error" && title', () => ({
-        template: '<m-message skin="page" state="error" title="Confirmation Title">A message for testing purposes for testing purposes</m-message>'
+        template: '<m-message skin="page" state="error" title="Error Title">A message for error purposes</m-message>'
     }));
 
 
