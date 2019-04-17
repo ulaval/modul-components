5ac1861f870268b92bd7b09953bcde73c9dec694
Frédéric Duguay <frederic.duguay@dti.ulaval.ca>
2019-04-17 14:55:21 -0400

    MODUL-909 - Optimize link component stories

--- src/components/link/link.stories.ts
+++ src/components/link/link.stories.ts
@@ -1,5 +1,5 @@
 import { withA11y } from '@storybook/addon-a11y';
-import { withKnobs } from '@storybook/addon-knobs';
+import { text, select, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/vue';
 import Vue from 'vue';
 import { componentsHierarchyRootSeparator } from '../../../conf/storybook/utils';
@@ -20,7 +20,12 @@ storiesOf(`${componentsHierarchyRootSeparator}${LINK_NAME}`, module)
             template: '<m-link mode="link" url="#">A link</m-link>'
     }))
     .add('url', () => ({
-        template: '<m-link mode="link" url="http://www.google.ca">A link</m-link>'
+        props: {
+            url: {
+                default: text('url', 'http://www.google.ca')
+            }
+        },
+        template: '<m-link mode="link" :url="url">A link</m-link>'
     }))
     .add('disabled', () => ({
         template: '<m-link mode="link" url="#" :disabled="true">A link</m-link>'
@@ -31,14 +36,46 @@ storiesOf(`${componentsHierarchyRootSeparator}${LINK_NAME}`, module)
     .add('underline', () => ({
         template: '<m-link mode="link" url="#" :underline="false">A link</m-link>'
     }))
+    .add('target', () => ({
+        props: {
+            target: {
+                default: select('iconName', {
+                    'self': '_self',
+                    'blank': '_blank',
+                    'parent': '_parent',
+                    'top': '_top',
+                }, 'self')
+            },
+        },
+        template: '<m-link :target="target" mode="link" url="#" icon-name="iconName">A link</m-link>'
+    }))
     .add('icon', () => ({
         template: '<m-link mode="link" url="#" :icon="true">A link</m-link>'
     }))
-    .add('icon-size="20px"', () => ({
-        template: '<m-link :icon="true" mode="link" url="#" icon-size="20px">A link</m-link>'
+    .add('icon-size', () => ({
+        props: {
+            iconSize: {
+                default: text('iconSize', '20px')
+            }
+        },
+        template: '<m-link :icon="true" mode="link" url="#" :icon-size="iconSize">A link</m-link>'
     }))
-    .add('icon-name="m-svg__clock"', () => ({
-        template: '<m-link mode="link" url="#" icon-name="m-svg__clock">A link</m-link>'
+    .add('icon-name', () => ({
+        props: {
+            iconName: {
+                default: select('iconName', {
+                    'profile': 'm-svg__profile',
+                    'information': 'm-svg__information',
+                    'confirmation': 'm-svg__confirmation',
+                    'warning': 'm-svg__warning',
+                    'error': 'm-svg__error',
+                    'calendar': 'm-svg__calendar',
+                    'hint': 'm-svg__hint',
+                    'clock': 'm-svg__clock'
+                }, 'profile')
+            },
+        },
+        template: '<m-link :icon-name="iconName" mode="link" url="#" >A link</m-link>'
     }))
     .add('icon-position="right"', () => ({
         template: '<m-link mode="link" url="#" :icon="true" icon-position="right">A link</m-link>'
@@ -62,9 +99,23 @@ storiesOf(`${componentsHierarchyRootSeparator}${LINK_NAME}/skin="light"`, module
                         <m-link :icon="true" mode="link" url="#" skin="light">A link</m-link>
                    </div>`
     }))
-    .add('icon-name="m-svg__clock"', () => ({
+    .add('icon-name', () => ({
+        props: {
+            iconName: {
+                default: select('iconName', {
+                    'profile': 'm-svg__profile',
+                    'information': 'm-svg__information',
+                    'confirmation': 'm-svg__confirmation',
+                    'warning': 'm-svg__warning',
+                    'error': 'm-svg__error',
+                    'calendar': 'm-svg__calendar',
+                    'hint': 'm-svg__hint',
+                    'clock': 'm-svg__clock'
+                }, 'profile')
+            },
+        },
         template: `<div style="background: grey;">
-                        <m-link :icon="true" icon-name="m-svg__clock" mode="link" url="#" skin="light">A link</m-link>
+                        <m-link :icon="true" :icon-name="iconName" mode="link" url="#" skin="light">A link</m-link>
                    </div>`
     }))
     .add('icon-position="right"', () => ({
@@ -78,36 +129,28 @@ storiesOf(`${componentsHierarchyRootSeparator}${LINK_NAME}/skin="light"`, module
                    </div>`
     }));
 
+
 storiesOf(`${componentsHierarchyRootSeparator}${LINK_NAME}/skin="text"`, module)
     .addDecorator(withA11y)
     .add('default', () => ({
-        template: `<div style="background: grey;">
-                        <m-link mode="link" url="#" skin="text">A link</m-link>
-                   </div>`
+        template: '<m-link mode="link" url="#" skin="text">A link</m-link>'
     }))
     .add('icon', () => ({
-        template: `<div style="background: grey;">
-                        <m-link :icon="true" mode="link" url="#" skin="text">A link</m-link>
-                   </div>`
+        template: '<m-link :icon="true" mode="link" url="#" skin="text">A link</m-link>'
     }))
     .add('icon-name="m-svg__clock"', () => ({
-        template: `<div style="background: grey;">
-                        <m-link :icon="true" icon-name="m-svg__clock" mode="link" url="#" skin="text">A link</m-link>
-                   </div>`
+        template: '<m-link :icon="true" icon-name="m-svg__clock" mode="link" url="#" skin="text">A link</m-link>'
     }))
     .add('icon-position="right"', () => ({
-        template: `<div style="background: grey;">
-                        <m-link :icon="true" icon-position="right" mode="link" url="#" skin="text">A link</m-link>
-                   </div>`
+        template: '<m-link icon-position="right" icon-name="m-svg__clock" mode="link" url="#" skin="text">A link</m-link>'
     }))
     .add('icon-size="20px"', () => ({
-        template: `<div style="background: grey;">
-                        <m-link :icon="true" icon-size="20px" mode="link" url="#" skin="text">A link</m-link>
-                   </div>`
+        template: '<m-link icon-size="20px" icon-name="m-svg__clock" mode="link" url="#" skin="text">A link</m-link>'
     }));
 
 storiesOf(`${componentsHierarchyRootSeparator}${LINK_NAME}/mode`, module)
     .addDecorator(withA11y)
+    //TODO Register custom element
     // .add('default (router-link)', () => ({
     //     data: () => ({
     //         routerLink: { name: 'router-storybook', path: 'components/m-link' }
