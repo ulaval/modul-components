85f3950e9f31860ca5caf63da3ce708a7f924913
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2019-04-18 09:02:11 -0400

    MODUL-921 - Reset input avec le model (#896)

--- src/components/autocomplete/autocomplete.ts
+++ src/components/autocomplete/autocomplete.ts
@@ -79,6 +79,9 @@ export class MAutocomplete extends ModulVue {
     @Watch('selection')
     onSelection(): string {
         this.refreshItemsOnSelectionChange(this.selection);
+        if (this.selection === '') {
+            this.inputText = this.selection;
+        }
         return this.selection;
     }
 
