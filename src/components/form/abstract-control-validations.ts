0bcfa7693e0989bbe08e08ecd41e42d6c434a873
vguillemette <vincent.guillemette@dti.ulaval.ca>
2019-04-23 15:50:44 -0400

    * more shared validators

--- src/components/form/abstract-control-validations.ts
+++ src/components/form/abstract-control-validations.ts
@@ -29,20 +29,83 @@ export const AbstractControlRequiredValidator: (name: string) => AbstractControl
 export const AbstractControlMinLengthValidator: (name: string, minLength: number) => AbstractControlValidator = (name: string, minLength: number): AbstractControlValidator => {
     return {
         validationFunction: (control: FormControl<any>): boolean => {
-            let isMinLenght: boolean = false;
+            let isMinLength: boolean = false;
 
             if (control instanceof FormGroup) {
                 throw Error('the min length validator should not be attached to a form group');
             } else {
-                isMinLenght = control.value.length >= minLength;
+                isMinLength = control.value.length >= minLength;
             }
 
-            return isMinLenght;
+            return isMinLength;
         },
         error: {
             key: 'min-length',
             message: `the min length for this field is ${minLength}`,
             summaryMessage: `The min length for ${name} is ${minLength}`
+        }
+    };
+};
+
+export const AbstractControlMaxLengthValidator: (name: string, maxLength: number) => AbstractControlValidator = (name: string, maxLength: number): AbstractControlValidator => {
+    return {
+        validationFunction: (control: FormControl<any>): boolean => {
+            let isMaxLength: boolean = false;
+
+            if (control instanceof FormGroup) {
+                throw Error('the max length validator should not be attached to a form group');
+            } else {
+                isMaxLength = control.value.length >= maxLength;
+            }
+
+            return isMaxLength;
+        },
+        error: {
+            key: 'max-length',
+            message: `the max length for this field is ${maxLength}`,
+            summaryMessage: `The max length for ${name} is ${maxLength}`
+        }
+    };
+};
+
+export const AbstractControlMinValidator: (name: string, min: number) => AbstractControlValidator = (name: string, min: number): AbstractControlValidator => {
+    return {
+        validationFunction: (control: FormControl<any>): boolean => {
+            let isMin: boolean = false;
+
+            if (control instanceof FormGroup) {
+                throw Error('the min validator should not be attached to a form group');
+            } else {
+                isMin = Number(control.value) >= min;
+            }
+
+            return isMin;
+        },
+        error: {
+            key: 'min',
+            message: `the min for this field is ${min}`,
+            summaryMessage: `The min for ${name} is ${min}`
+        }
+    };
+};
+
+export const AbstractControlMaxValidator: (name: string, max: number) => AbstractControlValidator = (name: string, max: number): AbstractControlValidator => {
+    return {
+        validationFunction: (control: FormControl<any>): boolean => {
+            let isMax: boolean = false;
+
+            if (control instanceof FormGroup) {
+                throw Error('the max validator should not be attached to a form group');
+            } else {
+                isMax = Number(control.value) <= max;
+            }
+
+            return isMax;
+        },
+        error: {
+            key: 'max',
+            message: `the max for this field is ${max}`,
+            summaryMessage: `The min for ${name} is ${max}`
         }
     };
 };
