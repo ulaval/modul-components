c5da1b7f76c421110ea55acca22986e99da54a5a
Frédéric Duguay <frederic.duguay@dti.ulaval.ca>
2019-04-24 16:34:16 -0400

    MODUL-937 - Delete textfield sandboxes/ snapshot tests

--- src/components/textfield/textfield.spec.ts
+++ src/components/textfield/textfield.spec.ts
@@ -1,9 +1,7 @@
 import { mount, Wrapper } from '@vue/test-utils';
 import Vue from 'vue';
-import { renderComponent } from '../../../tests/helpers/render';
 import uuid from '../../utils/uuid/uuid';
 import TextfieldPlugin, { MTextfield, MTextfieldType } from './textfield';
-
 
 jest.mock('../../utils/uuid/uuid');
 (uuid.generate as jest.Mock).mockReturnValue('uuid');
@@ -11,13 +9,6 @@ jest.mock('../../utils/uuid/uuid');
 describe('MTextfield', () => {
     beforeEach(() => {
         Vue.use(TextfieldPlugin);
-    });
-    it('should render correctly', () => {
-        const component: Wrapper<MTextfield> = mount(MTextfield, {
-            localVue: Vue
-        });
-
-        return expect(renderComponent(component.vm)).resolves.toMatchSnapshot();
     });
 
     [MTextfieldType.Email, MTextfieldType.Password, MTextfieldType.Telephone, MTextfieldType.Text, MTextfieldType.Url]
