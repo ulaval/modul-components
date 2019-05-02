2e3af88302a1c114789cd2f4064000b823ec4350
Frédéric Duguay <frederic.duguay@dti.ulaval.ca>
2019-05-02 14:31:25 -0400

    MODUL-970 - Remove icon-button and error-pages deprecated snapshot tests

--- src/components/icon-button/icon-button.spec.ts
+++ src/components/icon-button/icon-button.spec.ts
@@ -1,8 +1,6 @@
 import { createLocalVue, mount, Wrapper } from '@vue/test-utils';
 import Vue, { VueConstructor } from 'vue';
-
-import { renderComponent } from '../../../tests/helpers/render';
-import IconButtonPlugin, { MIconButton, MIconButtonSkin } from './icon-button';
+import IconButtonPlugin, { MIconButton } from './icon-button';
 
 describe('MIconButton', () => {
     let localVue: VueConstructor<Vue>;
@@ -12,65 +10,12 @@ describe('MIconButton', () => {
         localVue.use(IconButtonPlugin);
     });
 
-    it('should render correctly', () => {
-        const btn: Wrapper<MIconButton> = mount(MIconButton, {
-            localVue: localVue
-        });
-
-        return expect(renderComponent(btn.vm)).resolves.toMatchSnapshot();
-    });
-
-    it('should render correctly selected skin', async () => {
-        const btn: Wrapper<MIconButton> = mount(MIconButton, {
-            localVue: localVue
-        });
-
-        for (const skin in MIconButtonSkin) {
-            btn.setProps({ skin: MIconButtonSkin[skin] });
-            expect(await renderComponent(btn.vm)).toMatchSnapshot(skin);
-        }
-    });
-
-    it('should render correctly when button size is set', () => {
-        const btn: Wrapper<MIconButton> = mount(MIconButton, {
-            localVue: localVue,
-            propsData: {
-                buttonSize: '58px'
-            }
-        });
-
-        return expect(renderComponent(btn.vm)).resolves.toMatchSnapshot();
-    });
-
-    it('should render correctly icon name when set', () => {
-        const btn: Wrapper<MIconButton> = mount(MIconButton, {
-            localVue: localVue,
-            propsData: {
-                iconName: 'clock'
-            }
-        });
-
-        return expect(renderComponent(btn.vm)).resolves.toMatchSnapshot();
-    });
-
-    it('should render correctly icon size when set', () => {
-        const btn: Wrapper<MIconButton> = mount(MIconButton, {
-            localVue: localVue,
-            propsData: {
-                iconSize: '14px'
-            }
-        });
-
-        return expect(renderComponent(btn.vm)).resolves.toMatchSnapshot();
-    });
-
     it('should emit click event when clicked', () => {
         const btn: Wrapper<MIconButton> = mount(MIconButton, {
             localVue: localVue
         });
 
         btn.find('button').trigger('click');
-
         expect(btn.emitted('click')).toBeTruthy();
     });
 });
