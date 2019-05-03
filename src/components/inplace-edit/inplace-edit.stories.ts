633e7e79ba70c27177556138c2e5173b7cf08016
Frédéric Duguay <frederic.duguay@dti.ulaval.ca>
2019-05-03 16:26:34 -0400

    MODUL-968 - Tweak input-edit stories

--- src/components/inplace-edit/inplace-edit.stories.ts
+++ src/components/inplace-edit/inplace-edit.stories.ts
@@ -1,5 +1,5 @@
 import { withA11y } from '@storybook/addon-a11y';
-import { boolean, text, withKnobs } from '@storybook/addon-knobs';
+import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/vue';
 import Vue from 'vue';
 import { componentsHierarchyRootSeparator } from '../../../conf/storybook/utils';
@@ -12,11 +12,32 @@ declare module '@storybook/addon-knobs' {
     export function withKnobs(): any;
 }
 
+const INPLACE_EDIT_TAGSTYLES: {} = {
+    'h1': 'h1',
+    'h2': 'h2',
+    'h3': 'h3',
+    'h4': 'h4',
+    'h5': 'h5',
+    'h6': 'h6',
+    'h7': 'h7'
+};
+
 storiesOf(`${componentsHierarchyRootSeparator}${INPLACE_EDIT_NAME}`, module)
     .addDecorator(withA11y)
     .addDecorator(withKnobs)
-    .add('default', () => ({
-        template: '<m-inplace-edit></m-inplace-edit>'
+    .add('readmode only', () => ({
+        data: () => ({
+            value: 'This is a value'
+        }),
+        props: {
+            editMode: {
+                default: boolean('editMode', false)
+            }
+        },
+        template: `<m-inplace-edit :edit-mode.sync="editMode">
+                       <span slot="readMode">{{ value }}</span>
+                       <m-textfield slot="editMode" label="Story #1 (readMode only)" v-model="value"></m-textfield>
+                   </m-inplace-edit>`
     }))
     .add('editMode', () => ({
         data: () => ({
@@ -28,13 +49,13 @@ storiesOf(`${componentsHierarchyRootSeparator}${INPLACE_EDIT_NAME}`, module)
             }
         },
         template: `<m-inplace-edit :edit-mode.sync="editMode">
-                       <span slot="readMode">{{value}}</span>
-                       <m-textfield slot="editMode" label="Story #1 (editMode)" v-model="value"></m-textfield>
+                       <span slot="readMode">{{ value }}</span>
+                       <m-textfield slot="editMode" label="Story #2 (editMode)" v-model="value"></m-textfield>
                    </m-inplace-edit>`
     }))
     .add('error', () => ({
         data: () => ({
-            value: 'This is a value'
+            value: '15826cvv5218'
         }),
         props: {
             editMode: {
@@ -45,8 +66,8 @@ storiesOf(`${componentsHierarchyRootSeparator}${INPLACE_EDIT_NAME}`, module)
             }
         },
         template: `<m-inplace-edit :edit-mode.sync="editMode" :error="error">
-                       <span slot="readMode">{{value}}</span>
-                       <m-textfield slot="editMode" label="Story #2 (error)" :v-model="value"></m-textfield>
+                       <span slot="readMode">{{ value }}</span>
+                       <m-textfield slot="editMode" label="Story #3 (error)" v-model="value"></m-textfield>
                    </m-inplace-edit>`
     }))
     .add('waiting', () => ({
@@ -62,28 +83,28 @@ storiesOf(`${componentsHierarchyRootSeparator}${INPLACE_EDIT_NAME}`, module)
             }
         },
         template: `<m-inplace-edit :edit-mode.sync="editMode" :waiting="waiting">
-                       <span slot="readMode">{{value}}</span>
-                       <m-textfield slot="editMode" label="Story #3 (waiting)" :v-model="value"></m-textfield>
+                       <span slot="readMode">{{ value }}</span>
+                       <m-textfield slot="editMode" label="Story #4 (waiting)" v-model="value"></m-textfield>
                    </m-inplace-edit>`
     }))
-    .add('padding', () => ({
+    .add('padding="26px"', () => ({
         data: () => ({
-            value: 'This is a value'
+            value: 'This is a value!'
         }),
         props: {
             editMode: {
                 default: boolean('editMode', false)
             },
             padding: {
-                default: text('waiting', '17px')
+                default: text('waiting', '26px')
             }
         },
         template: `<m-inplace-edit :edit-mode.sync="editMode" :padding="padding">
-                       <span slot="readMode">{{value}}</span>
-                       <m-textfield slot="editMode" label="Story #4 (padding)" :v-model="value"></m-textfield>
+                       <span slot="readMode">{{ value }}</span>
+                       <m-textfield slot="editMode" label="Story #5 (padding)" v-model="value"></m-textfield>
                    </m-inplace-edit>`
     }))
-    .add('editModePadding', () => ({
+    .add('editModePadding="32px"', () => ({
         data: () => ({
             value: 'This is a value'
         }),
@@ -92,12 +113,12 @@ storiesOf(`${componentsHierarchyRootSeparator}${INPLACE_EDIT_NAME}`, module)
                 default: boolean('editMode', true)
             },
             editModePadding: {
-                default: text('editModePadding', '10px')
+                default: text('editModePadding', '32px')
             }
         },
         template: `<m-inplace-edit :edit-mode.sync="editMode" :editModePadding="editModePadding">
-                       <span slot="readMode">{{value}}</span>
-                       <m-textfield slot="editMode" label="Story #5 (editModePadding)" :v-model="value"></m-textfield>
+                       <span slot="readMode">{{ value }}</span>
+                       <m-textfield slot="editMode" label="Story #6 (editModePadding)" v-model="value"></m-textfield>
                    </m-inplace-edit>`
     }))
     .add('title', () => ({
@@ -112,9 +133,35 @@ storiesOf(`${componentsHierarchyRootSeparator}${INPLACE_EDIT_NAME}`, module)
                 default: text('title', 'This is a custom title')
             }
         },
-        template: `<m-inplace-edit :edit-mode.sync="editMode" :title="title">
-                       <span slot="readMode">{{value}}</span>
-                       <m-textfield slot="editMode" label="Story #6 (title)" :v-model="value"></m-textfield>
+        template: `<div>
+                    <m-inplace-edit :edit-mode.sync="editMode" :title="title">
+                       <span slot="readMode">{{ value }}</span>
+                       <m-textfield slot="editMode" label="Story #7 (title)" v-model="value"></m-textfield>
+                   </m-inplace-edit>
+                    <br>
+                    <p><span style="color: blue">title</span> is set to <span style="color: red">{{ title }}</span></p>
+                    </div>`
+    }));
+
+storiesOf(`${componentsHierarchyRootSeparator}${INPLACE_EDIT_NAME}/tag-style`, module)
+    .addDecorator(withA11y)
+    .addDecorator(withKnobs)
+    .add('all tag styles', () => ({
+        data: () => ({
+            value: 'This is a value'
+        }),
+        props: {
+            editMode: {
+                default: boolean('editMode', true)
+            },
+            tagStyle: {
+                default: select('i-p-e tag style', INPLACE_EDIT_TAGSTYLES, 'h1')
+            }
+        },
+        template: `<m-inplace-edit :edit-mode.sync="editMode" :editModePadding="editModePadding">
+                       <span slot="readMode">{{ tagStyle }}</span>
+                       <m-textfield :tag-style="tagStyle" slot="editMode" label="Story #7 (Tag Styles)" 
+                       v-model="value"></m-textfield>
                    </m-inplace-edit>`
     }));
 
