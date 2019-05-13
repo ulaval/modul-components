d13975d3bafeb6e6b38f22b6939e42e8fb2c591e
Mathieu Boulianne <mathieu.boulianne1@gmail.com>
2019-05-13 14:42:54 -0400

    CurrencyFormat: Add the possibility to hide decimal 0 if the user wish to. 12.00 => 12 1.02 => 1.02 1.20 => 1.20

--- src/filters/money/money.spec.ts
+++ src/filters/money/money.spec.ts
@@ -1,29 +1,24 @@
 import Vue from 'vue';
+import { MCurrencyType } from '../../utils/money/money';
 import { MONEY_NAME } from '../filter-names';
-import { formatCurrency, MCurrencyType } from './money';
+import { formatCurrency } from './money';
 
 
 describe(MONEY_NAME, () => {
-
     it(`should return empty if the currency can't be displayed`, () => {
-        expect(formatCurrency(NaN)).toBe('');
+        expect(formatCurrency(NaN, MCurrencyType.CAD)).toBe('');
     });
 
     it(`should work correctly for valid data for french locale`, () => {
         let moneyAmout: number = 1;
-
-
         (Vue.prototype).$i18n.getCurrentLocale = jest.fn(() => 'fr-CA');
 
         const formatedCurrency: string = formatCurrency(moneyAmout, MCurrencyType.USD);
 
         expect(formatedCurrency).toBeDefined();
-
         expect((Vue.prototype).$i18n.getCurrentLocale)
             .toHaveBeenCalled();
     });
-
-
 });
 
 
--- src/filters/money/money.ts
+++ src/filters/money/money.ts
@@ -1,30 +1,38 @@
 import Vue, { PluginObject } from 'vue';
+import { MCurrencyType } from '../../utils/money/money';
 import { ModulVue } from '../../utils/vue/vue';
 import { MONEY_NAME } from '../filter-names';
 
-
-// ISO 4217  https://www.currency-iso.org/en/home/tables/table-a1.html
-export enum MCurrencyType {
-    CAD = 'CAD',
-    USD = 'USD',
-    EUR = 'EUR'
+export interface FormatCurrencyOptions {
+    currency: MCurrencyType;
+    stripDecimalZeroes?: boolean;
 }
 
-export function formatCurrency(money: number, currency = MCurrencyType.CAD): string {
-
+export function formatCurrency(money: number, currency: MCurrencyType): string {
     if (!isNaN(money)) {
-
-        let numberString: string = new Number(money).toLocaleString((Vue.prototype as ModulVue).$i18n.getCurrentLocale(), {
+        return new Number(money).toLocaleString((Vue.prototype as ModulVue).$i18n.getCurrentLocale(), {
             style: 'currency',
             currency: currency
         });
-
-        return numberString;
     } else {
         return '';
     }
-
 }
+
+export function formatCurrencyWithOptions(money: number, options: FormatCurrencyOptions): string {
+    if (!isNaN(money)) {
+        const stripDecimalZeroes: boolean = Number.isInteger(money) && !!options.stripDecimalZeroes;
+        return new Number(money).toLocaleString((Vue.prototype as ModulVue).$i18n.getCurrentLocale(), {
+            style: 'currency',
+            currency: options.currency,
+            minimumFractionDigits: stripDecimalZeroes ? 0 : undefined,
+            maximumFractionDigits: stripDecimalZeroes ? 0 : undefined
+        });
+    } else {
+        return '';
+    }
+}
+
 
 
 const MoneyPlugin: PluginObject<any> = {
