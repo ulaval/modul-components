fd16673d7e87fc690efd9c7447151223140bc3d9
Charles Maheu <CHMAH33@ulaval.ca>
2019-05-15 15:18:02 -0400

    MODUL-1002 - add unit test for formcontrol

--- src/utils/form/form-contols.spec.ts
+++ src/utils/form/form-contols.spec.ts
@@ -1,3 +1,4 @@
+import { ControlValidatorValidationType } from './control-validator-validation-type';
 import { FormControl } from './form-control';
 
 describe('FromControl', () => {
@@ -6,25 +7,14 @@ describe('FromControl', () => {
 
     describe('given a FormControl with no validation', () => {
 
-        it('should be empty of control and valid', () => {
-            formControl = new FormControl<string>();
-
-            expect(formControl.valid).toBe(true);
-            expect(formControl.hasError()).toBe(false);
-            expect(formControl.waiting).toBe(false);
-            expect(formControl.enabled).toBe(true);
-            expect(formControl.readonly).toBe(false);
-            expect(formControl.pristine).toBe(true);
-            expect(formControl.touched).toBe(false);
-            expect(formControl.value).toBeUndefined();
-        });
-
-        it('initial value should be set', () => {
+        beforeAll(() => {
             formControl = new FormControl<string>([],
                 {
                     initialValue: 'foo'
                 });
+        });
 
+        it('should be empty of control and valid', () => {
             expect(formControl.valid).toBe(true);
             expect(formControl.hasError()).toBe(false);
             expect(formControl.waiting).toBe(false);
@@ -33,6 +23,85 @@ describe('FromControl', () => {
             expect(formControl.pristine).toBe(true);
             expect(formControl.touched).toBe(false);
             expect(formControl.value).toBe('foo');
+        });
+
+        describe('when edited', () => {
+            beforeAll(() => {
+                formControl.initEdition();
+                formControl.value = 'test';
+                formControl.endEdition();
+            });
+
+
+            it('should run validation and set flag', () => {
+                expect(formControl.valid).toBe(true);
+                expect(formControl.hasError()).toBe(false);
+                expect(formControl.waiting).toBe(false);
+                expect(formControl.pristine).toBe(false);
+                expect(formControl.touched).toBe(true);
+                expect(formControl.value).toBe('test');
+            });
+
+
+            it('should reset to inital value on reset', () => {
+                formControl.reset();
+
+                expect(formControl.valid).toBe(true);
+                expect(formControl.hasError()).toBe(false);
+                expect(formControl.waiting).toBe(false);
+                expect(formControl.enabled).toBe(true);
+                expect(formControl.readonly).toBe(false);
+                expect(formControl.pristine).toBe(true);
+                expect(formControl.touched).toBe(false);
+                expect(formControl.value).toBe('foo');
+            });
+        });
+    });
+
+    describe('given a FormControl with a always false OnGoing validation', () => {
+
+        beforeAll(() => {
+            formControl = new FormControl<string>(
+                [{
+                    key: 'always-false',
+                    validationFunction: (control: FormControl<string>) => {
+                        return Promise.resolve(false);
+                    },
+                    error: {
+                        message: 'Always false'
+                    },
+                    validationType: ControlValidatorValidationType.OnGoing
+                }]
+            );
+        });
+
+        it('should be invalid and no error', () => {
+            expect(formControl.valid).toBe(false);
+            expect(formControl.hasError()).toBe(false);
+            expect(formControl.errorMessage).toBe('');
+        });
+
+        describe('when edited', () => {
+            beforeAll(() => {
+                formControl.initEdition();
+                formControl.value = 'test';
+                formControl.endEdition();
+            });
+
+            it('should be invalid and no error', () => {
+                expect(formControl.valid).toBe(false);
+                expect(formControl.hasError()).toBe(true);
+                expect(formControl.errorMessage).toBe('Always false');
+            });
+
+            it('should reset message inital value on reset', () => {
+
+                formControl.reset();
+
+                expect(formControl.valid).toBe(false);
+                expect(formControl.hasError()).toBe(false);
+                expect(formControl.errorMessage).toBe('');
+            });
         });
     });
 });
