fb7011a1f29fe178005bb1fae7891dcea9ccfc83
Charles Maheu <CHMAH33@ulaval.ca>
2019-05-23 09:27:28 -0400

    bonification de l'example form-complex

--- src/sandboxes/form/form-complex.sandbox.ts
+++ src/sandboxes/form/form-complex.sandbox.ts
@@ -22,56 +22,19 @@ export class MFormAllSandbox extends ModulVue {
         name: 'Patate',
         description: 'La patate est\nun légume vraiment...',
         birthday: '1970-01-01',
-        type: 'sec'
+        type: 'sec',
+        champSupplActive: true,
+        champSuppl: {
+            supplField1: 'suppl field 1',
+            supplField3: 'suppl field 3'
+        },
+        items: [
+            { name: 'Joe', optionalData: 'blow' },
+            { name: 'John' }
+        ]
     };
 
-    formGroup: FormGroup = new FormGroup({
-        name: new FormControl<string>(
-            [
-                RequiredValidator('name'),
-                MaxLengthValidator('name', 20),
-                MinLengthValidator('name', 2)
-            ]
-        ),
-        description: new FormControl<string>(
-            [
-                RequiredValidator('description'),
-                MaxLengthValidator('description', 255)
-            ]
-        ),
-        birthdate: new FormControl<string>(
-            [
-                RequiredValidator('birthdate')
-            ]
-        ),
-        type: new FormControl<string>(
-            [
-                RequiredValidator('type')
-            ]
-        ),
-        champSupplActive: new FormControl<boolean>(),
-        champSuppl: new FormGroup({
-            supplField1: new FormControl<string>([
-                RequiredValidator('supplField1')]),
-            supplField2: new FormControl<string>(),
-            supplField3: new FormControl<string>()
-        }),
-        items: new FormArray()
-
-    });
-
-    submit(): void {
-        this.pendingSubmit = true;
-        setTimeout(() => {
-            this.pendingSubmit = false;
-            alert('MFormAllSandbox.submit!');
-            this.$log.info();
-        }, 2000);
-    }
-
-    reset(): void {
-        alert('MFormAllSandbox.reset');
-    }
+    formGroup: FormGroup = this.buildFormGroup();
 
     get nameField(): FormControl<string> {
         return this.formGroup.getControl('name') as FormControl<string>;
@@ -117,11 +80,25 @@ export class MFormAllSandbox extends ModulVue {
         return this.formGroup.getControl('items') as FormArray;
     }
 
+    submit(): void {
+        this.pendingSubmit = true;
+        setTimeout(() => {
+            this.pendingSubmit = false;
+            alert('MFormAllSandbox.submit!');
+            this.$log.info();
+        }, 2000);
+    }
+
+    reset(): void {
+        alert('MFormAllSandbox.reset');
+    }
+
     loadValue(): void {
-        this.nameField.value = this.loadedValues.name;
-        this.descriptionField.value = this.loadedValues.description;
-        this.birthdateField.value = this.loadedValues.birthday;
-        this.typeField.value = this.loadedValues.type;
+        this.formGroup = this.buildFormGroup(this.loadedValues);
+    }
+
+    clearValue(): void {
+        this.formGroup = this.buildFormGroup({});
     }
 
     toggleReadonly(): void {
@@ -138,20 +115,7 @@ export class MFormAllSandbox extends ModulVue {
     }
 
     addItem(): void {
-        this.itemsArray.addControl(new FormGroup({
-            name: new FormControl<string>(
-                [
-                    RequiredValidator('name'),
-                    MaxLengthValidator('name', 20),
-                    MinLengthValidator('name', 2)
-                ]
-            ),
-            optionalData: new FormControl<string>(
-                [
-                    MaxLengthValidator('optionalData', 255)
-                ]
-            )
-        }));
+        this.itemsArray.addControl(this.buildItemsFormGroup());
     }
 
     deleteItem(index): void {
@@ -167,6 +131,102 @@ export class MFormAllSandbox extends ModulVue {
             this.champSupplGroup.enabled = false;
         }
 
+    }
+
+    private buildFormGroup(data: any = {}): FormGroup {
+        const _formGroup: FormGroup = new FormGroup({
+            name: new FormControl<string>(
+                [
+                    RequiredValidator('name'),
+                    MaxLengthValidator('name', 20),
+                    MinLengthValidator('name', 2)
+                ],
+                {
+                    initialValue: data.name ? data.name : ''
+                }
+            ),
+            description: new FormControl<string>(
+                [
+                    RequiredValidator('description'),
+                    MaxLengthValidator('description', 255)
+                ],
+                {
+                    initialValue: data.description ? data.description : ''
+                }
+            ),
+            birthdate: new FormControl<string>(
+                [
+                    RequiredValidator('birthdate')
+                ],
+                {
+                    initialValue: data.birthday ? data.birthday : ''
+                }
+            ),
+            type: new FormControl<string>(
+                [
+                    RequiredValidator('type')
+                ],
+                {
+                    initialValue: data.type ? data.type : ''
+                }
+            ),
+            champSupplActive: new FormControl<boolean>([],
+                {
+                    initialValue: data.champSupplActive ? data.champSupplActive : false
+                }),
+            champSuppl: this.buildChampSupplFormGroup(data.champSuppl ? data.champSuppl : {}),
+            items: new FormArray()
+        });
+
+        if (data.items && data.items.length > 0) {
+            data.items.forEach(data => {
+                (_formGroup.getControl('items') as FormArray).addControl(this.buildItemsFormGroup(data));
+            });
+        }
+
+        return _formGroup;
+    }
+
+
+    private buildChampSupplFormGroup(data: any = {}): FormGroup {
+        return new FormGroup({
+            supplField1: new FormControl<string>([
+                RequiredValidator('supplField1')],
+                {
+                    initialValue: data.supplField1 ? data.supplField1 : ''
+                }),
+            supplField2: new FormControl<string>([],
+                {
+                    initialValue: data.supplField2 ? data.supplField2 : ''
+                }),
+            supplField3: new FormControl<string>([],
+                {
+                    initialValue: data.supplField3 ? data.supplField3 : ''
+                })
+        });
+    }
+
+    private buildItemsFormGroup(data: any = {}): FormGroup {
+        return new FormGroup({
+            name: new FormControl<string>(
+                [
+                    RequiredValidator('name'),
+                    MaxLengthValidator('name', 20),
+                    MinLengthValidator('name', 2)
+                ],
+                {
+                    initialValue: data.name ? data.name : ''
+                }
+            ),
+            optionalData: new FormControl<string>(
+                [
+                    MaxLengthValidator('optionalData', 255)
+                ],
+                {
+                    initialValue: data.optionalData ? data.optionalData : ''
+                }
+            )
+        });
     }
 
 
