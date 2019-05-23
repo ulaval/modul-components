aa7de474b8a317c258fbed35e4b54529afdb2af8
Jean-François Dion <jeff.dion@gmail.com>
2019-05-22 20:17:16 -0400

    MODUL-1016 - Added base structure for address-lookup service

---
+++ src/utils/address-lookup/address-lookup-loqate-service.ts
@@ -0,0 +1,12 @@
+import AddressLookupService from './address-lookup-service';
+
+export default class AddressLookupLoqateService implements AddressLookupService {
+
+    find() {
+        throw new Error('Method not implemented.');
+    }
+
+    retrieve() {
+        throw new Error('Method not implemented.');
+    }
+}
---
+++ src/utils/address-lookup/address.ts
@@ -0,0 +1,8 @@
+export default interface Address {
+    street: string;
+    city: string;
+    provinceState: string;
+    postalCode: string;
+    country: string;
+    subBuilding: string;
+}
