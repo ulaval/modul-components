69dc666754bb2c1b9e81d171d87ffd44dc4f7cf9
Alvéric Alie <Netnix@users.noreply.github.com>
2019-05-31 14:43:34 -0400

    MODUL-1068 - Add reactivity to initial value (#1003)
    * MODUL-1068 - Add reactivity to initial value
    
    * MODUL-1068 - add setter for intial value and optional param in reset
    
    * MODUL-1068 - fix min max unit test
    
    * MODUL-1068 - fix unit test + modification after code review

--- src/components/form/form.stories.ts
+++ src/components/form/form.stories.ts
@@ -113,6 +113,53 @@ storiesOf(`${componentsHierarchyRootSeparator}${FORM_NAME}`, module)
 
         `
     }));
+storiesOf(`${componentsHierarchyRootSeparator}${FORM_NAME}`, module)
+    .add('reactive initial value', () => ({
+        data: () => ({
+            formGroup: new FormGroup(
+                {
+                    'name': new FormControl<string>([RequiredValidator('name')], { initialValue: 'blabla' })
+                }
+
+            )
+        }),
+        methods: {
+            submit(value: string): void {
+                Vue.prototype.$log.log('submited');
+                this.$data.formGroup.getControl('name').initalValue = value;
+            },
+            reset(): void {
+                Vue.prototype.$log.log('reseted');
+            },
+            resetToNewValue(): void {
+                this.$data.formGroup.getControl('name').reset('newValue');
+                Vue.prototype.$log.log('resetToNewValue');
+            }
+
+        },
+        template: `
+            <div>
+                <m-form class="m-u--margin-top"
+                    :form-group="formGroup"
+                    @reset="reset()"
+                    @submit="submit(formGroup.getControl('name').value)">
+                <m-textfield v-model.trim="formGroup.getControl('name').value"
+                            :error-message="formGroup.getControl('name').errors.length > 0 ? formGroup.getControl('name').errors[0].message : null"
+                            :label="formGroup.getControl('name').name"
+                            v-m-control="formGroup.getControl('name')">
+                </m-textfield>
+                <p class="m-u--margin-bottom--l">
+                    <m-button type="submit">Submit</m-button>
+                    <m-button type="reset"
+                            >Reset</m-button>
+                    <m-button skin="secondary"
+                            @click="resetToNewValue">Reset to new value</m-button>
+                </p>
+                </m-form>
+            </div>
+
+            `
+    }));
 
 storiesOf(`${componentsHierarchyRootSeparator}${FORM_NAME}/built-in action-fallouts`, module)
     .addDecorator(withA11y)
