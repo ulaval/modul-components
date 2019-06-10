9080b40a07a9143282084a4cf3d98f41100af15c
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2019-06-10 19:57:18 -0400

    MODUL-1067 - Ajout flag-icon-css

--- src/components/flag/flag.ts
+++ src/components/flag/flag.ts
@@ -1,13 +1,26 @@
+import 'flag-icon-css/css/flag-icon.min.css';
 import { PluginObject } from 'vue';
 import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
 import { ModulVue } from '../../utils/vue/vue';
 import { FLAG_NAME } from '../component-names';
-import WithRender from './flag.html?style=./flag.scss';
-
+import WithRender from './flag.html';
 
 @WithRender
 @Component
 export class MFlag extends ModulVue {
+    @Prop({ required: true })
+    public iso: string;
+
+    @Prop()
+    public title: boolean;
+
+    @Prop()
+    public squared: boolean;
+
+    public get flagIconClass(): string {
+        return ((!!this.squared) ? 'flag-icon-squared ' : '') + 'flag-icon-' + this.iso.toLowerCase();
+    }
 
 }
 
