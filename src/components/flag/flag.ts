924eee06d330590bb2940a957c14462e615444df
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2019-06-11 12:44:18 -0400

    MODUL-1067 - Ajout sprite flag

--- src/components/flag/flag.ts
+++ src/components/flag/flag.ts
@@ -1,31 +1,54 @@
 import 'flag-icon-css/css/flag-icon.min.css';
-import { PluginObject } from 'vue';
+import Vue, { PluginObject } from 'vue';
 import Component from 'vue-class-component';
-import { Prop } from 'vue-property-decorator';
+import { Emit, Prop } from 'vue-property-decorator';
 import { ModulVue } from '../../utils/vue/vue';
 import { FLAG_NAME } from '../component-names';
-import WithRender from './flag.html';
+import WithRender from './flag.html?style=./flag.scss';
 
 @WithRender
 @Component
 export class MFlag extends ModulVue {
     @Prop({ required: true })
     public iso: string;
-
     @Prop()
-    public title: boolean;
-
-    @Prop()
+    public title: string;
+    @Prop({ default: '20px' })
+    public size: string;
+    @Prop({ default: false })
     public squared: boolean;
 
-    public get flagIconClass(): string {
-        return ((!!this.squared) ? 'flag-icon-squared ' : '') + 'flag-icon-' + this.iso.toLowerCase();
+    @Emit('click')
+    onClick(event: Event): void { }
+
+    @Emit('keydown')
+    onKeydown(event: Event): void { }
+
+    private get hasTitle(): boolean {
+        return !!this.title;
+    }
+
+    private get height(): string {
+        return this.size;
+    }
+
+    private get width(): string {
+        return ((parseInt(this.size, 10) * 4) / 3) + 'px';
+    }
+
+    private get spriteId(): string | undefined {
+        if (document.getElementById('m-svg__flag-' + this.iso)) {
+            return '#m-svg__flag-' + this.iso;
+        } else if (this.iso) {
+            Vue.prototype.$log.warn('"' + this.iso + '" is not a valid iso country. Make sure that the sprite has been loaded via the $svg instance service.');
+        }
     }
 
 }
 
 const FlagPlugin: PluginObject<any> = {
-    install(v, options): void {
+    install(v): void {
+        v.prototype.$log.debug(FLAG_NAME, 'plugin.install');
         v.component(FLAG_NAME, MFlag);
     }
 };
