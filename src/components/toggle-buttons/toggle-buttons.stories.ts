c47bb6df506c0ab7a0d0500157a603bf1aa94be6
Jonathan Matte <jonathan.matte@dti.ulaval.ca>
2019-06-21 10:01:41 -0400

    MODUL-1103 - Renommer constante

--- src/components/toggle-buttons/toggle-buttons.stories.ts
+++ src/components/toggle-buttons/toggle-buttons.stories.ts
@@ -12,17 +12,17 @@ declare module '@storybook/addon-knobs' {
     export function withKnobs(): any;
 }
 
-const june: MToggleButton = { id: 'june', title: 'June' };
-const july: MToggleButton = { id: 'july', title: 'July' };
-const august: MToggleButton = { id: 'august', title: 'August' };
-const september: MToggleButton = { id: 'september', title: 'September' };
-const october: MToggleButton = { id: 'october', title: 'October', pressed: true };
-const november: MToggleButton = { id: 'november', title: 'November' };
-const december: MToggleButton = { id: 'december', title: 'December', pressed: true };
+const JUNE: MToggleButton = { id: 'june', title: 'June' };
+const JULY: MToggleButton = { id: 'july', title: 'July' };
+const AUGUST: MToggleButton = { id: 'august', title: 'August' };
+const SEPTEMBER: MToggleButton = { id: 'september', title: 'September' };
+const OCTOBER: MToggleButton = { id: 'october', title: 'October', pressed: true };
+const NOVEMBER: MToggleButton = { id: 'november', title: 'November' };
+const DECEMBER: MToggleButton = { id: 'december', title: 'December', pressed: true };
 
-const monthsDefault: MToggleButton[] = [june, july, august, september];
-const monthsMultipleSelection: MToggleButton[] = [september, october, november, december];
-const monthsSingleSelection: MToggleButton[] = [august, september, october, november];
+const monthsDefault: MToggleButton[] = [JUNE, JULY, AUGUST, SEPTEMBER];
+const monthsMultipleSelection: MToggleButton[] = [SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER];
+const monthsSingleSelection: MToggleButton[] = [AUGUST, SEPTEMBER, OCTOBER, NOVEMBER];
 
 storiesOf(`${componentsHierarchyRootSeparator}${TOGGLE_BUTTONS_NAME}`, module)
     .addDecorator(withA11y)
