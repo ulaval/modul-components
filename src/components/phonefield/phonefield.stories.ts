69574e11007d53827ce28dafe680d7552ac039d5
Charles Maheu <CHMAH33@ulaval.ca>
2019-06-21 11:06:55 -0400

    MODUL-1067 - merge with develop

--- src/components/phonefield/phonefield.stories.ts
+++ src/components/phonefield/phonefield.stories.ts
@@ -1,5 +1,3 @@
-import { withA11y } from '@storybook/addon-a11y';
-import { withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/vue';
 import Vue from 'vue';
 import { componentsHierarchyRootSeparator } from '../../../conf/storybook/utils';
@@ -8,14 +6,7 @@ import PhonefieldPlugin from './phonefield';
 
 Vue.use(PhonefieldPlugin);
 
-declare module '@storybook/addon-knobs' {
-    export function withKnobs(): any;
-}
-
-
 storiesOf(`${componentsHierarchyRootSeparator}${PHONEFIELD_NAME}`, module)
-    .addDecorator(withA11y)
-    .addDecorator(withKnobs)
     .add('default', () => ({
         data: () => ({
             model: '',
