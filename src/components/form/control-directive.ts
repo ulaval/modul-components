f49b7adeabd56cb4137e4d6381c87b8e8d768955
Alvéric Alie <alveric.alie@dti.ulaval.ca>
2019-07-11 09:09:01 -0400

    Revert focus changes

--- src/components/form/control-directive.ts
+++ src/components/form/control-directive.ts
@@ -7,32 +7,36 @@ const INPUT_SELECTOR: string = 'input, textarea, [contenteditable=true]';
 export const AbstractControlDirective: DirectiveOptions = {
     bind(
         el: HTMLElement,
-        binding: VNodeDirective
+        binding: VNodeDirective,
+        vnode: VNode
     ): void {
         const control: AbstractControl = binding.value;
 
-        if (control) {
-            // We can't assign the element directly on recent Vue version.
-            // https://github.com/vuejs/vue/issues/7788
-            control.htmlElementAccessor = () => el.querySelector(INPUT_SELECTOR) as HTMLElement;
-        }
+        // We can't assign the element directly on recent Vue version.
+        // https://github.com/vuejs/vue/issues/7788
+        control.htmlElementAccessor = () => el.querySelector(INPUT_SELECTOR) as HTMLElement;
 
         if (control instanceof FormControl) {
-            el.addEventListener('focusin', control.initEdition);
-            el.addEventListener('focusout', control.endEdition);
+            Object.defineProperty(el, 'ControlDirectiveListeners', {
+                value: {
+                    focusListener: () => control.initEdition(),
+                    blurListener: () => control.endEdition()
+                }
+            });
+            (vnode.componentInstance as Vue).$on('focus', el['ControlDirectiveListeners'].focusListener);
+            (vnode.componentInstance as Vue).$on('blur', el['ControlDirectiveListeners'].blurListener);
         }
     },
     unbind(
         el: HTMLElement,
-        binding: VNodeDirective
+        binding: VNodeDirective,
+        vnode: VNode
     ): void {
         const control: AbstractControl = binding.value;
-        const inputElement: Element | null = el.querySelector(INPUT_SELECTOR);
-        if (!control || !inputElement || control.htmlElement === inputElement) {
-            control.htmlElementAccessor = () => { return undefined! as HTMLElement; };
+        control.htmlElementAccessor = () => { return undefined! as HTMLElement; };
+        if (control instanceof FormControl) {
+            (vnode.componentInstance as Vue).$off('focus', el['ControlDirectiveListeners'].focusListener);
+            (vnode.componentInstance as Vue).$off('blur', el['ControlDirectiveListeners'].blurListener);
         }
-
-        el.removeEventListener('focusin', control.initEdition);
-        el.removeEventListener('focusout', control.endEdition);
     }
 };
