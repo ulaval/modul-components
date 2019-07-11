9ce0b4dede39ce9243262397831120c192390aa9
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2019-07-11 16:36:28 -0400

    MODUL-1132 - Ménage composant input-group

--- src/components/input-group/input-group.ts
+++ src/components/input-group/input-group.ts
@@ -1,6 +1,7 @@
 import { PluginObject } from 'vue';
 import Component from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
+import uuid from '../../utils/uuid/uuid';
 import { INPUT_GROUP_NAME } from '../component-names';
 import ValidationMessagePlugin from '../validation-message/validation-message';
 import { InputManagement } from './../../mixins/input-management/input-management';
@@ -8,6 +9,10 @@ import { InputState } from './../../mixins/input-state/input-state';
 import { ModulVue } from './../../utils/vue/vue';
 import WithRender from './input-group.html?style=./input-group.scss';
 
+export enum MMInputGroupValidationMessagePosition {
+    Top = 'top',
+    Bottom = 'bottom'
+}
 
 @WithRender
 @Component({
@@ -18,25 +23,53 @@ import WithRender from './input-group.html?style=./input-group.scss';
 })
 export class MInputGroup extends ModulVue {
 
-    @Prop({ default: true })
-    visible: boolean;
+    @Prop()
+    readonly label!: string;
 
     @Prop()
-    legend: string;
+    readonly requiredMarker!: boolean;
 
-    @Prop({ default: true })
-    displayValidation: boolean;
+    @Prop()
+    readonly inline!: boolean;
 
-    get classes(): { [name: string]: boolean } {
-        return {
-            'm--is-readonly': this.as<InputState>().readonly,
-            'm--is-disabled': this.as<InputState>().isDisabled,
-            'm--is-waiting': this.as<InputState>().isWaiting,
-            'm--has-error': this.as<InputState>().hasError,
-            'm--is-valid': this.as<InputState>().isValid,
-            'm--has-validation-message': this.as<InputState>().hasValidationMessage,
-            'm--border-hidden': !this.visible
-        };
+    @Prop({
+        default: MMInputGroupValidationMessagePosition.Top,
+        validator: value =>
+            value === MMInputGroupValidationMessagePosition.Top ||
+            value === MMInputGroupValidationMessagePosition.Bottom
+    })
+    public validationMessagePosition: MMInputGroupValidationMessagePosition;
+
+    get stateIsDisabled(): boolean {
+        return this.as<InputState>().isDisabled;
+    }
+
+    get stateIsError(): boolean {
+        return this.as<InputState>().hasError;
+    }
+
+    get stateIsValid(): boolean {
+        return this.as<InputState>().isValid;
+    }
+
+    get hasLabel(): boolean {
+        return !!this.label;
+    }
+
+    get isValidationMessagePositionTop(): boolean {
+        return this.validationMessagePosition === MMInputGroupValidationMessagePosition.Top;
+    }
+
+    get isValidationMessagePositionBottom(): boolean {
+        return this.validationMessagePosition === MMInputGroupValidationMessagePosition.Bottom;
+    }
+
+    get idLabel(): string | undefined {
+        return this.hasLabel ? uuid.generate() : undefined;
+    }
+
+    get idValidationMessage(): string | undefined {
+        return this.as<InputState>().errorMessage || this.as<InputState>().validMessage || this.as<InputState>().helperMessage ? uuid.generate() : undefined;
     }
 }
 
