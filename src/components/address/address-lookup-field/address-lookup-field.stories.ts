d075c4421b40bf810d3b76fb5242bbdf1f1575b3
Mathieu Boulianne <mathieu.boulianne1@gmail.com>
2019-07-19 10:21:12 -0400

    Code review

--- src/components/address/address-lookup-field/address-lookup-field.stories.ts
+++ src/components/address/address-lookup-field/address-lookup-field.stories.ts
@@ -1,7 +1,7 @@
 import { storiesOf } from '@storybook/vue';
-import { Address } from 'cluster';
 import Vue from 'vue';
 import { componentsHierarchyRootSeparator } from '../../../../conf/storybook/utils';
+import { Address } from '../../../utils/address-lookup/address';
 import { ADDRESS_LOOKUP_FIELD_NAME } from '../../component-names';
 import AddressPlugin from '../address';
 
