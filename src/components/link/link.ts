8c3436b867fa84e5e70d18c45e86397d7e93dce2
Jean-Philippe Goydadin <jean-philippe.goydadin@dti.ulaval.ca>
2019-07-31 16:51:08 -0400

    MODUL-1162 - Ajustement de la taille de l'icone par défaut, prise 2

--- src/components/link/link.ts
+++ src/components/link/link.ts
@@ -77,7 +77,7 @@ export class MLink extends ModulVue {
     })
     public iconPosition: MLinkIconPosition;
 
-    @Prop({ default: '16px' })
+    @Prop({ default: '24px' })
     public iconSize: string;
 
     @Prop({ default: '0' })
